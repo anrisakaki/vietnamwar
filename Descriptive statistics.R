3c69a09f89a494e3d850ba4f9a96425c751f6ab7
Anri Sakakibara <anrisakaki@gmail.com>
2023-09-25 16:17:59 +0100

    Descriptive statistics

--- Descriptive statistics.R
+++ Descriptive statistics.R
@@ -1,59 +1,9 @@
-birthcohort_sum <- varhs1_08 %>%
-  # filter(p2q4_!= 98) %>% 
-  # mutate(female = ifelse(p1q3_ == 2, 1, 0)) %>%
-  group_by(p2q4_) %>% 
-  summarise(educ_mean = mean(p3q14_, na.rm = T)) %>% 
-  mutate(educ_mean = round(educ_mean, 2)) %>% 
-  filter(p2q4_ < 1988 & p2q4_ > 1945)
-
 birthcohort_sum_vhlss <- vhlss06 %>%
   group_by(birth_year) %>%
   filter(!is.na(educ)) %>% 
   summarise(educ_mean = sum(educ * hhwt) / sum(hhwt)) %>% 
   mutate(educ_mean = round(educ_mean, 2)) %>% 
   filter(birth_year < 1988 & birth_year > 1945)
-
-ggplot(birthcohort_sum, aes(x = p2q4_, y = educ_mean)) +
-  geom_line(size = 1.1) +
-  # End of French rule 
-  geom_vline(xintercept = 1954, linetype = "dashed", color = "blue") +  
-  geom_text(aes(x = 1954, y = max(10.2), label = "End of French rule"),
-            vjust = -0.5, hjust = 1.3, color = "blue") +  
-  # Start of VN War
-  geom_vline(xintercept = 1955, linetype = "dashed", color = "red") +  
-  geom_text(aes(x = 1955, y = max(10.2), label = "Start of Vietnam War"),
-            vjust = -0.5, hjust = -0.2, color = "red") +  
-  # Fall of Saigon 
-  geom_vline(xintercept = 1975, linetype = "dashed", color = "red") +  
-  geom_text(aes(x = 1975, y = max(10.2), label = "Fall of Saigon"),
-            vjust = -0.5, hjust = 1.3, color = "red") +
-  # Reunification of Vietnam 
-  geom_vline(xintercept = 1976, linetype = "dashed", color = "dark green") +  
-  geom_text(aes(x = 1976, y = max(10.2), label = "Reunification"),
-            vjust = -0.5, hjust = -0.2, color = "dark green") +  
-  # Doi Moi reforms 
-  geom_vline(xintercept = 1980, linetype = "dashed", color = "dark green") +  
-  geom_text(aes(x = 1980, y = max(10.2), label = "Doi Moi reforms"),
-            vjust = -0.5, hjust = -0.2, color = "dark green") +  
-  # Adding years of American bombing
-  geom_rect(
-    xmin = 1965, xmax = 1975, ymin = -Inf, ymax = Inf,  
-    fill = "gray", alpha = 0.01  
-  ) +
-  annotate(
-    "text", x = (1970), y = 9.5,  # Add text label
-    label = "Period of intense American bombing", size = 4
-  ) +  
-  scale_x_continuous(breaks=seq(1946,1987,1)) +
-  labs(x = "Birth cohort",
-       y = "Avg. years in education") +
-  theme_minimal() +
-  theme(axis.line = element_line(color='black'),
-        plot.background = element_blank(),
-        panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank(),
-        panel.border = element_blank(),
-        legend.title=element_blank())
 
 ggplot(birthcohort_sum_vhlss, aes(x = birth_year, y = educ_mean)) +
   geom_line(size = 1.1) +
