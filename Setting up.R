b50a1b639f19b2b3488fbb84184750e660ca80b2
Anri Sakakibara <anrisakaki@gmail.com>
2023-09-28 15:50:11 +0100

    Setting up

--- Setting up.R
+++ Setting up.R
@@ -4,12 +4,41 @@ library(haven)
 library(sf)
 library(fixest)
 
-#  SECTION 1A. HOUSEHOLD ROSTER, GENERAL CHARACTERISTICS OF HOUSEHOLD MEMBERS BELONGING TO HOUSEHOLD
+# VARHS 
+
+##  SECTION 1A. HOUSEHOLD ROSTER, GENERAL CHARACTERISTICS OF HOUSEHOLD MEMBERS BELONGING TO HOUSEHOLD
 varhs1_08 <- read_dta(file = "2008_new/Q1a_New.dta")
 varhs1_10 <- read_dta(file = "2010_new/Q1_New_10.dta")
 varhs1_12 <- read_dta(file = "2012_new/Q1_New_12.dta")
 varhs1_14 <- read_dta(file = "2014_new/Q1_New_14.dta")
 varhs1_16 <- read_dta(file = "2016_new/Q1_New_16.dta")
+
+## SECTION 5. OCCUPATION, TIME USE, AND OTHER SOURCES OF INCOME
+
+varhs5_08 <- read_dta(file = "2008_new/Q5_New.dta")
+varhs5_10 <- read_dta(file = "2010_new/Q5_New_10.dta")
+varhs5_10 <- read_dta(file = "2010_new/Q5_New_10.dta")
+varhs5_12 <- read_dta(file = "2012_new/Q5_New_12.dta")
+varhs5_14 <- read_dta(file = "2014_new/Q5_New_14.dta")
+varhs5_16 <- read_dta(file = "2016_new/Q5_New_16.dta")
+
+## SECTION 5A. 5A Wage/salary employment (Fill in for all individuals who answered yes to Q1A in section 5A)
+
+varhs5a_08 <- read_dta(file = "2008_new/Q5a_New.dta")
+varhs5a_10 <- read_dta(file = "2010_new/Q5a_New_10.dta")
+varhs5a_10 <- read_dta(file = "2010_new/Q5a_New_10.dta")
+varhs5a_12 <- read_dta(file = "2012_new/Q5a_New_12.dta")
+varhs5a_14 <- read_dta(file = "2014_new/Q5a_New_14.dta")
+varhs5a_16 <- read_dta(file = "2016_new/Q5a_New_16.dta")
+
+## 10A GROUP MEMBERSHIP
+
+varhs10_08 <- read_dta(file = "2008_new/Q10a_New.dta")
+varhs10_10 <- read_dta(file = "2010_new/Q10a_New_10.dta")
+varhs10_10 <- read_dta(file = "2010_new/Q10a_New_10.dta")
+varhs10_12 <- read_dta(file = "2012_new/Q10a_New_12.dta")
+varhs10_14 <- read_dta(file = "2014_new/Q10a_New_14.dta")
+varhs10_16 <- read_dta(file = "2016_new/Q10a_New_16.dta")
 
 # LFS 
 
@@ -24,3 +53,8 @@ m1_06 <- vhlss06[[1]]
 m2a_06 <- vhlss06[[2]]
 m4a_06 <- vhlss06[[3]]
 weights_vhlss <- read_dta("hhinc06.dta")
+
+# Bombing data 
+
+bombs_district <- read_dta(file = "Corrigendum_Impact-bombing-Vietnam_Code-Data_2023-07-24/data/clean/district_bombing_corrected.dta")
+bombs_province <- read_dta(file = "Corrigendum_Impact-bombing-Vietnam_Code-Data_2023-07-24/data/clean/province_bombing_corrected.dta")
