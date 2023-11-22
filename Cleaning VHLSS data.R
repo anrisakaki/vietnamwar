5573df21fcae7b38b98f27e78853228966215d92
Anri Sakakibara <anrisakaki@gmail.com>
2023-11-22 16:47:14 +0000

    Cleaning VHLSS data

--- Cleaning VHLSS data.R
+++ Cleaning VHLSS data.R
@@ -14,10 +14,34 @@ m2a_06 <- m2a_06 %>%
   rename(educ = m2ac1) %>% 
   select(tinh, huyen, xa, diaban, hoso, matv, educ)
 
+m3_06 <- m3_06 %>% 
+  rename(wage_work = m3c1a,
+         agri_hh = m3c1b, 
+         nonagri_hh = m3c1c, 
+         work = m3c2,
+         work_for = m3c8) %>% 
+  mutate(wage_work = ifelse(wage_work == 1, 1, 0),
+         agri_hh = ifelse(agri_hh == 1, 1, 0),
+         nonagri_hh = ifelse(nonagri_hh == 1, 1, 0),
+         work = ifelse(work == 1, 1, 0)) %>% 
+  select(tinh02, huyen02, xa02, hoso02, matv02, wage_work, nonagri_hh, agri_hh, work, work_for) %>% 
+  rename_with(~ str_replace(.x, "02", ""), everything())
+
 m4a_06 <- m4a_06 %>% 
   rename(work = m4ac2) %>%
   mutate(income = m4ac11 +m4ac12f) %>% 
   select(tinh, huyen, xa, diaban, hoso, matv, work, income)
+
+m4ho_06 <- m4ho_06 %>% 
+  rename(ethnicity = ch_dantoc) %>%
+  select(tinh, xa, hoso, ethnicity) %>%
+  mutate(
+    tinh = as.numeric(substr(xa, 1, 3)),
+    huyen = as.numeric(substr(xa, 4, 5)),
+    xa = as.numeric(substr(xa, 6, 7)),
+    hoso = as.numeric(substr(hoso, nchar(hoso) - 1, nchar(hoso)))
+  ) %>% 
+  select(tinh, huyen, xa, hoso, ethnicity)
 
 weights_06 <- weights_vhlss %>%
   select(tinh, huyen, xa, wt45) %>% 
@@ -32,46 +56,24 @@ vhlss06 <- list(m1_06, m2a_06, m4a_06) %>%
   mutate(ivid = cur_group_id()) %>% 
   mutate(age = 2006 - birth_year)
 
+vhlss06 <- merge(vhlss06, m4ho_06, by = c("tinh", "huyen", "xa", "hoso"))
+
 # Adding weights 
 
 vhlss06 <- left_join(vhlss06, weights_06, by = c("tinh", "huyen", "xa"))
 
-# Creating district variable
-
-vhlss06 <- vhlss06 %>%
-  mutate(xa = sprintf("%02d", xa),
-          district = paste0(as.character(tinh), xa))
-
-hhinc06 <- weights_vhlss %>% 
-  mutate(xa = sprintf("%02d", xa),
-         district = paste0(as.character(tinh), xa),
-         tot_hhinc = rlincomepc * hhsize)  
-
-# Matching with Miguel and THOR bombing data 
-
-bombs_dist <- bombs_district %>% select(district, north_lat, east_long, south, tot_bmr, tot_bmr_per, districtname, provincename, urban) %>% 
-  mutate(district = as.character(district))
-bombs_prov <- bombs_province %>% select(province, tot_bmr, tot_bmr_per) %>% 
+# Matching with Miguel bombing data 
+bombs_prov <- bombs_province %>%
+  select(province, tot_bmr, tot_bmr_per, area_251_500m, area_501_1000m, area_over_1000m, log_popdensity6061, south) %>% 
   rename(tot_bmr_prov = tot_bmr,
          tinh = province,
          tot_bmr_per_prov = tot_bmr_per)
 
-vhlss06_bombs <- list(vhlss06, bombs_dist, thor_dist) %>% 
-  reduce(merge, by = "district") %>% 
-  mutate(tot_bmr = ifelse(is.na(tot_bmr), 0, tot_bmr),
-         tot_bombs = ifelse(is.na(tot_bombs), 0, tot_bmr))
+vhlss06_bombs <- list(vhlss06, bombs_prov) %>% 
+  reduce(merge, by = "tinh") %>% 
+  mutate(war_time = ifelse(birth_year > 1965 & birth_year < 1976, 1, 0),
+         exposed = ifelse(birth_year > 1959 & birth_year < 1971, 1, 0),
+         work = ifelse(work == 1, 1, 0))  
 
-vhlss06_bombs <- list(vhlss06_bombs, bombs_prov, thor_prov) %>% 
+hhinc06_bombs <- list(hhinc06_bombs, bombs_prov) %>% 
   reduce(merge, by = "tinh")
-
-hhinc06_bombs <- list(hhinc06, bombs_dist, thor_dist) %>% 
-  reduce(merge, by = "district") %>%   
-  select(-"urban.y") %>% 
-  rename(urban = urban.x)
-
-hhinc06_bombs <- list(hhinc06_bombs, bombs_prov, thor_prov) %>% 
-  reduce(merge, by = "tinh")
-
-vhlss06_bombs <- vhlss06_bombs %>% 
-  mutate(war_time = ifelse(birth_year > 1959 & birth_year < 1976, 1, 0),
-         exposed = ifelse(birth_year > 1964 & birth_year < 1976, 1, 0))
