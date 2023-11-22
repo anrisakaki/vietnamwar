84a7de62963ba913f58a043497aa8088a0d3e298
Anri Sakakibara <anrisakaki@gmail.com>
2023-11-22 16:47:32 +0000

    VHLSS regressions

--- VHLSS Regressions.R
+++ VHLSS Regressions.R
@@ -1,14 +1,20 @@
-etable(feols(educ ~ i(as.factor(war_time), tot_bmr_per_prov) + female | birth_year, 
-             vhlss06_bombs,
-             weights = ~hhwt,
-             vcov = ~tinh))
+vhlss06_exposed <- vhlss06_bombs %>% 
+  filter(exposed == 1)
 
-feols(educ ~ tot_bmr_per_prov + female | birth_year, 
-      subset(vhlss06_bombs, war_time == 1),
-      weights = ~hhwt,
-      vcov = ~tinh)
+etable(feols(educ ~ tot_bmr_per_prov + female + as.factor(ethnicity) + log_popdensity6061 + as.factor(south) | birth_year,
+       subset(vhlss06_bombs, exposed == 1),
+       weights = ~hhwt,
+       vcov = ~tinh))
 
-feols(educ ~ i(as.factor(female), tot_bmr_per_prov) | birth_year, 
-      subset(vhlss06_bombs, war_time == 1),
-      weights = ~hhwt,
-      vcov = ~tinh)
+iplot(feols(
+  educ ~ i(as.factor(exposed), tot_bmr_per_prov) + as.factor(ethnicity) + as.factor(female) | birth_year + tinh,
+  subset(vhlss06_bombs, birth_year > 1959 & birth_year < 1980),
+  weights = ~hhwt,
+  vcov = ~tinh))
+
+
+iplot(feols(educ ~ i(as.factor(female), tot_bmr_per_prov) + as.factor(ethnicity)| birth_year^tinh, 
+            subset(vhlss06_bombs, birth_year > 1959 & birth_year < 1980),
+            weights = ~hhwt,
+            vcov = ~tinh), main = ""
+)
