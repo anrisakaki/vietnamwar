6076a1b7f55eead82868abbb438e0d004135c632
Anri Sakakibara <anrisakaki@gmail.com>
2023-11-22 16:47:01 +0000

    Cleaning population data

--- Cleaning population data.R
+++ Cleaning population data.R
@@ -6,4 +6,5 @@ ppn_total <- ppn %>%
 male_prewarppn <- male_prewarppn %>% 
   select(-X) %>%
   pivot_longer(!Tinh, names_to = "Year", values_to = "Male_ppn") %>%
-  mutate(Year = as.numeric(str_extract(Year, "\\d+")))
+  mutate(Year = as.numeric(str_extract(Year, "\\d+")),
+         Male_ppn = as.numeric(gsub(",", "", Male_ppn)))
