4a472d4e92a8076204ac49ba5a715be8781d9bc1
Anri Sakakibara <anrisakaki@gmail.com>
2024-01-08 15:06:09 +0000

    PHC regressions

--- PHC Regressions.R
+++ PHC Regressions.R
@@ -1,12 +1,22 @@
+setFixest_coefplot(zero.par = list( type="dotted", lty=2))
+
 # 1989
 
-iplot(feols(work ~ i(birthyr) + as.factor(minority) + yrschool + as.factor(geo1_vn) + nchild + married,
-            subset(phc89, female == 1 & birthyr > 1925 & birthyr < 1974),
+iplot(feols(work ~ i(birthyr) + as.factor(minority) + yrschool + nchild + married | geo1_vn,
+            subset(phc89, female == 1 & birthyr > 1925 & birthyr < 1974 & migration == 0),
             weights = ~perwt), xlab = "Birth Year", main = "")
 
 iplot(feols(work ~ i(birthyr) + as.factor(minority) + yrschool + as.factor(geo1_vn) + nchild + married,
             subset(phc89, female == 0 & birthyr > 1925 & birthyr < 1974),
             weights = ~perwt), xlab = "Birth Year", main = "")
+
+iplot(feols(work ~ i(age75, log_tot_bomb) + as.factor(minority) + yrschool + nchild + married,
+            subset(phc89, female == 1 & birthyr > 1925 & birthyr < 1974 & migration == 0),
+            weights = ~perwt), xlab = "Age in 1975", main = "")
+
+iplot(feols(work ~ i(age75, log_tot_bomb) + as.factor(minority) + yrschool  + nchild + married,
+            subset(phc89, female == 0 & birthyr > 1925 & birthyr < 1974 & migration == 0),
+            weights = ~perwt), xlab = "Age in 1975", main = "")
 
 # 1999
 
@@ -17,3 +27,29 @@ iplot(feols(work ~ i(birthyr) + as.factor(minority) + yrschool + as.factor(geo1_
 iplot(feols(work ~ i(birthyr) + as.factor(minority) + yrschool + as.factor(geo1_vn) + nchild + married,
             subset(phc99, female == 0 & birthyr > 1935 & birthyr < 1984),
             weights = ~perwt), xlab = "Birth Year", main = "")
+
+iplot(feols(work ~ i(age75, log_tot_bomb) + as.factor(minority) + yrschool  + nchild + married,
+            subset(phc89, female == 1 & birthyr > 1935 & birthyr < 1984),
+            weights = ~perwt), xlab = "Age in 1975", main = "")
+
+iplot(feols(work ~ i(age75, log_tot_bomb) + as.factor(minority) + yrschool  + nchild + married,
+            subset(phc89, female == 0 & birthyr > 1935 & birthyr < 1984),
+            weights = ~perwt), xlab = "Age in 1975", main = "")
+
+# 2009
+
+iplot(feols(work ~ i(birthyr) + as.factor(minority) + yrschool + as.factor(geo1_vn) + nchild + married,
+            subset(phc99, female == 1 & birthyr > 1945 & birthyr < 1994),
+            weights = ~perwt), xlab = "Birth Year", main = "")
+
+iplot(feols(work ~ i(birthyr) + as.factor(minority) + yrschool + as.factor(geo1_vn) + nchild + married,
+            subset(phc99, female == 0 & birthyr > 1945 & birthyr < 1994),
+            weights = ~perwt), xlab = "Birth Year", main = "")
+
+iplot(feols(work ~ i(age75, log_tot_bomb) + as.factor(minority) + yrschool  + nchild + married,
+            subset(phc89, female == 1 & birthyr > 1945 & birthyr < 1994),
+            weights = ~perwt), xlab = "Age in 1975", main = "")
+
+iplot(feols(work ~ i(age75, log_tot_bomb) + as.factor(minority) + yrschool  + nchild + married,
+            subset(phc89, female == 0 & birthyr > 1945 & birthyr < 1994),
+            weights = ~perwt), xlab = "Age in 1975", main = "")
