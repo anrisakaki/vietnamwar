ac98ab86ea03c3ca273d009390ed17cc7d130d5e
Anri Sakakibara <anrisakaki@gmail.com>
2024-01-22 11:01:00 +0000

    PHC Regressions

--- PHC Regressions.R
+++ PHC Regressions.R
@@ -10,12 +10,24 @@ iplot(feols(work ~ i(birthyr) + as.factor(minority) + yrschool + as.factor(geo1_
             subset(phc89, female == 0 & birthyr > 1925 & birthyr < 1974),
             weights = ~perwt), xlab = "Birth Year", main = "")
 
-iplot(feols(work ~ i(age75, log_tot_bomb) + as.factor(minority) + yrschool + nchild + married,
+etable(feols(yrschool ~ log_tot_bmr_per + as.factor(minority) + nchild + married | birthyr,
+            subset(phc89, female == 0 & birthyr > 1925 & birthyr < 1974),
+            vcov = ~geo1_vn,
+            weights = ~perwt))
+
+iplot(feols(work ~ i(age, log_tot_bmr_per) + as.factor(minority) + yrschool + nchild + married | geo1_vn1989,
             subset(phc89, female == 1 & birthyr > 1925 & birthyr < 1974 & migration == 0),
+            vcov = ~geo1_vn1989,
+            weights = ~perwt), xlab = "Age", main = "")
+
+iplot(feols(work ~ i(age75, log_tot_bmr_per) + as.factor(minority) + yrschool + nchild + married | geo1_vn1989,
+            subset(phc89, female == 1 & birthyr > 1925 & birthyr < 1974 & migration == 0),
+            vcov = ~geo1_vn1989,
             weights = ~perwt), xlab = "Age in 1975", main = "")
 
-iplot(feols(work ~ i(age75, log_tot_bomb) + as.factor(minority) + yrschool  + nchild + married,
+iplot(feols(work ~ i(age75, log_tot_bmr_per) + as.factor(minority) + yrschool + nchild + married | geo1_vn1989,
             subset(phc89, female == 0 & birthyr > 1925 & birthyr < 1974 & migration == 0),
+            vcov = ~geo1_vn1989,
             weights = ~perwt), xlab = "Age in 1975", main = "")
 
 # 1999
