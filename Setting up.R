7e8bfdba4d8ae4e2a772f8df5094743d9b3b19f7
Anri Sakakibara <anrisakaki@gmail.com>
2024-01-22 11:01:06 +0000

    Setting up

--- Setting up.R
+++ Setting up.R
@@ -6,6 +6,7 @@ library(fixest)
 library(extrafont)
 library(lubridate)
 library(sjPlot)
+library(r-vietnam-map)
 
 rm(list=ls())
 
@@ -30,18 +31,6 @@ ppn79  <- read.csv("Male and Female Population 1979.csv")
 
 LFS_2015 <- read_dta(file = "LFS_2015_final_full.dta")
 
-# VHLSS 
-
-vhlss06 <- list.files(pattern = "muc(.*)dta$")
-vhlss06 <- lapply(vhlss06, read_dta)
-
-m1_06 <- vhlss06[[1]]
-m2a_06 <- vhlss06[[2]]
-m3_06 <- vhlss06[[3]]
-m4a_06 <- vhlss06[[4]]
-m4ho_06 <- vhlss06[[5]] 
-weights_vhlss <- read_dta("hhinc06.dta")
-
 # Bombing data 
 
 bombs_district <- read_dta(file = "Corrigendum_Impact-bombing-Vietnam_Code-Data_2023-07-24/data/clean/district_bombing_corrected.dta")
@@ -63,16 +52,9 @@ vnmap1 <- read_sf("Chloropleth Maps/VNShapefile/gadm36_VNM_1.shp")
 vnmap2 <- read_sf("Chloropleth Maps/VNShapefile/gadm36_VNM_2.shp")
 vnmap3 <- read_sf("Chloropleth Maps/VNShapefile/gadm36_VNM_3.shp")
 
+provarea <- read.csv("province_area.csv")
+
 district <- read_dta(file = "Consistent 2019 to 1999 wards with 1999 districts.dta")
-
-# schools 
-
-educ <- list.files(pattern = "educ_(.*)csv$")
-educ <- lapply(educ, read.csv)
-
-educ_65 <- educ[[1]]
-educ_67 <- educ[[2]]
-educ_72 <- educ[[3]]
 
  # Consistent District Boundaries 
 
