90e3e8912f12079feaab553e9613b1a1ab81de89
Anri Sakakibara <anrisakaki@gmail.com>
2024-01-29 21:35:10 +0000

    Cleaning casualties data

--- Cleaning casualties data.R
+++ Cleaning casualties data.R
@@ -1,21 +1,48 @@
 # Cleaning casualties data 
-
-casualties_selected <- lapply(casualties_list, select_variables)
-
-select_variables <- function(df) {
-  df_selected <- df %>%
+process_dataframe <- function(df) {
+  df <- df %>%
+    mutate(ID = substr(UTM.MAP.COORDINATE, 1, 2)) %>%
     select(
       MAJOR.PROVINCE.CODE,
       NUMBER.DESTROYED.OF.KILLED,
       NUMBER.DAMAGED.OR.WOUNDED,
       NUMBER.CAPTURED.OR.MISSING,
       LOSS.NATIONALITY,
-      UTM.MAP.COORDINATE
+      UTM.MAP.COORDINATE,
+      ID
     )
-  return(df_selected)
+  
+  df <- left_join(df, zones, by = "ID")
+  
+  return(df)
 }
 
+casualties <- lapply(casualties_list, process_dataframe)
+
+casualties_sf_fn <- function(df) {
+  df <- df %>%
+    filter(!is.na(Zone),
+           !grepl("000000", UTM.MAP.COORDINATE)) %>%
+    mutate(MGRS = paste0(Zone, UTM.MAP.COORDINATE))
+  
+  return(df)
+}
+
+casualties_sf <- lapply(casualties, casualties_sf_fn)
+
+casualties_sf[[1]] <- st_as_sf(casualties_sf[[1]], coords = "MGRS")
+
 combined_casualties <- do.call(rbind, casualties_selected[1:44])
+
+combined_casualties <- combined_casualties %>% 
+  mutate(ID = substr(UTM.MAP.COORDINATE, 1, 2))
+
+combined_casualties <- left_join(combined_casualties, zones, by = "ID") %>% 
+  filter(!is.na(Zone),
+         !grepl("000000", UTM.MAP.COORDINATE)) %>% 
+  mutate(MGRS = paste0(Zone, UTM.MAP.COORDINATE))
+
+combined_casualties_sf <- st_as_sf(combined_casualties, coords = "UTM.MAP.COORDINATE")
 
 prov_casualties <- combined_casualties %>% 
   mutate(
@@ -30,8 +57,6 @@ prov_casualties <- combined_casualties %>%
             wounded_tot = sum(NUMBER.DAMAGED.OR.WOUNDED),
             missing_tot = sum(NUMBER.CAPTURED.OR.MISSING))
 
-cl_provcodes <- cl_provcodes %>% rename(MAJOR.PROVINCE.CODE = Code) 
-cl_provcodes$MAJOR.PROVINCE.CODE <- as.character(cl_provcodes$MAJOR.PROVINCE.CODE)
   
 prov_casualties <- left_join(prov_casualties, cl_provcodes, by = "MAJOR.PROVINCE.CODE") %>% 
   mutate(Meaning = ifelse(is.na(Meaning), MAJOR.PROVINCE.CODE, Meaning)) %>% 
@@ -39,7 +64,8 @@ prov_casualties <- left_join(prov_casualties, cl_provcodes, by = "MAJOR.PROVINCE
     varname_1 = case_when(
       MAJOR.PROVINCE.CODE == 'An Xuyen' ~ 'Ca Mau',
       MAJOR.PROVINCE.CODE == 'BA Xuyen' ~ 'Soc Trang',
-      MAJOR.PROVINCE.CODE == 'BA Xuyen' ~ 'Soc Trang',
+      MAJOR.PROVINCE.CODE == 'Bien Hoa' ~ 'Dong Nai',
+      MAJOR.PROVINCE.CODE == 'Binh Long' ~ 'Dong Nai',
       TRUE ~ MAJOR.PROVINCE.CODE
     )
   )
