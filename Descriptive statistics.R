852215eda8da99976d3f76866ce3ae8735bc4f00
Anri Sakakibara <anrisakaki@gmail.com>
2024-02-08 11:08:03 +0000

    Descriptive statistics

--- Descriptive statistics.R
+++ Descriptive statistics.R
@@ -12,6 +12,56 @@ ggplot(province_bombs_sf) +
   ggtitle("")
 ggsave("province_bombs_sf.jpeg", width = 7, height = 7)
 
+
+ggplot(province_bombs_sf) + 
+  geom_sf(aes(fill = log(deaths_per))) +
+  scale_fill_gradient(name = expression(log(Casualties~per~Km^2)), low = "green", high = "red", na.value = "grey") + 
+  theme(axis.text.x = element_blank(),
+        axis.text.y = element_blank(),
+        axis.ticks = element_blank(),
+        axis.title.y=element_blank(),
+        axis.title.x=element_blank(),
+        panel.background = element_blank()) +
+  ggtitle("")
+ggsave("province_casualties_sf.jpeg", width = 7, height = 7)
+
+
+# Casualties versus 
+# 1989
+ggplot(dplyr::filter(sexratio_prov_89, tot_casualties_per > 0), aes(x = log(tot_bomb_per), y = log(tot_casualties_per))) +
+  geom_point() +
+  geom_smooth(method = "lm",
+              se = T) +
+  theme_minimal() +
+  guides(fill = "none") +  
+  theme(axis.line = element_line(color='black'),
+        plot.background = element_blank(),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank(),
+        legend.title=element_blank(),
+        text = element_text(size=10)) + 
+  labs(x = expression(log(Bombs~per~Km^2)),
+       y = expression(log(Casualties~per~Km^2)))
+ggsave("bombs_casualties.jpeg", width = 7, height = 7)
+
+ggplot(sexratio_prov_89, aes(x = log(tot_casualties_per), y = sexratio*100)) +
+  geom_point() +
+  geom_smooth(method = "lm",
+              se = T) +
+  theme_minimal() +
+  guides(fill = "none") +  
+  theme(axis.line = element_line(color='black'),
+        plot.background = element_blank(),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank(),
+        legend.title=element_blank(),
+        text = element_text(size=10)) + 
+  labs(x = expression(log(Casualties~per~Km^2)),
+       y = "Province-level sex ratio in 1989")
+ggsave("casualties_sexratio89.jpeg", width = 7, height = 7)
+
 # Bombing intensity versus 
 
 # 1989
