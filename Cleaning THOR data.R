e774110ad0d76afebe19b831f77ede023876c2a8
Anri Sakakibara <anrisakaki@gmail.com>
2024-02-26 19:42:27 +0000

    Cleaning THOR data

--- Cleaning THOR data.R
+++ Cleaning THOR data.R
@@ -5,10 +5,9 @@ load("prov_casualties.Rda")
 ######################
 
 thor <- thor %>% 
-  filter(!(SOURCERECORD == "SACCOACT" & MSNDATE >= 19710301)) %>% 
+  filter(!(SOURCERECORD == "SACCOACT" & MSNDATE >= 19710301)) %>%
   mutate(MSNDATE = ifelse(MSNDATE == "1970-02-29", "1970-03-01", MSNDATE),
          MFUNC_DESC_CLASS = ifelse(MFUNC_DESC == "COMBT CARGO AIR  DROP", "KINETIC", MFUNC_DESC_CLASS)) %>%
-  filter(!(SOURCERECORD == "SACCOACT" & MSNDATE >= 19710301)) %>%
   mutate(MSNDATE = as.Date(MSNDATE),
          year = as.integer(format(MSNDATE, "%Y")),
          month = as.integer(format(MSNDATE, "%m")),
@@ -16,13 +15,14 @@ thor <- thor %>%
          TGTTYPE = ifelse(TGTTYPE == "", NA, TGTTYPE),
          MFUNC_DESC = ifelse(MFUNC_DESC == "", NA, MFUNC_DESC),
          MFUNC_DESC_CLASS = ifelse(MFUNC_DESC == "COMBT CARGO AIR  DROP", "KINETIC", MFUNC_DESC_CLASS),
-         MFUNC_DESC = ifelse(MFUNC_DESC == "COMBT CARGO AIR  DROP", "HEAVY BOMBARD", MFUNC_DESC)) %>% 
+         MFUNC_DESC = ifelse(MFUNC_DESC == "COMBT CARGO AIR  DROP", "HEAVY BOMBARD", MFUNC_DESC),
+         WEAPONSLOADEDWEIGHT = ifelse(WEAPONSLOADEDWEIGHT == 0, NA, WEAPONSLOADEDWEIGHT),
+         WEAPONSLOADEDWEIGHT = ifelse(WEAPONSLOADEDWEIGHT == "-1", NA, WEAPONSLOADEDWEIGHT)) %>% 
   filter(TGTCOUNTRY == "NORTH VIETNAM" | TGTCOUNTRY == "SOUTH VIETNAM") %>% 
   filter(!is.na(TGTLATDD_DDD_WGS84),
          !is.na(TGTLONDDD_DDD_WGS84))
 
-weapons_dict <- weapons_dict %>% 
-  select(-c("WEAPON_ID", "WEAPON_COUNT"))
+weapons_dict <- weapons_dict %>% select(-c(WEAPON_COUNT))
 
 thor <- left_join(thor, weapons_dict, by = "WEAPONTYPE")
 
@@ -30,29 +30,36 @@ thor <- thor %>%
   filter(WEAPON_CLASS != "SUPPORT",
          WEAPON_CLASS != "GUN",
          MFUNC_DESC_CLASS == "KINETIC") %>% 
+  # Removing rocket and missIle launchers to prevent double counting 
+  filter(!grepl("AGM|AIM|LAU|2.75", WEAPONTYPE),
+         !grepl("PAMPHLET", WEAPONTYPE_DESC)) %>% 
   mutate(
-    dualuse = case_when(
-      TGTTYPE %in% c("AGRICULTURAL AREA", "BRIDGE", "BUILDINGS", "CIV POPULATN CENTR", 
-                     "CONSTRUCTION SITE", "FACTORY INDUSTRIAL", "FACTORY,ANY", "ELEC. PWR. FAC.", 
-                     "FERRY", "FERRY CROSSING", "HUTS", "LIGHTS ON ROAD", "PIER", "PLANTS", 
-                     "TUNNEL", "TUNNELS", "VILLAGE") ~ 1,
-      grepl("RAILROAD|BRIDGE|ROAD", TGTTYPE, ignore.case = TRUE) ~ 1,
-      TRUE ~ 0
-    ),
     civilian = case_when(
-      TGTTYPE %in% c("AGRICULTURAL AREA", "CIV POPULATN CENTR", "VILLAGE") ~ 1,
+      TGTTYPE %in% c("AGRICULTURAL AREA", "CIV POPULATN CENTR", "VILLAGE", "BUILDINGS", "HUTS") ~ 1,
+      grepl("PERSONNEL", TGTTYPE, ignore.case = TRUE) ~ 1,
       TRUE ~ 0 
     ),
+    air = case_when(
+      grepl("AIR", MFUNC_DESC, ignore.case = TRUE) ~ 1,
+      TRUE ~ 0
+    ),
+    civilian_ex_per = case_when(
+      TGTTYPE %in% c("AGRICULTURAL AREA", "CIV POPULATN CENTR", "VILLAGE", "BUILDINGS", "HUTS") ~ 1,
+      TRUE ~ 0 
+    ),    
     industry = case_when(
       TGTTYPE %in% c("CONSTRUCTION SITE", "FACTORY INDUSTRIAL", "FACTORY,ANY", "ELEC. PWR. FAC.") ~ 1,
       TRUE ~ 0
     ),
     infrastructure = case_when(
       TGTTYPE %in% c("BRIDGE", "FERRY", "FERRY CROSSING", "PIER",
-                     "TUNNEL", "TUNNELS", "VILLAGE") ~ 1,
+                     "TUNNEL", "TUNNELS") ~ 1,
       grepl("RAILROAD|BRIDGE|ROAD", TGTTYPE, ignore.case = TRUE) ~ 1,
       TRUE ~ 0
-  ))
+  ),
+  WEIGHTDELIVERED = ifelse(is.na(WEAPONSLOADEDWEIGHT), NUMWEAPONSDELIVERED*WEAPONWEIGHT, WEAPONSLOADEDWEIGHT/10)) %>% 
+  # Max ordnance weight of B52 was 70,000 lb 
+  filter(WEIGHTDELIVERED <= 70000)
 
 # Convert to a GeoDataFrame
 gdf <- st_as_sf(thor, coords = c("TGTLONDDD_DDD_WGS84", "TGTLATDD_DDD_WGS84"), crs = 4326)
@@ -71,27 +78,16 @@ province_bombs_sum <- province_bombs %>%
   ungroup() %>% 
   filter(!is.na(varname_1)) %>% 
   group_by(varname_1, name_1) %>% 
-  summarise(tot_bmr = sum(numweaponsdelivered)) %>% 
+  summarise(tot_bmr = sum(numweaponsdelivered),
+            tot_bmr_lb = sum(weightdelivered)) %>% 
   ungroup()
 province_bombs_sum <- sf::st_drop_geometry(province_bombs_sum)
 
-province_mfunc_sum <- province_bombs %>% 
+province_civilian_ex_per_sum <- province_bombs %>% 
   ungroup() %>% 
-  mutate(mfunc_desc = ifelse(mfunc_desc == "DIR AIR SUPPT", "DIRECT AIR SUPPORT", mfunc_desc)) %>% 
-  filter(!is.na(varname_1),
-         mfunc_desc %in% c("STRIKE", "DIRECT AIR SUPPORT", "HEAVY BOMBARD", "CLOSE AIR SUPPORT", "AIR INTERDICTION") == T) %>% 
-  group_by(varname_1, name_1, mfunc_desc) %>% 
-  summarise(tot_bmr = sum(numweaponsdelivered)) %>% 
-  ungroup()
-province_mfunc_sum <- sf::st_drop_geometry(province_mfunc_sum)
-province_mfunc_sum <- province_mfunc_sum %>% 
-  pivot_wider(names_from = "mfunc_desc", values_from = "tot_bmr", values_fill = 0)
-
-province_dualuse_sum <- province_bombs %>% 
-  ungroup() %>% 
-  filter(dualuse == 1) %>% 
+  filter(civilian_ex_per == 1) %>% 
   group_by(varname_1, name_1) %>% 
-  summarise(tot_dualuse = sum(numweaponsdelivered)) %>% 
+  summarise(tot_civilian_ex_per = sum(numweaponsdelivered)) %>% 
   ungroup() %>% 
   filter(!is.na(varname_1))
 province_dualuse_sum <- sf::st_drop_geometry(province_dualuse_sum)
