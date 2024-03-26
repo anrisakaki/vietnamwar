4d03328b8ceb3324938d451e2ef63db1638b89c7
Anri Sakakibara <anrisakaki@gmail.com>
2024-03-26 17:46:44 +0000

    PHC Regressions

--- PHC Regressions.R
+++ PHC Regressions.R
@@ -1,91 +1,52 @@
-phc <- c("phc.Rda", "phc89.Rda", "phc99.Rda", "phc09.Rda")
+phc <- c("phc89.Rda", "phc99.Rda", "phc09.Rda")
 
 for (i in phc) {
   load(i)
 }
 
-dict <- c("age_cohort" = "Age Cohort $\times$ log(Bombs/km^2)")
-setFixest_coefplot(dict = dict, grid = F, zero.par = list( type="dotted", lty=2), main = "")
+# Individual-level regressions 
 
 etable(list(
-  feols(work ~ log(tot_bomb)/as.factor(south)/as.factor(female) + yrschool + nchild + married + as.factor(minority) + age | regnvn,
-        subset(phc89, birthyr > 1925 & birthyr < 1974 & migration == 0),
-        weights = ~perwt,
+  feols(work ~ as.factor(female)/log(tot_bomb) + yrschool + nchild + married + as.factor(minority) + age + age^2 | regnvn,
+        phc89,
         vcov = ~geo1_vn1989),
-  feols(work ~ log(tot_bomb)/as.factor(south)/as.factor(female) + yrschool + nchild + married + as.factor(minority) + age | regnvn,
-        subset(phc99, birthyr > 1935 & birthyr < 1984 & migration == 0),
-        weights = ~perwt,
+  feols(work ~ as.factor(female)/log(tot_bomb) + yrschool + nchild + married + as.factor(minority) + age + age^2 | regnvn,
+        phc99,
         vcov = ~geo1_vn1999),
-  feols(work ~ log(tot_bomb)/as.factor(south)/as.factor(female) + yrschool + nchild + married + as.factor(minority) + age | regnvn,
-        subset(phc09, birthyr > 1945 & birthyr < 1994 & migration == 0),
-        weights = ~perwt,
-        vcov = ~geo1_vn2009)), tex = T)
+  feols(work ~ as.factor(female)/log(tot_bomb) + yrschool + nchild + married + as.factor(minority) + age + age^2| regnvn,
+        phc09,
+        vcov = ~geo1_vn2009)
+), tex = T)
 
 etable(list(
-  feols(work ~ as.factor(female)/log(tot_infrastructure) + yrschool + nchild + married + as.factor(minority) + age | regnvn,
-        subset(phc89, birthyr > 1925 & birthyr < 1974 & migration == 0),
-        weights = ~perwt,
+  feols(work ~ as.factor(female)/log(tot_bomb) + yrschool + nchild + married + as.factor(minority) + age + age^2 | regnvn,
+        subset(phc89, south == 0),
         vcov = ~geo1_vn1989),
-  feols(work ~ as.factor(female)/log(tot_infrastructure) + yrschool + nchild + married + as.factor(minority) + age | regnvn,
-        subset(phc99, birthyr > 1935 & birthyr < 1984 & migration == 0),
-        weights = ~perwt,
+  feols(work ~ as.factor(female)/log(tot_bomb) + yrschool + nchild + married + as.factor(minority) + age + age^2 | regnvn,
+        subset(phc99, south == 0),
         vcov = ~geo1_vn1999),
-  feols(work ~ as.factor(female)/log(tot_infrastructure) + yrschool + nchild + married + as.factor(minority) + age | regnvn,
-        subset(phc09, birthyr > 1945 & birthyr < 1994 & migration == 0),
-        weights = ~perwt,
-        vcov = ~geo1_vn2009)), tex = T)
+  feols(work ~ as.factor(female)/log(tot_bomb) + yrschool + nchild + married + as.factor(minority) + age + age^2| regnvn,
+        subset(phc09, south == 0),
+        vcov = ~geo1_vn2009),
+  feols(work ~ as.factor(female)/log(tot_bomb) + yrschool + nchild + married + as.factor(minority) + age + age^2 | regnvn,
+        subset(phc89, south == 1),
+        vcov = ~geo1_vn1989),
+  feols(work ~ as.factor(female)/log(tot_bomb) + yrschool + nchild + married + as.factor(minority) + age + age^2 | regnvn,
+        subset(phc99, south == 1),
+        vcov = ~geo1_vn1999),
+  feols(work ~ as.factor(female)/log(tot_bomb) + yrschool + nchild + married + as.factor(minority) + age + age^2| regnvn,
+        subset(phc09, south == 1),
+        vcov = ~geo1_vn2009)
+), tex = T)
 
 etable(list(
-  feols(work ~ as.factor(female)/log(tot_infrastructure) + yrschool + nchild + married + as.factor(minority) + age | regnvn,
-        subset(phc89, birthyr > 1925 & birthyr < 1974 & migration == 0),
-        weights = ~perwt,
+  feols(work ~ as.factor(female)/log(tot_killed) + yrschool + nchild + married + as.factor(minority) + age + age^2 | regnvn,
+        phc89,
         vcov = ~geo1_vn1989),
-  feols(work ~ as.factor(female)/log(tot_infrastructure) + yrschool + nchild + married + as.factor(minority) + age | regnvn,
-        subset(phc99, birthyr > 1935 & birthyr < 1984 & migration == 0),
-        weights = ~perwt,
+  feols(work ~ as.factor(female)/log(tot_killed) + yrschool + nchild + married + as.factor(minority) + age + age^2 | regnvn,
+        phc99,
         vcov = ~geo1_vn1999),
-  feols(work ~ as.factor(female)/log(tot_infrastructure) + yrschool + nchild + married + as.factor(minority) + age | regnvn,
-        subset(phc09, birthyr > 1945 & birthyr < 1994 & migration == 0),
-        weights = ~perwt,
-        vcov = ~geo1_vn2009)), tex = T)
-
-etable(list(
-  feols(work ~ as.factor(female)/log(tot_bomb) + yrschool + nchild + married + as.factor(minority) + age | regnvn,
-        subset(phc89, birthyr > 1925 & birthyr < 1974 & migration == 0),
-        weights = ~perwt,
-        vcov = ~geo1_vn1989),
-  feols(work ~ as.factor(female)/log(tot_bomb) + yrschool + nchild + married + as.factor(minority) + age | regnvn,
-        subset(phc99, birthyr > 1935 & birthyr < 1984 & migration == 0),
-        weights = ~perwt,
-        vcov = ~geo1_vn1999),
-  feols(work ~ as.factor(female)/log(tot_bomb) + yrschool + nchild + married + as.factor(minority) + age | regnvn,
-        subset(phc09, birthyr > 1945 & birthyr < 1994 & migration == 0),
-        weights = ~perwt,
-        vcov = ~geo1_vn2009)), tex = T)
-
-
-# 1989
-
-png("flfp_bombs_89.png")
-iplot(feols(work ~ i(age_cohort, tot_killed) + yrschool + nchild + married + as.factor(minority) | geo1_vn1989 + age_cohort,
-            subset(phc89, female == 1 & birthyr > 1925 & birthyr < 1974 & migration == 0),
-            weights = ~perwt,
-            vcov = ~geo1_vn1989), main = " ", xlab = "")
-dev.off()
-
-# 1999
-png("flfp_bombs_99.png")
-iplot(feols(work ~ i(age_cohort, log(tot_bomb_per)) + yrschool + nchild + married + as.factor(minority) | geo1_vn1999,
-            subset(phc99, female == 1 & birthyr > 1935 & birthyr < 1984 & migration == 0),
-            weights = ~perwt,
-            vcov = ~geo1_vn1999), main = " ")
-dev.off()
-
-# 2009
-
-png("flfp_bombs_09.png")
-iplot(feols(work ~ i(age_cohort, log(tot_bomb_per)) + yrschool + nchild + married + as.factor(minority) | geo1_vn2009,
-            subset(phc09, female == 1 & birthyr > 1945 & birthyr < 1994 & migration == 0),
-            weights = ~perwt,
-            vcov = ~geo1_vn2009), main = " ")
-dev.off()
+  feols(work ~ as.factor(female)/log(tot_killed) + yrschool + nchild + married + as.factor(minority) + age + age^2| regnvn,
+        phc09,
+        vcov = ~geo1_vn2009)
+), tex = T)
