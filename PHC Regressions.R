ebb879e0019cf55f8d6916942ee5b9132fc052d7
Anri Sakakibara <anrisakaki@gmail.com>
2024-04-05 14:29:05 +0100

    PHC regressions

--- PHC Regressions.R
+++ PHC Regressions.R
@@ -4,7 +4,179 @@ for (i in phc) {
   load(i)
 }
 
-# Individual-level regressions 
+dn <- c("dn_prov.Rda", "dn_dist.Rda")
+
+for (i in dn) {
+  load(i)
+}
+
+# Enterprise Census regressions 
+
+## District level 
+
+dn_dist_femodels <- (list(
+  feols(workerratio ~ log(tot_bmr) | prov2018,
+        subset(dn_dist, year == 2000),
+        vcov = ~dist2018),
+  feols(workerratio ~ log(tot_bmr) | prov2018,
+        subset(dn_dist, year == 2001),
+        vcov = ~dist2018),
+  feols(workerratio ~ log(tot_bmr) | prov2018,
+        subset(dn_dist, year == 2002),
+        vcov = ~dist2018),
+  feols(workerratio ~ log(tot_bmr) | prov2018,
+        subset(dn_dist, year == 2003),
+        vcov = ~dist2018),
+  feols(workerratio ~ log(tot_bmr) | prov2018,
+        subset(dn_dist, year == 2004),
+        vcov = ~dist2018),
+  feols(workerratio ~ log(tot_bmr) | prov2018,
+        subset(dn_dist, year == 2005),
+        vcov = ~dist2018),
+  feols(workerratio ~ log(tot_bmr) | prov2018,
+        subset(dn_dist, year == 2006),
+        vcov = ~dist2018),
+  feols(workerratio ~ log(tot_bmr) | prov2018,
+        subset(dn_dist, year == 2007),
+        vcov = ~dist2018),
+  feols(workerratio ~ log(tot_bmr) | prov2018,
+        subset(dn_dist, year == 2008),
+        vcov = ~dist2018),
+  feols(workerratio ~ log(tot_bmr) | prov2018,
+        subset(dn_dist, year == 2009),
+        vcov = ~dist2018),
+  feols(workerratio ~ log(tot_bmr) | prov2018,
+        subset(dn_dist, year == 2010),
+        vcov = ~dist2018),
+  feols(workerratio ~ log(tot_bmr) | prov2018,
+        subset(dn_dist, year == 2011),
+        vcov = ~dist2018),
+  feols(workerratio ~ log(tot_bmr) | prov2018,
+        subset(dn_dist, year == 2012),
+        vcov = ~dist2018),
+  feols(workerratio ~ log(tot_bmr) | prov2018,
+        subset(dn_dist, year == 2013),
+        vcov = ~dist2018),
+  feols(workerratio ~ log(tot_bmr) | prov2018,
+        subset(dn_dist, year == 2014),
+        vcov = ~dist2018),
+  feols(workerratio ~ log(tot_bmr) | prov2018,
+        subset(dn_dist, year == 2015),
+        vcov = ~dist2018),
+  feols(workerratio ~ log(tot_bmr) | prov2018,
+        subset(dn_dist, year == 2016),
+        vcov = ~dist2018),
+  feols(workerratio ~ log(tot_bmr) | prov2018,
+        subset(dn_dist, year == 2017),
+        vcov = ~dist2018),
+  feols(workerratio ~ log(tot_bmr) | prov2018,
+        subset(dn_dist, year == 2018),
+        vcov = ~dist2018)))
+
+dn_dist_fecoef <- lapply(dn_dist_femodels, tidy)
+dn_dist_fecoef_df <- do.call(rbind, dn_dist_fecoef)
+dn_dist_fecoef_df$year <- rep(seq(2000, 2018), each = nrow(dn_dist_fecoef_df) / length(seq(2000, 2018)))
+
+ggplot(dn_dist_fecoef_df, aes(x = factor(year), y = estimate, ymin = estimate - std.error, ymax = estimate + std.error)) +
+  geom_pointrange() +
+  geom_hline(yintercept = 0, linetype = "dotted", color = "black") +
+  labs(x = "Year", y = "Estimate and 95% Interval") +
+  ggtitle("") +
+  theme_minimal() +
+  guides(fill = "none") +  
+  theme(axis.line = element_line(color='black'),
+        plot.background = element_blank(),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank(),
+        legend.title=element_blank(),
+        text = element_text(size=10)) + 
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+
+# Province level 
+
+dn_prov_femodels <- (list(
+  feols(workerratio ~ log(tot_bmr),
+        subset(dn_prov, year == 2000),
+        vcov = ~prov2018),
+  feols(workerratio ~ log(tot_bmr),
+        subset(dn_prov, year == 2001),
+        vcov = ~prov2018),
+  feols(workerratio ~ log(tot_bmr),
+        subset(dn_prov, year == 2002),
+        vcov = ~prov2018),
+  feols(workerratio ~ log(tot_bmr),
+        subset(dn_prov, year == 2003),
+        vcov = ~prov2018),
+  feols(workerratio ~ log(tot_bmr),
+        subset(dn_prov, year == 2004),
+        vcov = ~prov2018),
+  feols(workerratio ~ log(tot_bmr),
+        subset(dn_prov, year == 2005),
+        vcov = ~prov2018),
+  feols(workerratio ~ log(tot_bmr),
+        subset(dn_prov, year == 2006),
+        vcov = ~prov2018),
+  feols(workerratio ~ log(tot_bmr),
+        subset(dn_prov, year == 2007),
+        vcov = ~prov2018),
+  feols(workerratio ~ log(tot_bmr),
+        subset(dn_prov, year == 2008),
+        vcov = ~prov2018),
+  feols(workerratio ~ log(tot_bmr),
+        subset(dn_prov, year == 2009),
+        vcov = ~prov2018),
+  feols(workerratio ~ log(tot_bmr),
+        subset(dn_prov, year == 2010),
+        vcov = ~prov2018),
+  feols(workerratio ~ log(tot_bmr),
+        subset(dn_prov, year == 2011),
+        vcov = ~prov2018),
+  feols(workerratio ~ log(tot_bmr),
+        subset(dn_prov, year == 2012),
+        vcov = ~prov2018),
+  feols(workerratio ~ log(tot_bmr),
+        subset(dn_prov, year == 2013),
+        vcov = ~prov2018),
+  feols(workerratio ~ log(tot_bmr),
+        subset(dn_prov, year == 2014),
+        vcov = ~prov2018),
+  feols(workerratio ~ log(tot_bmr),
+        subset(dn_prov, year == 2015),
+        vcov = ~prov2018),
+  feols(workerratio ~ log(tot_bmr),
+        subset(dn_prov, year == 2016),
+        vcov = ~prov2018),
+  feols(workerratio ~ log(tot_bmr),
+        subset(dn_prov, year == 2017),
+        vcov = ~prov2018),
+  feols(workerratio ~ log(tot_bmr),
+        subset(dn_prov, year == 2018),
+        vcov = ~prov2018)))
+
+dn_prov_fecoef <- lapply(dn_prov_femodels, tidy)
+dn_dist_fecoef_df <- do.call(rbind, dn_dist_fecoef)
+dn_dist_fecoef_df$year <- rep(seq(2000, 2018), each = nrow(dn_dist_fecoef_df) / length(seq(2000, 2018)))
+
+ggplot(dn_dist_fecoef_df, aes(x = factor(year), y = estimate, ymin = estimate - std.error, ymax = estimate + std.error)) +
+  geom_pointrange() +
+  geom_hline(yintercept = 0, linetype = "dotted", color = "black") +
+  labs(x = "Year", y = "Estimate and 95% Interval") +
+  ggtitle("") +
+  theme_minimal() +
+  guides(fill = "none") +  
+  theme(axis.line = element_line(color='black'),
+        plot.background = element_blank(),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank(),
+        legend.title=element_blank(),
+        text = element_text(size=10)) + 
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+
+# PHC Regressions 
+
+## Individual-level regressions 
 
 etable(list(
   feols(work ~ as.factor(female)/log(tot_bomb) + yrschool + nchild + married + as.factor(minority) + age + age^2 | regnvn,
@@ -50,12 +222,6 @@ etable(list(
         phc09,
         vcov = ~geo1_vn2009)
 ), tex = T)
-
-# Province-level regressions 
-
-regvn <- phc %>% 
-  select(geo1_vn1989, geo1_vn1999, geo1_vn2009, regnvn) %>% 
-  distinct()
 
 ## Occupation 
 
