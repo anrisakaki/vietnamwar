5f303880fc15c4c0bc26ccdb2ff6d6b742e0c441
Anri Sakakibara <anrisakaki@gmail.com>
2024-04-15 19:23:44 +0100

    Coefficient plots for VEC regressions

--- Coefficient plots for VEC Regressions.R
+++ Coefficient plots for VEC Regressions.R
@@ -1,4 +1,6 @@
-# District-level 
+######
+# OLS#
+######
 
 ggplot(dn_dist_ols_coef_df, aes(x = factor(year), y = estimate, ymin = estimate - std.error, ymax = estimate + std.error)) +
   geom_pointrange() +
@@ -17,90 +19,7 @@ ggplot(dn_dist_ols_coef_df, aes(x = factor(year), y = estimate, ymin = estimate 
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
 ggsave("dn_dist_ols.jpeg", width = 7, height = 7)
 
-ggplot(dn_dist_fecoef_df, aes(x = factor(year), y = estimate, ymin = estimate - std.error, ymax = estimate + std.error)) +
-  geom_pointrange() +
-  geom_hline(yintercept = 0, linetype = "dotted", color = "black") +
-  labs(x = "Year", y = "Estimate and 95% Interval") +
-  ggtitle("") +
-  theme_minimal() +
-  guides(fill = "none") +  
-  theme(axis.line = element_line(color='black'),
-        plot.background = element_blank(),
-        panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank(),
-        panel.border = element_blank(),
-        legend.title=element_blank(),
-        text = element_text(size=10)) + 
-  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
-ggsave("dn_dist_fe.jpeg", width = 7, height = 7)
-
-(ggplot(dn_dist_olscoef_agri, aes(x = factor(year), y = estimate, ymin = estimate - std.error, ymax = estimate + std.error)) +
-    geom_pointrange() +
-    geom_hline(yintercept = 0, linetype = "dotted", color = "black") +
-    labs(x = "Year", y = "Estimate and 95% Interval") +
-    ggtitle("Agriculture") +
-    theme_minimal() +
-    guides(fill = "none") +  
-    theme(axis.line = element_line(color='black'),
-          plot.background = element_blank(),
-          panel.grid.major = element_blank(),
-          panel.grid.minor = element_blank(),
-          panel.border = element_blank(),
-          legend.title=element_blank(),
-          text = element_text(size=10)) + 
-    theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) | 
-    
-    ggplot(dn_dist_olscoef_manu, aes(x = factor(year), y = estimate, ymin = estimate - std.error, ymax = estimate + std.error)) +
-    geom_pointrange() +
-    geom_hline(yintercept = 0, linetype = "dotted", color = "black") +
-    labs(x = "Year", y = "Estimate and 95% Interval") +
-    ggtitle("Manufacturing") +
-    theme_minimal() +
-    guides(fill = "none") +  
-    theme(axis.line = element_line(color='black'),
-          plot.background = element_blank(),
-          panel.grid.major = element_blank(),
-          panel.grid.minor = element_blank(),
-          panel.border = element_blank(),
-          legend.title=element_blank(),
-          text = element_text(size=10)) + 
-    theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))) + 
-  plot_layout(guides = 'collect') & theme(legend.position = 'bottom')
-ggsave
-
-(ggplot(dn_dist_fecoef_agri, aes(x = factor(year), y = estimate, ymin = estimate - std.error, ymax = estimate + std.error)) +
-              geom_pointrange() +
-              geom_hline(yintercept = 0, linetype = "dotted", color = "black") +
-              labs(x = "Year", y = "Estimate and 95% Interval") +
-              ggtitle("Agriculture") +
-              theme_minimal() +
-              guides(fill = "none") +  
-              theme(axis.line = element_line(color='black'),
-                    plot.background = element_blank(),
-                    panel.grid.major = element_blank(),
-                    panel.grid.minor = element_blank(),
-                    panel.border = element_blank(),
-                    legend.title=element_blank(),
-                    text = element_text(size=10)) + 
-              theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) | 
-    ggplot(dn_dist_fecoef_manu, aes(x = factor(year), y = estimate, ymin = estimate - std.error, ymax = estimate + std.error)) +
-              geom_pointrange() +
-              geom_hline(yintercept = 0, linetype = "dotted", color = "black") +
-              labs(x = "Year", y = "Estimate and 95% Interval") +
-              ggtitle("Manufacturing") +
-              theme_minimal() +
-              guides(fill = "none") +  
-              theme(axis.line = element_line(color='black'),
-                    plot.background = element_blank(),
-                    panel.grid.major = element_blank(),
-                    panel.grid.minor = element_blank(),
-                    panel.border = element_blank(),
-                    legend.title=element_blank(),
-                    text = element_text(size=10)) + 
-              theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))) + 
-  plot_layout(guides = 'collect') & theme(legend.position = 'bottom')
-
-## By South 
+# By south 
 
 ggplot(dn_dist_ols_coef_ns, aes(x = factor(year), y = estimate, ymin = estimate - std.error, ymax = estimate + std.error, color = group)) +
   geom_pointrange() +
@@ -119,6 +38,101 @@ ggplot(dn_dist_ols_coef_ns, aes(x = factor(year), y = estimate, ymin = estimate 
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
 ggsave("dn_dist_ols_coef_ns.jpeg", width = 7, height = 7)
 
+# By Industry 
+
+ggplot(dn_dist_olscoef_agri, aes(x = factor(year), y = estimate, ymin = estimate - std.error, ymax = estimate + std.error)) +
+  geom_pointrange() +
+  geom_hline(yintercept = 0, linetype = "dotted", color = "black") +
+  labs(x = "Year", y = "Estimate and 95% Interval") +
+  ggtitle("") +
+  theme_minimal() +
+  guides(fill = "none") +  
+  theme(axis.line = element_line(color='black'),
+        plot.background = element_blank(),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank(),
+        legend.title=element_blank(),
+        text = element_text(size=10)) + 
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+ggsave("dn_dist_olscoef_agri.jpeg", width = 7, height = 7)
+
+ggplot(dn_dist_olscoef_manu, aes(x = factor(year), y = estimate, ymin = estimate - std.error, ymax = estimate + std.error)) +
+  geom_pointrange() +
+  geom_hline(yintercept = 0, linetype = "dotted", color = "black") +
+  labs(x = "Year", y = "Estimate and 95% Interval") +
+  ggtitle("") +
+  theme_minimal() +
+  guides(fill = "none") +  
+  theme(axis.line = element_line(color='black'),
+        plot.background = element_blank(),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank(),
+        legend.title=element_blank(),
+        text = element_text(size=10)) + 
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+ggsave("dn_dist_olscoef_manu.jpeg", width = 7, height = 7)
+
+# By industry - south 
+
+ggplot(dn_dist_olscoef_agri_ns, aes(x = factor(year), y = estimate, ymin = estimate - std.error, ymax = estimate + std.error, color = group)) +
+  geom_pointrange() +
+  geom_hline(yintercept = 0, linetype = "dotted", color = "black") +
+  labs(x = "Year", y = "Estimate and 95% Interval", color = "Region") +
+  ggtitle("") +
+  theme_minimal() +
+  guides(fill = "none") +  
+  theme(axis.line = element_line(color='black'),
+        plot.background = element_blank(),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank(),
+        legend.title=element_blank(),
+        text = element_text(size=10)) + 
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+ggsave("dn_dist_olscoef_agri_ns.jpeg", width = 7, height = 7)
+
+ggplot(dn_dist_olscoef_manu_ns, aes(x = factor(year), y = estimate, ymin = estimate - std.error, ymax = estimate + std.error, color = group)) +
+  geom_pointrange() +
+  geom_hline(yintercept = 0, linetype = "dotted", color = "black") +
+  labs(x = "Year", y = "Estimate and 95% Interval", color = "Region") +
+  ggtitle("") +
+  theme_minimal() +
+  guides(fill = "none") +  
+  theme(axis.line = element_line(color='black'),
+        plot.background = element_blank(),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank(),
+        legend.title=element_blank(),
+        text = element_text(size=10)) + 
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+ggsave("dn_dist_olscoef_manu_ns.jpeg", width = 7, height = 7)
+
+#################
+# FIXED EFFECTS #
+#################
+
+ggplot(dn_dist_fecoef_df, aes(x = factor(year), y = estimate, ymin = estimate - std.error, ymax = estimate + std.error)) +
+  geom_pointrange() +
+  geom_hline(yintercept = 0, linetype = "dotted", color = "black") +
+  labs(x = "Year", y = "Estimate and 95% Interval") +
+  ggtitle("") +
+  theme_minimal() +
+  guides(fill = "none") +  
+  theme(axis.line = element_line(color='black'),
+        plot.background = element_blank(),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank(),
+        legend.title=element_blank(),
+        text = element_text(size=10)) + 
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+ggsave("dn_dist_fe.jpeg", width = 7, height = 7)
+
+# By South 
+
 ggplot(dn_dist_fecoef_df_ns, aes(x = factor(year), y = estimate, ymin = estimate - std.error, ymax = estimate + std.error, color = group)) +
   geom_pointrange() +
   geom_hline(yintercept = 0, linetype = "dotted", color = "black") +
@@ -136,9 +150,45 @@ ggplot(dn_dist_fecoef_df_ns, aes(x = factor(year), y = estimate, ymin = estimate
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
 ggsave("dn_dist_fecoef_ns.jpeg", width = 7, height = 7)
 
-## By industry 
+# By Industry 
 
-ggplot(dn_dist_olscoef_agri_ns, aes(x = factor(year), y = estimate, ymin = estimate - std.error, ymax = estimate + std.error, color = group)) +
+ggplot(dn_dist_fecoef_agri, aes(x = factor(year), y = estimate, ymin = estimate - std.error, ymax = estimate + std.error)) +
+  geom_pointrange() +
+  geom_hline(yintercept = 0, linetype = "dotted", color = "black") +
+  labs(x = "Year", y = "Estimate and 95% Interval") +
+  ggtitle("") +
+  theme_minimal() +
+  guides(fill = "none") +  
+  theme(axis.line = element_line(color='black'),
+        plot.background = element_blank(),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank(),
+        legend.title=element_blank(),
+        text = element_text(size=10)) + 
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+ggsave("dn_dist_fecoef_agri.jpeg", width = 7, height = 7)
+
+ggplot(dn_dist_fecoef_manu, aes(x = factor(year), y = estimate, ymin = estimate - std.error, ymax = estimate + std.error)) +
+  geom_pointrange() +
+  geom_hline(yintercept = 0, linetype = "dotted", color = "black") +
+  labs(x = "Year", y = "Estimate and 95% Interval") +
+  ggtitle("") +
+  theme_minimal() +
+  guides(fill = "none") +  
+  theme(axis.line = element_line(color='black'),
+        plot.background = element_blank(),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank(),
+        legend.title=element_blank(),
+        text = element_text(size=10)) + 
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+ggsave("dn_dist_fecoef_manu.jpeg", width = 7, height = 7)
+
+# By industry - South 
+
+ggplot(dn_dist_fecoef_agri_ns, aes(x = factor(year), y = estimate, ymin = estimate - std.error, ymax = estimate + std.error, color = group)) +
   geom_pointrange() +
   geom_hline(yintercept = 0, linetype = "dotted", color = "black") +
   labs(x = "Year", y = "Estimate and 95% Interval", color = "Region") +
@@ -153,8 +203,9 @@ ggplot(dn_dist_olscoef_agri_ns, aes(x = factor(year), y = estimate, ymin = estim
         legend.title=element_blank(),
         text = element_text(size=10)) + 
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+ggsave("dn_dist_fecoef_agri_ns.jpeg", width = 7, height = 7)
 
-ggplot(dn_dist_olscoef_manu_ns, aes(x = factor(year), y = estimate, ymin = estimate - std.error, ymax = estimate + std.error, color = group)) +
+ggplot(dn_dist_fecoef_manu_ns, aes(x = factor(year), y = estimate, ymin = estimate - std.error, ymax = estimate + std.error, color = group)) +
   geom_pointrange() +
   geom_hline(yintercept = 0, linetype = "dotted", color = "black") +
   labs(x = "Year", y = "Estimate and 95% Interval", color = "Region") +
@@ -169,8 +220,11 @@ ggplot(dn_dist_olscoef_manu_ns, aes(x = factor(year), y = estimate, ymin = estim
         legend.title=element_blank(),
         text = element_text(size=10)) + 
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+ggsave("dn_dist_fecoef_manu_ns.jpeg", width = 7, height = 7)
 
-# Province-level 
+#################
+# Province-level#
+#################
 
 ggplot(dn_prov_ols_coef, aes(x = factor(year), y = estimate, ymin = estimate - std.error, ymax = estimate + std.error)) +
   geom_pointrange() +
@@ -189,6 +243,8 @@ ggplot(dn_prov_ols_coef, aes(x = factor(year), y = estimate, ymin = estimate - s
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
 ggsave("dn_prov_ols_coef.jpeg", width = 7, height = 7)
 
+# By south 
+
 ggplot(dn_prov_ols_coef_ns, aes(x = factor(year), y = estimate, ymin = estimate - std.error, ymax = estimate + std.error, color = group)) +
   geom_pointrange() +
   geom_hline(yintercept = 0, linetype = "dotted", color = "black") +
@@ -206,3 +262,72 @@ ggplot(dn_prov_ols_coef_ns, aes(x = factor(year), y = estimate, ymin = estimate 
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
 ggsave("dn_prov_ols_coef_ns.jpeg", width = 7, height = 7)
 
+# By industry 
+ggplot(dn_prov_olscoef_agri, aes(x = factor(year), y = estimate, ymin = estimate - std.error, ymax = estimate + std.error)) +
+  geom_pointrange() +
+  geom_hline(yintercept = 0, linetype = "dotted", color = "black") +
+  labs(x = "Year", y = "Estimate and 95% Interval") +
+  ggtitle("") +
+  theme_minimal() +
+  guides(fill = "none") +  
+  theme(axis.line = element_line(color='black'),
+        plot.background = element_blank(),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank(),
+        legend.title=element_blank(),
+        text = element_text(size=10)) + 
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+ggsave("dn_prov_olscoef_agri.jpeg", width = 7, height = 7)
+
+ggplot(dn_prov_olscoef_manu, aes(x = factor(year), y = estimate, ymin = estimate - std.error, ymax = estimate + std.error)) +
+  geom_pointrange() +
+  geom_hline(yintercept = 0, linetype = "dotted", color = "black") +
+  labs(x = "Year", y = "Estimate and 95% Interval") +
+  ggtitle("") +
+  theme_minimal() +
+  guides(fill = "none") +  
+  theme(axis.line = element_line(color='black'),
+        plot.background = element_blank(),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank(),
+        legend.title=element_blank(),
+        text = element_text(size=10)) + 
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+ggsave("dn_prov_olscoef_manu.jpeg", width = 7, height = 7)
+
+# By industry - south 
+ggplot(dn_prov_olscoef_agri_ns, aes(x = factor(year), y = estimate, ymin = estimate - std.error, ymax = estimate + std.error, color = group)) +
+  geom_pointrange() +
+  geom_hline(yintercept = 0, linetype = "dotted", color = "black") +
+  labs(x = "Year", y = "Estimate and 95% Interval", color = "Region") +
+  ggtitle("") +
+  theme_minimal() +
+  guides(fill = "none") +  
+  theme(axis.line = element_line(color='black'),
+        plot.background = element_blank(),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank(),
+        legend.title=element_blank(),
+        text = element_text(size=10)) + 
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+ggsave("dn_prov_olscoef_agri_ns.jpeg", width = 7, height = 7)
+
+ggplot(dn_prov_olscoef_manu_ns, aes(x = factor(year), y = estimate, ymin = estimate - std.error, ymax = estimate + std.error, color = group)) +
+  geom_pointrange() +
+  geom_hline(yintercept = 0, linetype = "dotted", color = "black") +
+  labs(x = "Year", y = "Estimate and 95% Interval", color = "Region") +
+  ggtitle("") +
+  theme_minimal() +
+  guides(fill = "none") +  
+  theme(axis.line = element_line(color='black'),
+        plot.background = element_blank(),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank(),
+        legend.title=element_blank(),
+        text = element_text(size=10)) + 
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+ggsave("dn_prov_olscoef_manu_ns.jpeg", width = 7, height = 7)
