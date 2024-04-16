4efd15103396be007413013dcfbf795e1d8201fd
Anri Sakakibara <anrisakaki@gmail.com>
2024-04-16 22:23:43 +0100

    Coefficient plots for VEC regressions

--- Coefficient plots for VEC Regressions.R
+++ Coefficient plots for VEC Regressions.R
@@ -55,6 +55,23 @@ ggplot(dn_dist_ols_coef_ns, aes(x = factor(year), y = estimate, ymin = estimate 
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
 ggsave("dn_dist_ols_coef_ns.jpeg", width = 7, height = 7)
 
+ggplot(dn_ols_coef_ns, aes(x = factor(year), y = estimate, ymin = estimate - std.error, ymax = estimate + std.error, color = group)) +
+  geom_pointrange() +
+  geom_hline(yintercept = 0, linetype = "dotted", color = "black") +
+  labs(x = "Year", y = "Estimate and 95% Interval", color = "Region") +
+  ggtitle("") +
+  theme_minimal() +
+  guides(fill = "none") +  
+  theme(axis.line = element_line(color='black'),
+        plot.background = element_blank(),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank(),
+        legend.title=element_blank(),
+        text = element_text(size=10)) + 
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+ggsave("dn_ols_coef_ns.jpeg", width = 7, height = 7)
+
 #################
 # FIXED EFFECTS #
 #################
@@ -111,6 +128,23 @@ ggplot(dn_dist_fecoef_df_ns, aes(x = factor(year), y = estimate, ymin = estimate
         text = element_text(size=10)) + 
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
 ggsave("dn_dist_fecoef_ns.jpeg", width = 7, height = 7)
+
+ggplot(dn_indfe_coef_ns, aes(x = factor(year), y = estimate, ymin = estimate - std.error, ymax = estimate + std.error, color = group)) +
+  geom_pointrange() +
+  geom_hline(yintercept = 0, linetype = "dotted", color = "black") +
+  labs(x = "Year", y = "Estimate and 95% Interval", color = "Region") +
+  ggtitle("") +
+  theme_minimal() +
+  guides(fill = "none") +  
+  theme(axis.line = element_line(color='black'),
+        plot.background = element_blank(),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank(),
+        legend.title=element_blank(),
+        text = element_text(size=10)) + 
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+ggsave("dn_indfe_coef_ns.jpeg", width = 7, height = 7)
 
 #################
 # Province-level#
