36d0b48067a704bcaf79337e0c642b1cef9ac2f0
Anri Sakakibara <anrisakaki@gmail.com>
2024-04-16 17:44:35 +0100

    Summary statistics of VHLSS

--- Summary statistics of VHLSS.R
+++ Summary statistics of VHLSS.R
@@ -4,16 +4,58 @@
 
 # District level ratio of workers and share of women working 
 
-dist02 <- vhlss02 %>% 
-  filter(age > 15 & age < 65) %>% 
+dist02_vhlss <- vhlss02 %>% 
+  filter(!is.na(female), age > 15 & age < 65) %>% 
   group_by(tinh02, huyen02) %>% 
-  summarise(m_total = sum(female == 0, na.rm = TRUE, weights = wt75),
-            f_total = sum(female == 1, na.rm = TRUE, weights = wt75),
-            m_workers = sum(female == 0 & work == 1, na.rm = TRUE, weights = wt75),
-            f_workers = sum(female == 1 & work == 1, na.rm = TRUE, weights = wt75),
-            m_selfemp = sum(female == 0 & selfemp == 1, na.rm = TRUE, weights = wt75),
-            f_selfemp = sum(female == 1 & selfemp == 1, na.rm = TRUE, weights = wt75)) %>% 
-  mutate(flfp = f_workers/f_total,
-         workerratio = m_workers/f_workers,
-         selfemp_workerratio = m_selfemp/f_selfemp)
+  summarise(
+    m_total = sum(wt75 * (female == 0), na.rm = T),
+    f_total = sum(wt75 * (female == 1), na.rm = T),
+    m_workers = sum(wt75 * (female == 0 & work == 1), na.rm = T),
+    f_workers = sum(wt75 * (female == 1 & work == 1), na.rm = T),
+    fself_emp = sum(wt75 * (female == 1 & selfemp == 1), na.rm = T),
+    mself_emp = sum(wt75 * (female == 0 & selfemp == 1), na.rm = T)
+  ) %>% 
+  mutate(
+    sex_ratio = m_total / f_total,
+    workerratio = m_workers / f_workers,
+    selfemp_workerratio = mself_emp / fself_emp,
+    flfp = f_workers / f_total
+  )
+  
+
+dist04_vhlss <- vhlss04 %>% 
+  filter(!is.na(female), age > 15 & age < 65) %>% 
+  group_by(tinh, huyen) %>% 
+  summarise(
+    m_total = sum(wt45 * (female == 0), na.rm = T),
+    f_total = sum(wt45 * (female == 1), na.rm = T),
+    m_workers = sum(wt45 * (female == 0 & work == 1), na.rm = T),
+    f_workers = sum(wt45 * (female == 1 & work == 1), na.rm = T),
+    fself_emp = sum(wt45 * (female == 1 & selfemp == 1), na.rm = T),
+    mself_emp = sum(wt45 * (female == 0 & selfemp == 1), na.rm = T)
+  ) %>% 
+  mutate(
+    sex_ratio = m_total / f_total,
+    workerratio = m_workers / f_workers,
+    selfemp_workerratio = mself_emp / fself_emp,
+    flfp = f_workers / f_total
+  )
+
+dist06_vhlss <- vhlss06 %>% 
+  filter(!is.na(female), age > 15 & age < 65) %>% 
+  group_by(tinh, huyen) %>% 
+  summarise(
+    m_total = sum(wt45 * (female == 0), na.rm = T),
+    f_total = sum(wt45 * (female == 1), na.rm = T),
+    m_workers = sum(wt45 * (female == 0 & work == 1), na.rm = T),
+    fself_emp = sum(wt45 * (female == 1 & selfemp == 1), na.rm = T),
+    f_workers = sum(wt45 * (female == 1 & work == 1), na.rm = T),
+    mself_emp = sum(wt45 * (female == 0 & selfemp == 1), na.rm = T)
+  ) %>% 
+  mutate(
+    sex_ratio = m_total / f_total,
+    workerratio = m_workers / f_workers,
+    selfemp_workerratio = mself_emp / fself_emp,
+    flfp = f_workers / f_total
+  )
 
