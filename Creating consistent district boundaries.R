9fb2c3c0714b5233ae50b2d114fc31f28f26bf11
Anri Sakakibara <anrisakaki@gmail.com>
2024-04-17 16:13:58 +0100

    Creating consistent district boundaries

--- Creating consistent district boundaries.R
+++ Creating consistent district boundaries.R
@@ -1,10 +1,18 @@
-district <- bombs_district %>% 
-  select(districtname, provincename, province, district) %>% 
-  mutate(huyen = as.numeric(str_sub(district, -2))) %>% 
-  rename(tinh = provincename)
-
 geoid_district <- lapply(geoid_list, function(i) {
   i %>%
     select(provname, distname, contains("prov"), contains("dist")) %>%
     distinct()
 })
+
+district0203 <- bombs_district %>% 
+  select(districtname, provincename, province, district, tot_bmr, south_corrected) %>% 
+  mutate(huyen = as.numeric(str_sub(district, -2))) %>% 
+  rename(tinh = province) %>% 
+  select(-district) %>% 
+  filter(!is.na(tinh))
+
+prov0203 <- bombs_province_miguel %>% 
+  select(province, tot_bmr) %>% 
+  rename(tinh = province,
+         tot_bmr_prov = tot_bmr) %>% 
+  filter(!is.na(tinh))
