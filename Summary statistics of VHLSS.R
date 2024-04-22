0c5b10d2b1ed1dbd07d85e0ee15a299814ddbf86
Anri Sakakibara <anrisakaki@gmail.com>
2024-04-22 15:23:58 +0100

    Summary statistics of VHLSS

--- Summary statistics of VHLSS.R
+++ Summary statistics of VHLSS.R
@@ -6,7 +6,7 @@
 
 prov02_vhlss <- vhlss02 %>% 
   filter(!is.na(female), age > 15 & age < 65) %>% 
-  group_by(tinh) %>% 
+  group_by(tinh02) %>% 
   summarise(
     m_total = sum(wt75 * (female == 0), na.rm = T),
     f_total = sum(wt75 * (female == 1), na.rm = T),
@@ -14,13 +14,16 @@ prov02_vhlss <- vhlss02 %>%
     f_workers = sum(wt75 * (female == 1 & work == 1), na.rm = T),
     fself_emp = sum(wt75 * (female == 1 & selfemp == 1), na.rm = T),
     mself_emp = sum(wt75 * (female == 0 & selfemp == 1), na.rm = T),
-    tot_bmr_prov = mean(tot_bmr_prov)
+    tot_bmr_prov = mean(tot_bmr_prov),
+    tot_bmr_lb_prov = mean(tot_bmr_lb_prov),
+    killed_tot_prov = mean(killed_tot_prov)
   ) %>% 
   mutate(
     sex_ratio = m_total / f_total,
     workerratio = m_workers / f_workers,
     selfemp_workerratio = mself_emp / fself_emp,
-    flfp = f_workers / f_total
+    flfp = f_workers / f_total,
+    south = ifelse(tinh02 > 407, 1, 0)
   )
   
 
@@ -37,14 +40,17 @@ prov04_vhlss <- vhlss04 %>%
     mself_emp = sum(wt45 * (female == 0 & selfemp == 1), na.rm = T),
     f_manager = sum(wt45 * (f_manager == 1), na.rm = T),
     m_manager = sum(wt45 * (m_manager == 1), na.rm = T),
-    tot_bmr_prov = mean(tot_bmr_prov)
+    tot_bmr_prov = mean(tot_bmr_prov),
+    tot_bmr_lb_prov = mean(tot_bmr_lb_prov),
+    killed_tot_prov = mean(killed_tot_prov)
   ) %>% 
   mutate(
     sex_ratio = m_total / f_total,
     workerratio = m_workers / f_workers,
     selfemp_workerratio = mself_emp / fself_emp,
     manager_workerratio = m_manager/f_manager,
-    flfp = f_workers / f_total
+    flfp = f_workers / f_total,
+    south = ifelse(tinh > 407, 1, 0)
   )
 
 prov06_vhlss <- vhlss06 %>% 
@@ -57,11 +63,14 @@ prov06_vhlss <- vhlss06 %>%
     fself_emp = sum(wt45 * (female == 1 & selfemp == 1), na.rm = T),
     f_workers = sum(wt45 * (female == 1 & work == 1), na.rm = T),
     mself_emp = sum(wt45 * (female == 0 & selfemp == 1), na.rm = T),
-    tot_bmr_prov = mean(tot_bmr_prov)
+    tot_bmr_prov = mean(tot_bmr_prov),
+    tot_bmr_lb_prov = mean(tot_bmr_lb_prov),
+    killed_tot_prov = mean(killed_tot_prov)
   ) %>% 
   mutate(
     sex_ratio = m_total / f_total,
     workerratio = m_workers / f_workers,
     selfemp_workerratio = mself_emp / fself_emp,
-    flfp = f_workers / f_total
+    flfp = f_workers / f_total,
+    south = ifelse(tinh > 407, 1, 0)
   )
