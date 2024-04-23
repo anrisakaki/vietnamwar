985e693e2d208a4c89ea577731d58f15ed91892a
Anri Sakakibara <anrisakaki@gmail.com>
2024-04-23 17:43:46 +0100

    Summary statistics of VHLSS

--- Summary statistics of VHLSS.R
+++ Summary statistics of VHLSS.R
@@ -80,7 +80,55 @@ prov06_vhlss <- vhlss06 %>%
     south = ifelse(tinh > 407, 1, 0)
   )
 
+prov08_vhlss <- vhlss08 %>% 
+  mutate(m_manager = ifelse(f_manager == 0, 1, 0)) %>% 
+  filter(!is.na(female), age > 15 & age < 65) %>% 
+  group_by(tinh) %>% 
+  summarise(
+    m_total = sum(wt9 * (female == 0), na.rm = T),
+    f_total = sum(wt9 * (female == 1), na.rm = T),
+    m_workers = sum(wt9 * (female == 0 & work == 1), na.rm = T),
+    fself_emp = sum(wt9 * (female == 1 & selfemp == 1), na.rm = T),
+    f_workers = sum(wt9 * (female == 1 & work == 1), na.rm = T),
+    mself_emp = sum(wt9 * (female == 0 & selfemp == 1), na.rm = T),
+    f_manager = sum(wt9 * (f_manager == 1), na.rm = T),
+    m_manager = sum(wt9 * (m_manager == 1), na.rm = T),
+    tot_bmr_prov = mean(tot_bmr_prov),
+    tot_bmr_lb_prov = mean(tot_bmr_lb_prov),
+    killed_tot_prov = mean(killed_tot_prov)
+  ) %>% 
+  mutate(
+    sex_ratio = m_total / f_total,
+    workerratio = m_workers / f_workers,
+    selfemp_workerratio = mself_emp / fself_emp,
+    manager_workerratio = m_manager/f_manager,
+    flfp = f_workers / f_total,
+    south = ifelse(tinh > 407, 1, 0)
+  )
+
 prov10_vhlss <- vhlss10 %>% 
+  filter(!is.na(female), age > 15 & age < 65) %>% 
+  group_by(tinh) %>% 
+  summarise(
+    m_total = sum(wt9 * (female == 0), na.rm = T),
+    f_total = sum(wt9 * (female == 1), na.rm = T),
+    m_workers = sum(wt9 * (female == 0 & work == 1), na.rm = T),
+    fself_emp = sum(wt9 * (female == 1 & selfemp == 1), na.rm = T),
+    f_workers = sum(wt9 * (female == 1 & work == 1), na.rm = T),
+    mself_emp = sum(wt9 * (female == 0 & selfemp == 1), na.rm = T),
+    tot_bmr_prov = mean(tot_bmr_prov),
+    tot_bmr_lb_prov = mean(tot_bmr_lb_prov),
+    killed_tot_prov = mean(killed_tot_prov)
+  ) %>% 
+  mutate(
+    sex_ratio = m_total / f_total,
+    workerratio = m_workers / f_workers,
+    selfemp_workerratio = mself_emp / fself_emp,
+    flfp = f_workers / f_total,
+    south = ifelse(tinh > 44, 1, 0)
+  )
+
+prov12_vhlss <- vhlss12 %>% 
   filter(!is.na(female), age > 15 & age < 65) %>% 
   group_by(tinh) %>% 
   summarise(
