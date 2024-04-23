9574d9edcab73fb9b405ec109f27215d8c645e22
Anri Sakakibara <anrisakaki@gmail.com>
2024-04-23 15:35:36 +0100

    VHLSS Regressions

--- VHLSS Regressions.R
+++ VHLSS Regressions.R
@@ -61,47 +61,49 @@ vhlss_prov_coef_s$year <- rep(seq(2001, 2009, by = 2), length.out = nrow(vhlss_p
 vhlss_prov_coef_s <-  vhlss_prov_coef_s %>% mutate(year = ifelse(year == 2007, 2009, year))
 vhlss_prov_coef_ns <- rbind(vhlss_prov_coef_n, vhlss_prov_coef_s)
 
-etable(list(
-  feols(selfemp_workerratio ~ log(tot_bmr_prov),
+# Ratio of managers of HH businesses 
+
+vhlss_prov <- list(
+  feols(workerratio ~ log(tot_bmr_prov),
         prov02_vhlss,
         vcov = ~tinh02),
-  feols(selfemp_workerratio ~ log(tot_bmr_prov),
+  feols(workerratio ~ log(tot_bmr_prov),
         prov04_vhlss,
         vcov = ~tinh),
-  feols(selfemp_workerratio ~ log(tot_bmr_prov),
+  feols(workerratio ~ log(tot_bmr_prov),
         prov06_vhlss,
         vcov = ~tinh),
-  feols(selfemp_workerratio ~ log(tot_bmr_prov),
+  feols(workerratio ~ log(tot_bmr_prov),
         prov10_vhlss,
         vcov = ~tinh)
-))
+)
 
-vhlss_selfemp_prov_s <- list(
-  feols(selfemp_workerratio ~ log(tot_bmr_prov),
+vhlss_prov_s <-  list(
+  feols(workerratio ~ log(tot_bmr_prov),
         subset(prov02_vhlss, south == 1),
         vcov = ~tinh02),
-  feols(selfemp_workerratio ~ log(tot_bmr_prov),
+  feols(workerratio ~ log(tot_bmr_prov),
         subset(prov04_vhlss, south == 1),
         vcov = ~tinh),
-  feols(selfemp_workerratio ~ log(tot_bmr_prov),
+  feols(workerratio ~ log(tot_bmr_prov),
         subset(prov06_vhlss, south == 1),
         vcov = ~tinh),
-  feols(selfemp_workerratio ~ log(tot_bmr_prov),
+  feols(workerratio ~ log(tot_bmr_prov),
         subset(prov10_vhlss, south == 1),
         vcov = ~tinh)  
 )
 
-vhlss_selfemp_prov_n <-list(
-  feols(selfemp_workerratio ~ log(tot_bmr_prov),
+vhlss_prov_n <-  list(
+  feols(workerratio ~ log(tot_bmr_prov),
         subset(prov02_vhlss, south == 0),
         vcov = ~tinh02),
-  feols(selfemp_workerratio ~ log(tot_bmr_prov),
+  feols(workerratio ~ log(tot_bmr_prov),
         subset(prov04_vhlss, south == 0),
         vcov = ~tinh),
-  feols(selfemp_workerratio ~ log(tot_bmr_prov),
+  feols(workerratio ~ log(tot_bmr_prov),
         subset(prov06_vhlss, south == 0),
         vcov = ~tinh),
-  feols(selfemp_workerratio ~ log(tot_bmr_prov),
+  feols(workerratio ~ log(tot_bmr_prov),
         subset(prov10_vhlss, south == 0),
         vcov = ~tinh)  
 )
@@ -110,47 +112,74 @@ vhlss_selfemp_prov_n <-list(
 # VHLSS REGRESSIONS - INDIVIDUAL LEVEL #
 ########################################
 
+# Probability of working 
+
 etable(list(
   feols(work ~ as.factor(female)/log(tot_bmr_prov) + age + age^2 + educ,
-        subset(vhlss02, age > 42 & age < 65),
+        subset(vhlss02),
         weights = ~wt75,
-        vcov = ~tinh+huyen),
+        vcov = ~tinh02),
   feols(work ~ as.factor(female)/log(tot_bmr_prov) + age + age^2 + educ,
-        subset(vhlss04, age > 44 & age < 65),
+        subset(vhlss04),
         weights = ~wt45,
-        vcov = ~tinh+huyen),
+        vcov = ~tinh),
   feols(work ~ as.factor(female)/log(tot_bmr_prov) + age + age^2 + educ,
-        subset(vhlss06, age > 46 & age < 65),
+        subset(vhlss06),
         weights = ~wt45,
-        vcov = ~tinh+huyen)  
+        vcov = ~tinh),
+  feols(work ~ as.factor(female)/log(tot_bmr_prov) + age + age^2 + educ,
+        subset(vhlss10),
+        weights = ~wt9,
+        vcov = ~tinh)
 ))
 
 etable(list(
-  feols(selfemp ~ as.factor(female)/log(tot_bmr_prov) + age + age^2 + educ,
-        subset(vhlss02, age > 15 & age < 65 & work == 1),
+  feols(work ~ as.factor(female)/log(tot_bmr_prov) + age + age^2 + educ,
+        subset(vhlss02, south == 1),
         weights = ~wt75,
-        vcov = ~tinh+huyen),
-  feols(selfemp ~ as.factor(female)/log(tot_bmr_prov) + age + age^2 + educ,
-        subset(vhlss04, age > 15 & age < 65 & work == 1),
+        vcov = ~tinh02),
+  feols(work ~ as.factor(female)/log(tot_bmr_prov) + age + age^2 + educ,
+        subset(vhlss04, south == 1),
         weights = ~wt45,
-        vcov = ~tinh+huyen),
-  feols(selfemp ~ as.factor(female)/log(tot_bmr_prov) + age + age^2 + educ,
-        subset(vhlss06, age > 15 & age < 65 & work == 1),
+        vcov = ~tinh),
+  feols(work ~ as.factor(female)/log(tot_bmr_prov) + age + age^2 + educ,
+        subset(vhlss06, south == 1),
         weights = ~wt45,
-        vcov = ~tinh+huyen)  
+        vcov = ~tinh)  
 ))
 
 etable(list(
-  feols(wagework ~ as.factor(female)/log(tot_bmr_prov) + age + age^2 + educ,
-        subset(vhlss02, age > 15 & age < 65),
+  feols(work ~ as.factor(female)/log(tot_bmr_prov) + age + age^2 + educ,
+        subset(vhlss02, south == 0),
         weights = ~wt75,
-        vcov = ~tinh+huyen),
-  feols(wagework ~ as.factor(female)/log(tot_bmr_prov) + age + age^2 + educ,
-        subset(vhlss04, age > 15 & age < 65),
+        vcov = ~tinh02),
+  feols(work ~ as.factor(female)/log(tot_bmr_prov) + age + age^2 + educ,
+        subset(vhlss04, south == 0),
         weights = ~wt45,
-        vcov = ~tinh+huyen),
-  feols(wagework ~ as.factor(female)/log(tot_bmr_prov) + age + age^2 + educ,
-        subset(vhlss06, age > 15 & age < 65),
+        vcov = ~tinh),
+  feols(work ~ as.factor(female)/log(tot_bmr_prov) + age + age^2 + educ,
+        subset(vhlss06, south == 0),
         weights = ~wt45,
-        vcov = ~tinh+huyen)  
+        vcov = ~tinh)  
+))
+
+# Probability of being a manager of HH business 
+
+etable(list(
+  feols(work ~ as.factor(female)/log(tot_bmr_prov) + age + age^2 + educ,
+        subset(vhlss02),
+        weights = ~wt75,
+        vcov = ~tinh02),
+  feols(work ~ as.factor(female)/log(tot_bmr_prov) + age + age^2 + educ,
+        subset(vhlss04),
+        weights = ~wt45,
+        vcov = ~tinh),
+  feols(work ~ as.factor(female)/log(tot_bmr_prov) + age + age^2 + educ,
+        subset(vhlss06),
+        weights = ~wt45,
+        vcov = ~tinh),
+  feols(work ~ as.factor(female)/log(tot_bmr_prov) + age + age^2 + educ,
+        subset(vhlss10),
+        weights = ~wt9,
+        vcov = ~tinh)
 ))
