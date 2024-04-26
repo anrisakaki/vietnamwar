3719339e1a5bfaa1605e7ba20eac9d9722ded45b
Anri Sakakibara <anrisakaki@gmail.com>
2024-04-26 13:29:36 +0100

    VHLSS Regressions

--- VHLSS Regressions.R
+++ VHLSS Regressions.R
@@ -15,86 +15,249 @@ for (i in prov_vhlss) {
 ######################################
 
 # Ratio of workers 
-
 vhlss_prov <- list(
-  feols(workerratio ~ log(tot_bmr_prov),
+  feols(workerratio ~ log(tot_bmr_prov_ppn) + sex_ratio + dist_nearest_base_prov + dist_nearest_hochi_prov,
         prov02_vhlss,
-        vcov = ~tinh02),
-  feols(workerratio ~ log(tot_bmr_prov),
+        vcov = ~tinh),
+  feols(workerratio ~ log(tot_bmr_prov_ppn) + sex_ratio + dist_nearest_base_prov + dist_nearest_hochi_prov,
         prov04_vhlss,
         vcov = ~tinh),
-  feols(workerratio ~ log(tot_bmr_prov),
+  feols(workerratio ~ log(tot_bmr_prov_ppn) + sex_ratio + dist_nearest_base_prov + dist_nearest_hochi_prov,
         prov06_vhlss,
         vcov = ~tinh),
-  feols(workerratio ~ log(tot_bmr_prov),
+  feols(workerratio ~ log(tot_bmr_prov_ppn) + sex_ratio + dist_nearest_base_prov + dist_nearest_hochi_prov,
         prov08_vhlss,
         vcov = ~tinh),
-  feols(workerratio ~ log(tot_bmr_prov),
+  feols(workerratio ~ log(tot_bmr_prov_ppn) + sex_ratio + dist_nearest_base_prov + dist_nearest_hochi_prov,
         prov10_vhlss,
         vcov = ~tinh),
-  feols(workerratio ~ log(tot_bmr_prov),
+  feols(workerratio ~ log(tot_bmr_prov_ppn) + sex_ratio + dist_nearest_base_prov + dist_nearest_hochi_prov,
         prov12_vhlss,
         vcov = ~tinh)
 )
 
 vhlss_prov_coef <- lapply(vhlss_prov, tidy)
-vhlss_prov_coef <- do.call(rbind, vhlss_prov_coef) %>% filter(term != "(Intercept)")
-vhlss_prov_coef$year <- rep(seq(2001, 2013, by = 2), length.out = nrow(vhlss_prov_coef))
+vhlss_prov_coef <- do.call(rbind, vhlss_prov_coef) %>% filter(term == "log(tot_bmr_prov_ppn)")
+vhlss_prov_coef$year <- rep(seq(2001, 2011, by = 2), length.out = nrow(vhlss_prov_coef))
 
 vhlss_prov_s <-  list(
-  feols(workerratio ~ log(tot_bmr_prov),
+  feols(workerratio ~ log(tot_bmr_prov_ppn) + sex_ratio + dist_nearest_base_prov + dist_nearest_hochi_prov,
         subset(prov02_vhlss, south == 1),
-        vcov = ~tinh02),
-  feols(workerratio ~ log(tot_bmr_prov),
+        vcov = ~tinh),
+  feols(workerratio ~ log(tot_bmr_prov_ppn) + sex_ratio + dist_nearest_base_prov + dist_nearest_hochi_prov,
         subset(prov04_vhlss, south == 1),
         vcov = ~tinh),
-  feols(workerratio ~ log(tot_bmr_prov),
+  feols(workerratio ~ log(tot_bmr_prov_ppn) + sex_ratio + dist_nearest_base_prov + dist_nearest_hochi_prov,
         subset(prov06_vhlss, south == 1),
         vcov = ~tinh),
-  feols(workerratio ~ log(tot_bmr_prov),
+  feols(workerratio ~ log(tot_bmr_prov_ppn) + sex_ratio + dist_nearest_base_prov + dist_nearest_hochi_prov,
         subset(prov08_vhlss, south == 1),
         vcov = ~tinh),
-  feols(workerratio ~ log(tot_bmr_prov),
+  feols(workerratio ~ log(tot_bmr_prov_ppn) + sex_ratio + dist_nearest_base_prov + dist_nearest_hochi_prov,
         subset(prov10_vhlss, south == 1),
         vcov = ~tinh),
-  feols(workerratio ~ log(tot_bmr_prov),
+  feols(workerratio ~ log(tot_bmr_prov_ppn) + sex_ratio + dist_nearest_base_prov + dist_nearest_hochi_prov,
         subset(prov12_vhlss, south == 1),
         vcov = ~tinh)
 )
 
 vhlss_prov_n <-  list(
-  feols(workerratio ~ log(tot_bmr_prov),
+  feols(workerratio ~ log(tot_bmr_prov_ppn) + sex_ratio + dist_nearest_base_prov + dist_nearest_hochi_prov,
         subset(prov02_vhlss, south == 0),
-        vcov = ~tinh02),
-  feols(workerratio ~ log(tot_bmr_prov),
+        vcov = ~tinh),
+  feols(workerratio ~ log(tot_bmr_prov_ppn) + sex_ratio + dist_nearest_base_prov + dist_nearest_hochi_prov,
         subset(prov04_vhlss, south == 0),
         vcov = ~tinh),
-  feols(workerratio ~ log(tot_bmr_prov),
+  feols(workerratio ~ log(tot_bmr_prov_ppn) + sex_ratio + dist_nearest_base_prov + dist_nearest_hochi_prov,
         subset(prov06_vhlss, south == 0),
         vcov = ~tinh),
-  feols(workerratio ~ log(tot_bmr_prov),
+  feols(workerratio ~ log(tot_bmr_prov_ppn) + sex_ratio + dist_nearest_base_prov + dist_nearest_hochi_prov,
         subset(prov08_vhlss, south == 0),
         vcov = ~tinh),
-  feols(workerratio ~ log(tot_bmr_prov),
+  feols(workerratio ~ log(tot_bmr_prov_ppn) + sex_ratio + dist_nearest_base_prov + dist_nearest_hochi_prov,
+        subset(prov08_vhlss, south == 0),
+        vcov = ~tinh),
+  feols(workerratio ~ log(tot_bmr_prov_ppn) + sex_ratio + dist_nearest_base_prov + dist_nearest_hochi_prov,
         subset(prov10_vhlss, south == 0),
         vcov = ~tinh),
-  feols(workerratio ~ log(tot_bmr_prov),
+  feols(workerratio ~ log(tot_bmr_prov_ppn) + sex_ratio + dist_nearest_base_prov + dist_nearest_hochi_prov,
         subset(prov12_vhlss, south == 0),
         vcov = ~tinh)
 )
 
 vhlss_prov_coef_n <- lapply(vhlss_prov_n, tidy)
 vhlss_prov_coef_s <- lapply(vhlss_prov_s, tidy)
-vhlss_prov_coef_n <- do.call(rbind, vhlss_prov_coef_n) %>% filter(term != "(Intercept)")
-vhlss_prov_coef_s <- do.call(rbind, vhlss_prov_coef_s) %>% filter(term != "(Intercept)")
+vhlss_prov_coef_n <- do.call(rbind, vhlss_prov_coef_n) %>% filter(term == "log(tot_bmr_prov_ppn)")
+vhlss_prov_coef_s <- do.call(rbind, vhlss_prov_coef_s) %>% filter(term == "log(tot_bmr_prov_ppn)")
 vhlss_prov_coef_n$group <- "North"
 vhlss_prov_coef_s$group <- "South"
-vhlss_prov_coef_n$year <- rep(seq(2001, 2013, by = 2), length.out = nrow(vhlss_prov_coef_n))
-vhlss_prov_coef_s$year <- rep(seq(2001, 2013, by = 2), length.out = nrow(vhlss_prov_coef_s)) 
+vhlss_prov_coef_n$year <- rep(seq(2001, 2011, by = 2), length.out = nrow(vhlss_prov_coef_n))
+vhlss_prov_coef_s$year <- rep(seq(2001, 2011, by = 2), length.out = nrow(vhlss_prov_coef_s)) 
 vhlss_prov_coef_ns <- rbind(vhlss_prov_coef_n, vhlss_prov_coef_s)
 
-# Ratio of managers of HH businesses 
+# Ratio of formal workers 
 
+vhlss_formal_prov <- list(
+  feols(wagework_workerratio ~ log(tot_bmr_prov_ppn) + sex_ratio + dist_nearest_base_prov + dist_nearest_hochi_prov,
+        prov02_vhlss,
+        vcov = ~tinh),
+  feols(wagework_workerratio ~ log(tot_bmr_prov_ppn) + sex_ratio + dist_nearest_base_prov + dist_nearest_hochi_prov,
+        prov04_vhlss,
+        vcov = ~tinh),
+  feols(wagework_workerratio ~ log(tot_bmr_prov_ppn) + sex_ratio + dist_nearest_base_prov + dist_nearest_hochi_prov,
+        prov06_vhlss,
+        vcov = ~tinh),
+  feols(wagework_workerratio ~ log(tot_bmr_prov_ppn) + sex_ratio + dist_nearest_base_prov + dist_nearest_hochi_prov,
+        prov08_vhlss,
+        vcov = ~tinh),
+  feols(wagework_workerratio ~ log(tot_bmr_prov_ppn) + sex_ratio + dist_nearest_base_prov + dist_nearest_hochi_prov,
+        prov10_vhlss,
+        vcov = ~tinh),
+  feols(wagework_workerratio ~ log(tot_bmr_prov_ppn) + sex_ratio + dist_nearest_base_prov + dist_nearest_hochi_prov,
+        prov12_vhlss,
+        vcov = ~tinh)
+)
+
+vhlss_formal_prov_coef <- lapply(vhlss_formal_prov, tidy)
+vhlss_formal_prov_coef <- do.call(rbind, vhlss_formal_prov_coef) %>% filter(term == "log(tot_bmr_prov_ppn)")
+vhlss_formal_prov_coef$year <- rep(seq(2001, 2011, by = 2), length.out = nrow(vhlss_formal_prov_coef))
+
+vhlss_formal_prov_s <-  list(
+  feols(wagework_workerratio ~ log(tot_bmr_prov_ppn) + sex_ratio + dist_nearest_base_prov + dist_nearest_hochi_prov,
+        subset(prov02_vhlss, south == 1),
+        vcov = ~tinh),
+  feols(wagework_workerratio ~ log(tot_bmr_prov_ppn) + sex_ratio + dist_nearest_base_prov + dist_nearest_hochi_prov,
+        subset(prov04_vhlss, south == 1),
+        vcov = ~tinh),
+  feols(wagework_workerratio ~ log(tot_bmr_prov_ppn) + sex_ratio + dist_nearest_base_prov + dist_nearest_hochi_prov,
+        subset(prov06_vhlss, south == 1),
+        vcov = ~tinh),
+  feols(wagework_workerratio ~ log(tot_bmr_prov_ppn) + sex_ratio + dist_nearest_base_prov + dist_nearest_hochi_prov,
+        subset(prov08_vhlss, south == 1),
+        vcov = ~tinh),
+  feols(wagework_workerratio ~ log(tot_bmr_prov_ppn) + sex_ratio + dist_nearest_base_prov + dist_nearest_hochi_prov,
+        subset(prov10_vhlss, south == 1),
+        vcov = ~tinh),
+  feols(wagework_workerratio ~ log(tot_bmr_prov_ppn) + sex_ratio + dist_nearest_base_prov + dist_nearest_hochi_prov,
+        subset(prov12_vhlss, south == 1),
+        vcov = ~tinh)
+)
+
+vhlss_formal_prov_n <-  list(
+  feols(wagework_workerratio ~ log(tot_bmr_prov_ppn) + sex_ratio + dist_nearest_base_prov + dist_nearest_hochi_prov,
+        subset(prov02_vhlss, south == 0),
+        vcov = ~tinh),
+  feols(wagework_workerratio ~ log(tot_bmr_prov_ppn) + sex_ratio + dist_nearest_base_prov + dist_nearest_hochi_prov,
+        subset(prov04_vhlss, south == 0),
+        vcov = ~tinh),
+  feols(wagework_workerratio ~ log(tot_bmr_prov_ppn) + sex_ratio + dist_nearest_base_prov + dist_nearest_hochi_prov,
+        subset(prov06_vhlss, south == 0),
+        vcov = ~tinh),
+  feols(wagework_workerratio ~ log(tot_bmr_prov_ppn) + sex_ratio + dist_nearest_base_prov + dist_nearest_hochi_prov,
+        subset(prov08_vhlss, south == 0),
+        vcov = ~tinh),
+  feols(wagework_workerratio ~ log(tot_bmr_prov_ppn) + sex_ratio + dist_nearest_base_prov + dist_nearest_hochi_prov,
+        subset(prov08_vhlss, south == 0),
+        vcov = ~tinh),
+  feols(wagework_workerratio ~ log(tot_bmr_prov_ppn) + sex_ratio + dist_nearest_base_prov + dist_nearest_hochi_prov,
+        subset(prov10_vhlss, south == 0),
+        vcov = ~tinh),
+  feols(wagework_workerratio ~ log(tot_bmr_prov_ppn) + sex_ratio + dist_nearest_base_prov + dist_nearest_hochi_prov,
+        subset(prov12_vhlss, south == 0),
+        vcov = ~tinh)
+)
+
+vhlss_formal_prov_coef_n <- lapply(vhlss_formal_prov_n, tidy)
+vhlss_formal_prov_coef_s <- lapply(vhlss_formal_prov_s, tidy)
+vhlss_formal_prov_coef_n <- do.call(rbind, vhlss_formal_prov_coef_n) %>% filter(term == "log(tot_bmr_prov_ppn)")
+vhlss_formal_prov_coef_s <- do.call(rbind, vhlss_formal_prov_coef_s) %>% filter(term == "log(tot_bmr_prov_ppn)")
+vhlss_formal_prov_coef_n$group <- "North"
+vhlss_formal_prov_coef_s$group <- "South"
+vhlss_formal_prov_coef_n$year <- rep(seq(2001, 2011, by = 2), length.out = nrow(vhlss_formal_prov_coef_n))
+vhlss_formal_prov_coef_s$year <- rep(seq(2001, 2011, by = 2), length.out = nrow(vhlss_formal_prov_coef_s)) 
+vhlss_formal_prov_coef_ns <- rbind(vhlss_formal_prov_coef_n, vhlss_formal_prov_coef_s)
+
+# Self employed 
+
+vhlss_selfemp_prov <- list(
+  feols(selfemp_workerratio ~ log(tot_bmr_prov_ppn) + sex_ratio + dist_nearest_base_prov + dist_nearest_hochi_prov,
+        prov02_vhlss,
+        vcov = ~tinh),
+  feols(selfemp_workerratio ~ log(tot_bmr_prov_ppn) + sex_ratio + dist_nearest_base_prov + dist_nearest_hochi_prov,
+        prov04_vhlss,
+        vcov = ~tinh),
+  feols(selfemp_workerratio ~ log(tot_bmr_prov_ppn) + sex_ratio + dist_nearest_base_prov + dist_nearest_hochi_prov,
+        prov06_vhlss,
+        vcov = ~tinh),
+  feols(selfemp_workerratio ~ log(tot_bmr_prov_ppn) + sex_ratio + dist_nearest_base_prov + dist_nearest_hochi_prov,
+        prov08_vhlss,
+        vcov = ~tinh),
+  feols(selfemp_workerratio ~ log(tot_bmr_prov_ppn) + sex_ratio + dist_nearest_base_prov + dist_nearest_hochi_prov,
+        prov10_vhlss,
+        vcov = ~tinh),
+  feols(selfemp_workerratio ~ log(tot_bmr_prov_ppn) + sex_ratio + dist_nearest_base_prov + dist_nearest_hochi_prov,
+        prov12_vhlss,
+        vcov = ~tinh)
+)
+
+vhlss_selfemp_prov_coef <- lapply(vhlss_selfemp_prov, tidy)
+vhlss_selfemp_prov_coef <- do.call(rbind, vhlss_selfemp_prov_coef) %>% filter(term == "log(tot_bmr_prov_ppn)")
+vhlss_selfemp_prov_coef$year <- rep(seq(2001, 2011, by = 2), length.out = nrow(vhlss_selfemp_prov_coef))
+
+vhlss_selfemp_prov_s <-  list(
+  feols(selfemp_workerratio ~ log(tot_bmr_prov_ppn) + sex_ratio + dist_nearest_base_prov + dist_nearest_hochi_prov,
+        subset(prov02_vhlss, south == 1),
+        vcov = ~tinh),
+  feols(selfemp_workerratio ~ log(tot_bmr_prov_ppn) + sex_ratio + dist_nearest_base_prov + dist_nearest_hochi_prov,
+        subset(prov04_vhlss, south == 1),
+        vcov = ~tinh),
+  feols(selfemp_workerratio ~ log(tot_bmr_prov_ppn) + sex_ratio + dist_nearest_base_prov + dist_nearest_hochi_prov,
+        subset(prov06_vhlss, south == 1),
+        vcov = ~tinh),
+  feols(selfemp_workerratio ~ log(tot_bmr_prov_ppn) + sex_ratio + dist_nearest_base_prov + dist_nearest_hochi_prov,
+        subset(prov08_vhlss, south == 1),
+        vcov = ~tinh),
+  feols(selfemp_workerratio ~ log(tot_bmr_prov_ppn) + sex_ratio + dist_nearest_base_prov + dist_nearest_hochi_prov,
+        subset(prov10_vhlss, south == 1),
+        vcov = ~tinh),
+  feols(selfemp_workerratio ~ log(tot_bmr_prov_ppn) + sex_ratio + dist_nearest_base_prov + dist_nearest_hochi_prov,
+        subset(prov12_vhlss, south == 1),
+        vcov = ~tinh)
+)
+
+vhlss_selfemp_prov_n <-  list(
+  feols(selfemp_workerratio ~ log(tot_bmr_prov_ppn) + sex_ratio + dist_nearest_base_prov + dist_nearest_hochi_prov,
+        subset(prov02_vhlss, south == 0),
+        vcov = ~tinh),
+  feols(selfemp_workerratio ~ log(tot_bmr_prov_ppn) + sex_ratio + dist_nearest_base_prov + dist_nearest_hochi_prov,
+        subset(prov04_vhlss, south == 0),
+        vcov = ~tinh),
+  feols(selfemp_workerratio ~ log(tot_bmr_prov_ppn) + sex_ratio + dist_nearest_base_prov + dist_nearest_hochi_prov,
+        subset(prov06_vhlss, south == 0),
+        vcov = ~tinh),
+  feols(selfemp_workerratio ~ log(tot_bmr_prov_ppn) + sex_ratio + dist_nearest_base_prov + dist_nearest_hochi_prov,
+        subset(prov08_vhlss, south == 0),
+        vcov = ~tinh),
+  feols(selfemp_workerratio ~ log(tot_bmr_prov_ppn) + sex_ratio + dist_nearest_base_prov + dist_nearest_hochi_prov,
+        subset(prov08_vhlss, south == 0),
+        vcov = ~tinh),
+  feols(selfemp_workerratio ~ log(tot_bmr_prov_ppn) + sex_ratio + dist_nearest_base_prov + dist_nearest_hochi_prov,
+        subset(prov10_vhlss, south == 0),
+        vcov = ~tinh),
+  feols(wagework_workerratio ~ log(tot_bmr_prov_ppn) + sex_ratio + dist_nearest_base_prov + dist_nearest_hochi_prov,
+        subset(prov12_vhlss, south == 0),
+        vcov = ~tinh)
+)
+
+vhlss_selfemp_prov_coef_n <- lapply(vhlss_selfemp_prov_n, tidy)
+vhlss_selfemp_prov_coef_s <- lapply(vhlss_selfemp_prov_s, tidy)
+vhlss_selfemp_prov_coef_n <- do.call(rbind, vhlss_selfemp_prov_coef_n) %>% filter(term == "log(tot_bmr_prov_ppn)")
+vhlss_selfemp_prov_coef_s <- do.call(rbind, vhlss_selfemp_prov_coef_s) %>% filter(term == "log(tot_bmr_prov_ppn)")
+vhlss_selfemp_prov_coef_n$group <- "North"
+vhlss_selfemp_prov_coef_s$group <- "South"
+vhlss_selfemp_prov_coef_n$year <- rep(seq(2001, 2011, by = 2), length.out = nrow(vhlss_selfemp_prov_coef_n))
+vhlss_selfemp_prov_coef_s$year <- rep(seq(2001, 2011, by = 2), length.out = nrow(vhlss_selfemp_prov_coef_s)) 
+vhlss_selfemp_prov_coef_ns <- rbind(vhlss_selfemp_prov_coef_n, vhlss_selfemp_prov_coef_s)
 
 ########################################
 # VHLSS REGRESSIONS - INDIVIDUAL LEVEL #
