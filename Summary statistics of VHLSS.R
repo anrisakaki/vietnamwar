0d1a113441e165f3aa34837787892a8c924dd85d
Anri Sakakibara <anrisakaki@gmail.com>
2024-04-26 13:29:16 +0100

    Summary statistics of VHLSS

--- Summary statistics of VHLSS.R
+++ Summary statistics of VHLSS.R
@@ -4,13 +4,20 @@ for (i in vhlss) {
   load(i)
 }
 
-####################################
-# Calculating sex ratio of workers #
-####################################
+prov_sum_fn <- function (i) {
+  i %>% 
+    mutate(
+      sex_ratio = m_total / f_total,
+      workerratio = m_workers / f_workers,
+      selfemp_workerratio = mself_emp / fself_emp,
+      flfp = f_workers / f_total,
+      wagework_workerratio = mwagework/fwagework
+    )
+  }
 
 prov02_vhlss <- vhlss02 %>% 
   filter(!is.na(female), age > 15 & age < 65) %>% 
-  group_by(tinh02) %>% 
+  group_by(tinh) %>% 
   summarise(
     m_total = sum(wt75 * (female == 0), na.rm = T),
     f_total = sum(wt75 * (female == 1), na.rm = T),
@@ -18,22 +25,17 @@ prov02_vhlss <- vhlss02 %>%
     f_workers = sum(wt75 * (female == 1 & work == 1), na.rm = T),
     fself_emp = sum(wt75 * (female == 1 & selfemp == 1), na.rm = T),
     mself_emp = sum(wt75 * (female == 0 & selfemp == 1), na.rm = T),
-    tot_bmr_prov = mean(tot_bmr_prov),
-    tot_bmr_lb_prov = mean(tot_bmr_lb_prov),
-    killed_tot_prov = mean(killed_tot_prov),
-    dist_nearest_base_prov = mean(dist_nearest_base_prov),
-    dist_nearest_hochi_prov = mean(dist_nearest_hochi_prov),
+    mwagework = sum(wt75 * (female == 0 & wagework == 1), na.rm = T),
+    fwagework = sum(wt75 * (female == 1 & wagework == 1), na.rm = T)
   ) %>% 
-  mutate(
-    sex_ratio = m_total / f_total,
-    workerratio = m_workers / f_workers,
-    selfemp_workerratio = mself_emp / fself_emp,
-    flfp = f_workers / f_total,
-    south = ifelse(tinh02 > 407, 1, 0)
-  )
+  left_join(province_bmr_sum, by = "tinh") %>% 
+  prov_sum_fn () %>% 
+  mutate(south = ifelse(tinh > 407, 1, 0))
 
 prov04_vhlss <- vhlss04 %>% 
-  mutate(m_manager = ifelse(f_manager == 0, 1, 0)) %>% 
+  mutate(tinh = ifelse(tinh == 302, 301, tinh),
+         tinh = ifelse(tinh == 606, 605, tinh),
+         tinh = ifelse(tinh == 816, 815, tinh)) %>% 
   filter(!is.na(female), age > 15 & age < 65) %>% 
   group_by(tinh) %>% 
   summarise(
@@ -43,74 +45,57 @@ prov04_vhlss <- vhlss04 %>%
     f_workers = sum(wt45 * (female == 1 & work == 1), na.rm = T),
     fself_emp = sum(wt45 * (female == 1 & selfemp == 1), na.rm = T),
     mself_emp = sum(wt45 * (female == 0 & selfemp == 1), na.rm = T),
-    f_manager = sum(wt45 * (f_manager == 1), na.rm = T),
-    m_manager = sum(wt45 * (m_manager == 1), na.rm = T),
-    tot_bmr_prov = mean(tot_bmr_prov),
-    tot_bmr_lb_prov = mean(tot_bmr_lb_prov),
-    killed_tot_prov = mean(killed_tot_prov)
+    mwagework = sum(wt45 * (female == 0 & wagework == 1), na.rm = T),
+    fwagework = sum(wt45 * (female == 1 & wagework == 1), na.rm = T)
   ) %>% 
-  mutate(
-    sex_ratio = m_total / f_total,
-    workerratio = m_workers / f_workers,
-    selfemp_workerratio = mself_emp / fself_emp,
-    manager_workerratio = m_manager/f_manager,
-    flfp = f_workers / f_total,
-    south = ifelse(tinh > 407, 1, 0)
-  )
+  prov_sum_fn () %>% 
+  left_join(province_bmr_sum, by = "tinh") %>% 
+  mutate(south = ifelse(tinh > 407, 1, 0))
 
 prov06_vhlss <- vhlss06 %>% 
-  mutate(m_manager = ifelse(f_manager == 0, 1, 0)) %>% 
+  mutate(tinh = ifelse(tinh == 302, 301, tinh),
+         tinh = ifelse(tinh == 606, 605, tinh),
+         tinh = ifelse(tinh == 816, 815, tinh)) %>% 
   filter(!is.na(female), age > 15 & age < 65) %>% 
   group_by(tinh) %>% 
   summarise(
     m_total = sum(wt45 * (female == 0), na.rm = T),
     f_total = sum(wt45 * (female == 1), na.rm = T),
     m_workers = sum(wt45 * (female == 0 & work == 1), na.rm = T),
-    fself_emp = sum(wt45 * (female == 1 & selfemp == 1), na.rm = T),
     f_workers = sum(wt45 * (female == 1 & work == 1), na.rm = T),
+    fself_emp = sum(wt45 * (female == 1 & selfemp == 1), na.rm = T),
     mself_emp = sum(wt45 * (female == 0 & selfemp == 1), na.rm = T),
-    f_manager = sum(wt45 * (f_manager == 1), na.rm = T),
-    m_manager = sum(wt45 * (m_manager == 1), na.rm = T),
-    tot_bmr_prov = mean(tot_bmr_prov),
-    tot_bmr_lb_prov = mean(tot_bmr_lb_prov),
-    killed_tot_prov = mean(killed_tot_prov)
+    mwagework = sum(wt45 * (female == 0 & wagework == 1), na.rm = T),
+    fwagework = sum(wt45 * (female == 1 & wagework == 1), na.rm = T)
   ) %>% 
-  mutate(
-    sex_ratio = m_total / f_total,
-    workerratio = m_workers / f_workers,
-    selfemp_workerratio = mself_emp / fself_emp,
-    manager_workerratio = m_manager/f_manager,
-    flfp = f_workers / f_total,
-    south = ifelse(tinh > 407, 1, 0)
-  )
+  left_join(province_bmr_sum, by = "tinh") %>% 
+  prov_sum_fn () %>% 
+  mutate(south = ifelse(tinh > 407, 1, 0))
 
 prov08_vhlss <- vhlss08 %>% 
-  mutate(m_manager = ifelse(f_manager == 0, 1, 0)) %>% 
+  mutate(tinh = ifelse(tinh == 302, 301, tinh),
+         tinh = ifelse(tinh == 606, 605, tinh),
+         tinh = ifelse(tinh == 816, 815, tinh)) %>% 
   filter(!is.na(female), age > 15 & age < 65) %>% 
   group_by(tinh) %>% 
   summarise(
     m_total = sum(wt9 * (female == 0), na.rm = T),
     f_total = sum(wt9 * (female == 1), na.rm = T),
     m_workers = sum(wt9 * (female == 0 & work == 1), na.rm = T),
-    fself_emp = sum(wt9 * (female == 1 & selfemp == 1), na.rm = T),
     f_workers = sum(wt9 * (female == 1 & work == 1), na.rm = T),
+    fself_emp = sum(wt9 * (female == 1 & selfemp == 1), na.rm = T),
     mself_emp = sum(wt9 * (female == 0 & selfemp == 1), na.rm = T),
-    f_manager = sum(wt9 * (f_manager == 1), na.rm = T),
-    m_manager = sum(wt9 * (m_manager == 1), na.rm = T),
-    tot_bmr_prov = mean(tot_bmr_prov),
-    tot_bmr_lb_prov = mean(tot_bmr_lb_prov),
-    killed_tot_prov = mean(killed_tot_prov)
+    mwagework = sum(wt9 * (female == 0 & wagework == 1), na.rm = T),
+    fwagework = sum(wt9 * (female == 1 & wagework == 1), na.rm = T)
   ) %>% 
-  mutate(
-    sex_ratio = m_total / f_total,
-    workerratio = m_workers / f_workers,
-    selfemp_workerratio = mself_emp / fself_emp,
-    manager_workerratio = m_manager/f_manager,
-    flfp = f_workers / f_total,
-    south = ifelse(tinh > 407, 1, 0)
-  )
+  left_join(province_bmr_sum, by = "tinh") %>% 
+  prov_sum_fn() %>% 
+  mutate(south = ifelse(tinh > 407, 1, 0))
 
 prov10_vhlss <- vhlss10 %>% 
+  mutate(tinh = ifelse(tinh == 67, 66, tinh),
+         tinh = ifelse(tinh == 11, 12, tinh),
+         tinh = ifelse(tinh == 93, 92, tinh)) %>% 
   filter(!is.na(female), age > 15 & age < 65) %>% 
   group_by(tinh) %>% 
   summarise(
@@ -120,19 +105,17 @@ prov10_vhlss <- vhlss10 %>%
     fself_emp = sum(wt9 * (female == 1 & selfemp == 1), na.rm = T),
     f_workers = sum(wt9 * (female == 1 & work == 1), na.rm = T),
     mself_emp = sum(wt9 * (female == 0 & selfemp == 1), na.rm = T),
-    tot_bmr_prov = mean(tot_bmr_prov),
-    tot_bmr_lb_prov = mean(tot_bmr_lb_prov),
-    killed_tot_prov = mean(killed_tot_prov)
+    mwagework = sum(wt9 * (female == 0 & wagework == 1), na.rm = T),
+    fwagework = sum(wt9 * (female == 1 & wagework == 1), na.rm = T)
   ) %>% 
-  mutate(
-    sex_ratio = m_total / f_total,
-    workerratio = m_workers / f_workers,
-    selfemp_workerratio = mself_emp / fself_emp,
-    flfp = f_workers / f_total,
-    south = ifelse(tinh > 44, 1, 0)
-  )
+  left_join(province_bmr_sum2, by = "tinh") %>% 
+  prov_sum_fn() %>% 
+  mutate(south = ifelse(tinh > 44, 1, 0))  
 
 prov12_vhlss <- vhlss12 %>% 
+  mutate(tinh = ifelse(tinh == 67, 66, tinh),
+         tinh = ifelse(tinh == 11, 12, tinh),
+         tinh = ifelse(tinh == 93, 92, tinh)) %>% 
   filter(!is.na(female), age > 15 & age < 65) %>% 
   group_by(tinh) %>% 
   summarise(
@@ -142,17 +125,12 @@ prov12_vhlss <- vhlss12 %>%
     fself_emp = sum(wt9 * (female == 1 & selfemp == 1), na.rm = T),
     f_workers = sum(wt9 * (female == 1 & work == 1), na.rm = T),
     mself_emp = sum(wt9 * (female == 0 & selfemp == 1), na.rm = T),
-    tot_bmr_prov = mean(tot_bmr_prov),
-    tot_bmr_lb_prov = mean(tot_bmr_lb_prov),
-    killed_tot_prov = mean(killed_tot_prov)
+    mwagework = sum(wt9 * (female == 0 & wagework == 1), na.rm = T),
+    fwagework = sum(wt9 * (female == 1 & wagework == 1), na.rm = T)
   ) %>% 
-  mutate(
-    sex_ratio = m_total / f_total,
-    workerratio = m_workers / f_workers,
-    selfemp_workerratio = mself_emp / fself_emp,
-    flfp = f_workers / f_total,
-    south = ifelse(tinh > 44, 1, 0)
-  )
+  left_join(province_bmr_sum2, by = "tinh") %>% 
+  prov_sum_fn() %>% 
+  mutate(south = ifelse(tinh > 44, 1, 0)) 
 
 save(prov02_vhlss, file = "prov02_vhlss.Rda")
 save(prov04_vhlss, file = "prov04_vhlss.Rda")
