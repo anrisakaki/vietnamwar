0db13a1184d79bbbd87556271583f9ac1680da89
Anri Sakakibara <anrisakaki@gmail.com>
2024-04-26 13:28:47 +0100

    Coefficient plots for VHLSS Regressions

--- Coefficient plots for VHLSS Regressions.R
+++ Coefficient plots for VHLSS Regressions.R
@@ -1,8 +1,8 @@
-##############################
-# PROVINCE-LEVEL REGRESSIONS #
-##############################
+############################################
+# PROVINCE-LEVEL REGRESSIONS - ALL WORKERS #
+############################################
 
-ggplot(vhlss_prov_coef, aes(x = factor(year), y = estimate*100, ymin = (estimate - std.error)*100, ymax = (estimate + std.error)*100)) +
+ggplot(vhlss_prov_coef, aes(x = factor(year), y = estimate, ymin = estimate - std.error, ymax = estimate + std.error)) +
   geom_pointrange() +
   geom_hline(yintercept = 0, linetype = "dotted", color = "black") +
   labs(x = "Year", y = "Estimate and 95% Interval") +
@@ -19,7 +19,7 @@ ggplot(vhlss_prov_coef, aes(x = factor(year), y = estimate*100, ymin = (estimate
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
 ggsave("vhlss_prov_coef.jpeg", width = 7, height = 7)
 
-ggplot(vhlss_prov_coef_ns, aes(x = factor(year), y = estimate*100, ymin = (estimate - std.error)*100, ymax = (estimate + std.error)*100, color = group)) +
+ggplot(vhlss_prov_coef_ns, aes(x = factor(year), y = (estimate), ymin = ((estimate - std.error)), ymax = ((estimate + std.error)), color = group)) +
   geom_pointrange() +
   geom_hline(yintercept = 0, linetype = "dotted", color = "black") +
   labs(x = "Year", y = "Estimate and 95% Interval", color = "Region") +
@@ -36,4 +36,75 @@ ggplot(vhlss_prov_coef_ns, aes(x = factor(year), y = estimate*100, ymin = (estim
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
 ggsave("vhlss_prov_coef_ns.jpeg", width = 7, height = 7)
 
+###############################################
+# PROVINCE-LEVEL REGRESSIONS - FORMAL WORKERS #
+###############################################
+
+ggplot(vhlss_formal_prov_coef, aes(x = factor(year), y = estimate, ymin = estimate - std.error, ymax = estimate + std.error)) +
+  geom_pointrange() +
+  geom_hline(yintercept = 0, linetype = "dotted", color = "black") +
+  labs(x = "Year", y = "Estimate and 95% Interval") +
+  ggtitle("") +
+  theme_minimal() +
+  guides(fill = "none") +  
+  theme(axis.line = element_line(color='black'),
+        plot.background = element_blank(),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank(),
+        legend.title=element_blank(),
+        text = element_text(size=10)) + 
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+
+ggplot(vhlss_formal_prov_coef_ns, aes(x = factor(year), y = (estimate), ymin = ((estimate - std.error)), ymax = ((estimate + std.error)), color = group)) +
+  geom_pointrange() +
+  geom_hline(yintercept = 0, linetype = "dotted", color = "black") +
+  labs(x = "Year", y = "Estimate and 95% Interval", color = "Region") +
+  ggtitle("") +
+  theme_minimal() +
+  guides(fill = "none") +  
+  theme(axis.line = element_line(color='black'),
+        plot.background = element_blank(),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank(),
+        legend.title=element_blank(),
+        text = element_text(size=10)) + 
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+
+###############################################
+# PROVINCE-LEVEL REGRESSIONS - FORMAL WORKERS #
+###############################################
+
+ggplot(vhlss_selfemp_prov_coef, aes(x = factor(year), y = estimate, ymin = estimate - std.error, ymax = estimate + std.error)) +
+  geom_pointrange() +
+  geom_hline(yintercept = 0, linetype = "dotted", color = "black") +
+  labs(x = "Year", y = "Estimate and 95% Interval") +
+  ggtitle("") +
+  theme_minimal() +
+  guides(fill = "none") +  
+  theme(axis.line = element_line(color='black'),
+        plot.background = element_blank(),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank(),
+        legend.title=element_blank(),
+        text = element_text(size=10)) + 
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+
+ggplot(vhlss_selfemp_prov_coef_ns, aes(x = factor(year), y = (estimate), ymin = ((estimate - std.error)), ymax = ((estimate + std.error)), color = group)) +
+  geom_pointrange() +
+  geom_hline(yintercept = 0, linetype = "dotted", color = "black") +
+  labs(x = "Year", y = "Estimate and 95% Interval", color = "Region") +
+  ggtitle("") +
+  theme_minimal() +
+  guides(fill = "none") +  
+  theme(axis.line = element_line(color='black'),
+        plot.background = element_blank(),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank(),
+        legend.title=element_blank(),
+        text = element_text(size=10)) + 
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
 
