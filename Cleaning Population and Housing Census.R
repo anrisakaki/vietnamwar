a5e0e4f48299640e0b37cc576ab921bd5211a39d
Anri Sakakibara <anrisakaki@gmail.com>
2024-04-29 15:41:45 +0100

    Cleaning PHC

--- Cleaning Population and Housing Census.R
+++ Cleaning Population and Housing Census.R
@@ -15,8 +15,14 @@ phc <- phc %>%
          disabled = ifelse(disabled == 1, 1, 0),
          female = ifelse(sex == 2, 1, 0),
          agri = ifelse(indgen == 10, 1, 0),
+         housework = ifelse(empstatd == 310, 1, 0),
+         manu = ifelse(indgen == 30, 1, 0),
          migration = ifelse(migrate5 == 10 | migrate5 == 11 | migrate5 == 12, 0, 1),
-         age = ifelse(age == 999, NA, age)) %>% 
+         age = ifelse(age == 999, NA, age),
+         work = ifelse(work == 0 & age < 17 | work == 0 & age > 64, NA, work),
+         agri = ifelse(agri == 0 & age < 17 | agri == 0 & age > 64, NA, agri),
+         manu = ifelse(manu == 0 & age < 17 | manu == 0 & age > 64, NA, manu),
+         housework = ifelse(housework == 0 & age < 17 | housework == 0 & age > 64, NA, housework)) %>% 
   mutate(
     age_cohort = case_when(
       age <= 4 ~ "0-4",
@@ -40,23 +46,30 @@ save(phc, file = "phc.Rda")
 
 phc89 <- phc %>% 
   filter(year == 1989) %>% 
-  select(year, serial, hhwt, geo1_vn, geo1_vn1989, regnvn, pernum, perwt, nchild, age, age_cohort, age_cohort75, female, marst, married, widowed,
-         birthyr, minority, migration, literate, work, edattain, yrschool, labforce, occisco, indgen, agri, empsect, ind, geomig1_5)
-phc89 <- left_join(phc89, bombs_province89, by = c("year", "geo1_vn1989"))
+  mutate(geo1_vn1989 = ifelse(geo1_vn1989 == 16, 13, geo1_vn1989)) %>% 
+  select(year, serial, hhwt, geo1_vn, geo1_vn1989, regnvn, pernum, perwt, nchild, age, age_cohort, female, marst, married, widowed,
+         birthyr, minority, migration, literate, work, edattain, yrschool, labforce, occisco, indgen, agri, manu, housework, empsect, ind, geomig1_5) %>% 
+  left_join(bombs_province89, by = "geo1_vn1989") %>% 
+  mutate(south = ifelse(geo1_vn1989 > 26 | geo1_vn1989 == 2, 1, 0))
 
 phc99 <- phc %>% 
   filter(year == 1999) %>% 
-  # Ha Tay merged with Hanoi 
-  mutate(geo1_vn1999 = ifelse(geo1_vn1999 == 105, 101, geo1_vn1999)) %>% 
-  select(year, serial, hhwt, geo1_vn, geo1_vn1999, regnvn, pernum, perwt, nchild, age, age_cohort, age_cohort75, female, marst, married, widowed,
-         birthyr, minority, migration, literate, work, edattain, yrschool, labforce, occisco, indgen, agri, empsect, ind, geomig1_5)
-phc99 <- left_join(phc99, bombs_province99, by = c("year", "geo1_vn1999"))
+  mutate(geo1_vn1999 = ifelse(geo1_vn1999 == 105, 101, geo1_vn1999),
+         geo1_vn1999 = ifelse(geo1_vn1999 == 303, 301, geo1_vn1999)) %>% 
+  select(year, serial, hhwt, geo1_vn, geo1_vn1999, regnvn, pernum, perwt, nchild, age, age_cohort, female, marst, married, widowed,
+         birthyr, minority, migration, literate, work, edattain, yrschool, labforce, occisco, indgen, agri, manu, housework, empsect, ind, geomig1_5) %>% 
+  left_join(bombs_province99, by = "geo1_vn1999") %>% 
+  mutate(south = ifelse(geo1_vn1999 > 405, 1, 0))
+
 
 phc09 <- phc %>% 
   filter(year == 2009) %>% 
-  select(year, serial, hhwt, geo1_vn, geo1_vn2009, regnvn, pernum, perwt, nchild, age, age_cohort, age_cohort75, female, marst, married, widowed,
-         birthyr, minority, migration, literate, work, edattain, yrschool, labforce, occisco, indgen, agri, empsect, ind, geomig1_5)
-phc09 <- left_join(phc09, bombs_province09, by = c("year", "geo1_vn2009"))
+  mutate(geo1_vn2009 = ifelse(geo1_vn2009 == 11, 12, geo1_vn2009),
+         geo1_vn2009 = ifelse(geo1_vn2009 == 14, 12, geo1_vn2009)) %>% 
+  select(year, serial, hhwt, geo1_vn, geo1_vn2009, regnvn, pernum, perwt, nchild, age, age_cohort, female, marst, married, widowed,
+         birthyr, minority, migration, literate, work, edattain, yrschool, labforce, occisco, indgen, agri, manu, housework, empsect, ind, geomig1_5) %>% 
+  left_join(bombs_province09, by = "geo1_vn2009") %>% 
+  mutate(south = ifelse(geo1_vn2009 > 44, 1, 0))
 
 save(phc89, file = "phc89.Rda")
 save(phc99, file = "phc99.Rda")
