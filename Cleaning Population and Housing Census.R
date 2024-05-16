86de84078a824fc465074c4ac263385fbf842ad9
Anri Sakakibara <anrisakaki@gmail.com>
2024-05-16 11:09:07 +0100

    Cleaning PHC

--- Cleaning Population and Housing Census.R
+++ Cleaning Population and Housing Census.R
@@ -9,6 +9,7 @@ for (i in provbombs) {
 phc <- phc %>%
   mutate(married = ifelse(marst == 2, 1, 0),
          widowed = ifelse(marst == 4, 1, 0),
+         single = ifelse(marst == 1, 1, 0),
          minority = ifelse(ethnicvn > 1, 1, 0),
          literate = ifelse(lit == 2, 1, 0),
          work = ifelse(empstat == 1, 1, 0),
@@ -41,7 +42,7 @@ phc <- phc %>%
       age <= 54 ~ "50-54",
       age <= 59 ~ "55-59",
       age <= 64 ~ "60-64",
-      age <= 100 ~ "65+"))  
+      age <= 100 ~ "65+"))
 
 save(phc, file = "phc.Rda")
 
@@ -50,29 +51,37 @@ save(phc, file = "phc.Rda")
 phc89 <- phc %>% 
   filter(year == 1989) %>% 
   mutate(geo1_vn1989 = ifelse(geo1_vn1989 == 16, 13, geo1_vn1989)) %>% 
-  select(year, serial, hhwt, geo1_vn, geo1_vn1989, regnvn, pernum, perwt, nchild, age, age_cohort, female, marst, married, widowed,
+  select(year, serial, hhwt, geo1_vn, geo1_vn1989, regnvn, pernum, perwt, nchild, relate, famsize, age, age_cohort, female, marst, married, widowed, single,
          birthyr, minority, migration, literate, work, edattain, yrschool, labforce, occisco, indgen, agri, manu, housework, empsect, self, ind, geomig1_5, urban, popdensgeo1) %>% 
   left_join(bombs_province89, by = "geo1_vn1989") %>% 
-  mutate(south = ifelse(geo1_vn1989 > 26 | geo1_vn1989 == 2, 1, 0))
+  mutate(south = ifelse(geo1_vn1989 > 26 | geo1_vn1989 == 2, 1, 0))  %>% 
+  group_by(serial) %>% 
+  mutate(widow_hh = ifelse(any(widowed == 1 & female == 1 & age > 38 & age < 69), 1, 0),
+         widow_hh = ifelse(is.na(widow_hh), 0, widow_hh))
 
 phc99 <- phc %>% 
   filter(year == 1999) %>% 
   mutate(geo1_vn1999 = ifelse(geo1_vn1999 == 105, 101, geo1_vn1999),
          geo1_vn1999 = ifelse(geo1_vn1999 == 303, 301, geo1_vn1999)) %>% 
-  select(year, serial, hhwt, geo1_vn, geo1_vn1999, regnvn, pernum, perwt, nchild, age, age_cohort, female, marst, married, widowed,
+  select(year, serial, hhwt, geo1_vn, geo1_vn1999, regnvn, pernum, perwt, nchild, relate, age, age_cohort, female, marst, married, widowed, single,
          birthyr, minority, migration, literate, work, edattain, yrschool, labforce, occisco, indgen, agri, manu, housework, empsect, self, ind, geomig1_5, urban, popdensgeo1) %>% 
   left_join(bombs_province99, by = "geo1_vn1999") %>% 
-  mutate(south = ifelse(geo1_vn1999 > 405, 1, 0))
-
+  mutate(south = ifelse(geo1_vn1999 > 405, 1, 0))  %>% 
+  group_by(serial) %>% 
+  mutate(widow_hh = ifelse(any(widowed == 1 & female == 1 & age > 48 & age < 79), 1, 0),
+         widow_hh = ifelse(is.na(widow_hh), 0, widow_hh))
 
 phc09 <- phc %>% 
   filter(year == 2009) %>% 
   mutate(geo1_vn2009 = ifelse(geo1_vn2009 == 11, 12, geo1_vn2009),
          geo1_vn2009 = ifelse(geo1_vn2009 == 14, 12, geo1_vn2009)) %>% 
-  select(year, serial, hhwt, geo1_vn, geo1_vn2009, regnvn, pernum, perwt, nchild, age, age_cohort, female, marst, married, widowed,
+  select(year, serial, hhwt, geo1_vn, geo1_vn2009, regnvn, pernum, perwt, nchild, relate, age, age_cohort, female, marst, married, widowed, single,
          birthyr, minority, migration, literate, work, edattain, yrschool, labforce, occisco, indgen, agri, manu, housework, empsect, self, ind, geomig1_5, urban, popdensgeo1) %>% 
   left_join(bombs_province09, by = "geo1_vn2009") %>% 
-  mutate(south = ifelse(geo1_vn2009 > 44, 1, 0))
+  mutate(south = ifelse(geo1_vn2009 > 44, 1, 0))%>% 
+  group_by(serial) %>% 
+  mutate(widow_hh = ifelse(any(widowed == 1 & female == 1 & age > 58 & age < 89), 1, 0),
+         widow_hh = ifelse(is.na(widow_hh), 0, widow_hh))
 
 save(phc89, file = "phc89.Rda")
 save(phc99, file = "phc99.Rda")
