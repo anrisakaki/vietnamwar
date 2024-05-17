c11a2961f959b7106904597c11bd0449374fa37e
Anri Sakakibara <anrisakaki@gmail.com>
2024-05-17 17:22:40 +0100

    PHC Regressions

--- PHC Regressions.R
+++ PHC Regressions.R
@@ -73,15 +73,15 @@ dev.off()
 png("agri_phc.png")
 iplot(list(
   feols(agri ~ as.factor(female) + i(as.factor(female), log(tot_bmr_prov_ppn)) + yrschool + nchild + age + age^2 + dist_nearest_base_prov + dist_nearest_hochi_prov + log(popdensgeo1) | age + regnvn + urban + minority + married,
-        subset(phc89, work == 1 & migration == 0),
+        subset(phc89, migration == 0),
         weights = ~perwt,
         vcov = ~geo1_vn1989),
   feols(agri ~ as.factor(female) + i(as.factor(female), log(tot_bmr_prov_ppn)) + yrschool + nchild + age + age^2 + dist_nearest_base_prov + dist_nearest_hochi_prov + log(popdensgeo1) | age + regnvn + urban + minority + married,
-        subset(phc99, work == 1 & migration == 0),
+        subset(phc99, migration == 0),
         weights = ~perwt,
         vcov = ~geo1_vn1999),
   feols(agri ~ as.factor(female) + i(as.factor(female), log(tot_bmr_prov_ppn)) + yrschool + nchild + age + age^2 + dist_nearest_base_prov + dist_nearest_hochi_prov + log(popdensgeo1) | age + regnvn + urban + minority + married,
-        subset(phc09, work == 1 & migration == 0),
+        subset(phc09, migration == 0),
         weights = ~perwt,
         vcov = ~geo1_vn2009)))
 legend("bottomleft", col = 1:3, pch = 16, bty = "n", cex = 0.9, 
@@ -91,15 +91,15 @@ dev.off()
 png("agri_phc_n.png")
 iplot(list(
   feols(agri ~ as.factor(female) + i(as.factor(female), log(tot_bmr_prov_ppn)) + yrschool + nchild + age + age^2 + dist_nearest_base_prov + dist_nearest_hochi_prov + log(popdensgeo1) | regnvn + urban + minority + married,
-        subset(phc89, work == 1 & south == 0 & migration == 0),
+        subset(phc89, south == 0 & migration == 0),
         weights = ~perwt,
         vcov = ~geo1_vn1989),
   feols(agri ~ as.factor(female) + i(as.factor(female), log(tot_bmr_prov_ppn)) + yrschool + nchild + age + age^2 + dist_nearest_base_prov + dist_nearest_hochi_prov + log(popdensgeo1) | regnvn + urban + minority + married,
-        subset(phc99, work == 1 & south == 0 & migration == 0),
+        subset(phc99, south == 0 & migration == 0),
         weights = ~perwt,
         vcov = ~geo1_vn1999),
   feols(agri ~ as.factor(female) + i(as.factor(female), log(tot_bmr_prov_ppn)) + yrschool + nchild + age + age^2 + dist_nearest_base_prov + dist_nearest_hochi_prov + log(popdensgeo1) | regnvn + urban + minority + married,
-        subset(phc09, work == 1 & south == 0 & migration == 0),
+        subset(phc09, south == 0 & migration == 0),
         weights = ~perwt,
         vcov = ~geo1_vn2009)))
 legend("bottomleft", col = 1:3, pch = 16, bty = "n", cex = 0.9, 
@@ -109,15 +109,15 @@ dev.off()
 png("agri_phc_s.png")
 iplot(list(
   feols(agri ~ as.factor(female) + i(as.factor(female), log(tot_bmr_prov_ppn)) + yrschool + nchild + age + age^2 + dist_nearest_base_prov + dist_nearest_hochi_prov + log(popdensgeo1) | regnvn + urban + minority + married,
-        subset(phc89, work == 1 & south == 1 & migration == 0),
+        subset(phc89, south == 1 & migration == 0),
         weights = ~perwt,
         vcov = ~geo1_vn1989),
   feols(agri ~ as.factor(female) + i(as.factor(female), log(tot_bmr_prov_ppn)) + yrschool + nchild + age + age^2 + dist_nearest_base_prov + dist_nearest_hochi_prov + log(popdensgeo1) | regnvn + urban + minority + married,
-        subset(phc99, work == 1 & south == 1 & migration == 0),
+        subset(phc99, south == 1 & migration == 0),
         weights = ~perwt,
         vcov = ~geo1_vn1999),
   feols(agri ~ as.factor(female) + i(as.factor(female), log(tot_bmr_prov_ppn)) + yrschool + nchild + age + age^2 + dist_nearest_base_prov + dist_nearest_hochi_prov + log(popdensgeo1) | regnvn + urban + minority + married,
-        subset(phc09, work == 1 & south == 1 & migration == 0),
+        subset(phc09, south == 1 & migration == 0),
         weights = ~perwt,
         vcov = ~geo1_vn2009)))
 legend("bottomleft", col = 1:3, pch = 16, bty = "n", cex = 0.9, 
