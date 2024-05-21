73e1bbed3ab755ee86ddab7c2e8cb8b8fb8005cb
Anri Sakakibara <anrisakaki@gmail.com>
2024-05-21 17:04:15 +0100

    Coefficient plots for VEC Regressions

--- Coefficient plots for VEC Regressions.R
+++ Coefficient plots for VEC Regressions.R
@@ -2,216 +2,6 @@
 # FIRM-LEVEL #
 ##############
 
-ggplot(dn_ols_coef_prov, aes(x = factor(year), y = estimate, ymin = estimate - std.error, ymax = estimate + std.error)) +
-  geom_pointrange() +
-  geom_hline(yintercept = 0, linetype = "dotted", color = "black") +
-  labs(x = "Year", y = "Estimate and 95% Interval") +
-  ggtitle("") +
-  theme_minimal() +
-  guides(fill = "none") +  
-  theme(axis.line = element_line(color='black'),
-        plot.background = element_blank(),
-        panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank(),
-        panel.border = element_blank(),
-        legend.title=element_blank(),
-        text = element_text(size=10)) + 
-  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
-ggsave("dn_ols_coef_prov.jpeg", width = 7, height = 7)
-
-ggplot(dn_ols_prov_cas_coef, aes(x = factor(year), y = estimate, ymin = estimate - std.error, ymax = estimate + std.error)) +
-  geom_pointrange() +
-  geom_hline(yintercept = 0, linetype = "dotted", color = "black") +
-  labs(x = "Year", y = "Estimate and 95% Interval") +
-  ggtitle("") +
-  theme_minimal() +
-  guides(fill = "none") +  
-  theme(axis.line = element_line(color='black'),
-        plot.background = element_blank(),
-        panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank(),
-        panel.border = element_blank(),
-        legend.title=element_blank(),
-        text = element_text(size=10)) + 
-  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
-ggsave("dn_ols_prov_cas_coef.jpeg", width = 7, height = 7)
-
-ggplot(dn_formal_ols_coef_prov, aes(x = factor(year), y = estimate, ymin = estimate - std.error, ymax = estimate + std.error)) +
-  geom_pointrange() +
-  geom_hline(yintercept = 0, linetype = "dotted", color = "black") +
-  labs(x = "Year", y = "Estimate and 95% Interval") +
-  ggtitle("") +
-  theme_minimal() +
-  guides(fill = "none") +  
-  theme(axis.line = element_line(color='black'),
-        plot.background = element_blank(),
-        panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank(),
-        panel.border = element_blank(),
-        legend.title=element_blank(),
-        text = element_text(size=10)) + 
-  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
-ggsave("dn_formal_ols_coef_prov.jpeg", width = 7, height = 7)
-
-ggplot(dn_formal_ols_prov_coef_cas, aes(x = factor(year), y = estimate, ymin = estimate - std.error, ymax = estimate + std.error)) +
-  geom_pointrange() +
-  geom_hline(yintercept = 0, linetype = "dotted", color = "black") +
-  labs(x = "Year", y = "Estimate and 95% Interval") +
-  ggtitle("") +
-  theme_minimal() +
-  guides(fill = "none") +  
-  theme(axis.line = element_line(color='black'),
-        plot.background = element_blank(),
-        panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank(),
-        panel.border = element_blank(),
-        legend.title=element_blank(),
-        text = element_text(size=10)) + 
-  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
-ggsave("dn_formal_ols_prov_coef_cas.jpeg", width = 7, height = 7)
-
-# By south 
-
-ggplot(dn_ols_prov_coef_ns, aes(x = factor(year), y = estimate, ymin = estimate - std.error, ymax = estimate + std.error, color = group)) +
-  geom_pointrange() +
-  geom_hline(yintercept = 0, linetype = "dotted", color = "black") +
-  labs(x = "Year", y = "Estimate and 95% Interval", color = "Region") +
-  ggtitle("") +
-  theme_minimal() +
-  guides(fill = "none") +  
-  theme(axis.line = element_line(color='black'),
-        plot.background = element_blank(),
-        panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank(),
-        panel.border = element_blank(),
-        legend.title=element_blank(),
-        text = element_text(size=10)) + 
-  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
-ggsave("dn_ols_prov_coef_ns.jpeg", width = 7, height = 7)
-
-ggplot(dn_formal_ols_prov_coef_ns, aes(x = factor(year), y = estimate, ymin = estimate - std.error, ymax = estimate + std.error, color = group)) +
-  geom_pointrange() +
-  geom_hline(yintercept = 0, linetype = "dotted", color = "black") +
-  labs(x = "Year", y = "Estimate and 95% Interval", color = "Region") +
-  ggtitle("") +
-  theme_minimal() +
-  guides(fill = "none") +  
-  theme(axis.line = element_line(color='black'),
-        plot.background = element_blank(),
-        panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank(),
-        panel.border = element_blank(),
-        legend.title=element_blank(),
-        text = element_text(size=10)) + 
-  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
-ggsave("dn_formal_ols_prov_coef_ns.jpeg", width = 7, height = 7)
-
-ggplot(dn_ols_manu_prov_coef_ns, aes(x = factor(year), y = estimate, ymin = estimate - std.error, ymax = estimate + std.error, color = group)) +
-  geom_pointrange() +
-  geom_hline(yintercept = 0, linetype = "dotted", color = "black") +
-  labs(x = "Year", y = "Estimate and 95% Interval", color = "Region") +
-  ggtitle("") +
-  theme_minimal() +
-  guides(fill = "none") +  
-  theme(axis.line = element_line(color='black'),
-        plot.background = element_blank(),
-        panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank(),
-        panel.border = element_blank(),
-        legend.title=element_blank(),
-        text = element_text(size=10)) + 
-  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
-ggsave("dn_ols_manu_prov_coef_ns.jpeg", width = 7, height = 7)
-
-ggplot(dn_manu_formal_prov_coef_ns, aes(x = factor(year), y = estimate, ymin = estimate - std.error, ymax = estimate + std.error, color = group)) +
-  geom_pointrange() +
-  geom_hline(yintercept = 0, linetype = "dotted", color = "black") +
-  labs(x = "Year", y = "Estimate and 95% Interval", color = "Region") +
-  ggtitle("") +
-  theme_minimal() +
-  guides(fill = "none") +  
-  theme(axis.line = element_line(color='black'),
-        plot.background = element_blank(),
-        panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank(),
-        panel.border = element_blank(),
-        legend.title=element_blank(),
-        text = element_text(size=10)) + 
-  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
-ggsave("dn_manu_formal_prov_coef_ns.jpeg", width = 7, height = 7)
-
-#################
-# FIXED EFFECTS #
-#################
-
-ggplot(dn_indfe_prov_coef, aes(x = factor(year), y = estimate, ymin = estimate - std.error, ymax = estimate + std.error)) +
-  geom_pointrange() +
-  geom_hline(yintercept = 0, linetype = "dotted", color = "black") +
-  labs(x = "Year", y = "Estimate and 95% Interval") +
-  ggtitle("") +
-  theme_minimal() +
-  guides(fill = "none") +  
-  theme(axis.line = element_line(color='black'),
-        plot.background = element_blank(),
-        panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank(),
-        panel.border = element_blank(),
-        legend.title=element_blank(),
-        text = element_text(size=10)) + 
-  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
-ggsave("dn_indfe_prov_coef.jpeg", width = 7, height = 7)
-
-ggplot(dn_indfe_prov_coef_cas, aes(x = factor(year), y = estimate, ymin = estimate - std.error, ymax = estimate + std.error)) +
-  geom_pointrange() +
-  geom_hline(yintercept = 0, linetype = "dotted", color = "black") +
-  labs(x = "Year", y = "Estimate and 95% Interval") +
-  ggtitle("") +
-  theme_minimal() +
-  guides(fill = "none") +  
-  theme(axis.line = element_line(color='black'),
-        plot.background = element_blank(),
-        panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank(),
-        panel.border = element_blank(),
-        legend.title=element_blank(),
-        text = element_text(size=10)) + 
-  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
-ggsave("dn_indfe_prov_coef_cas.jpeg", width = 7, height = 7)
-
-ggplot(dn_formal_fe_prov_coef, aes(x = factor(year), y = estimate, ymin = estimate - std.error, ymax = estimate + std.error)) +
-  geom_pointrange() +
-  geom_hline(yintercept = 0, linetype = "dotted", color = "black") +
-  labs(x = "Year", y = "Estimate and 95% Interval") +
-  ggtitle("") +
-  theme_minimal() +
-  guides(fill = "none") +  
-  theme(axis.line = element_line(color='black'),
-        plot.background = element_blank(),
-        panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank(),
-        panel.border = element_blank(),
-        legend.title=element_blank(),
-        text = element_text(size=10)) + 
-  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
-ggsave("dn_formal_fe_prov_coef.jpeg", width = 7, height = 7)
-
-ggplot(dn_workerratio_coef_prov_cas, aes(x = factor(year), y = estimate, ymin = estimate - std.error, ymax = estimate + std.error)) +
-  geom_pointrange() +
-  geom_hline(yintercept = 0, linetype = "dotted", color = "black") +
-  labs(x = "Year", y = "Estimate and 95% Interval") +
-  ggtitle("") +
-  theme_minimal() +
-  guides(fill = "none") +  
-  theme(axis.line = element_line(color='black'),
-        plot.background = element_blank(),
-        panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank(),
-        panel.border = element_blank(),
-        legend.title=element_blank(),
-        text = element_text(size=10)) + 
-  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
-ggsave("dn_workerratio_coef_prov_cas.jpeg", width = 7, height = 7)
-
 # By South 
 
 ggplot(dn_indfe_prov_coef_ns, aes(x = factor(year), y = estimate, ymin = estimate - std.error, ymax = estimate + std.error, color = group)) +
@@ -248,11 +38,9 @@ ggplot(dn_formal_fe_coef_prov_ns, aes(x = factor(year), y = estimate, ymin = est
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
 ggsave("dn_formal_fe_coef_prov_ns.jpeg", width = 7, height = 7)
 
-#################
-# Province-level#
-#################
+## With casualties 
 
-ggplot(dn_prov_ols_coef, aes(x = factor(year), y = estimate, ymin = estimate - std.error, ymax = estimate + std.error)) +
+ggplot(dn_indfe_prov_coef_cas, aes(x = factor(year), y = estimate, ymin = estimate - std.error, ymax = estimate + std.error)) +
   geom_pointrange() +
   geom_hline(yintercept = 0, linetype = "dotted", color = "black") +
   labs(x = "Year", y = "Estimate and 95% Interval") +
@@ -267,31 +55,12 @@ ggplot(dn_prov_ols_coef, aes(x = factor(year), y = estimate, ymin = estimate - s
         legend.title=element_blank(),
         text = element_text(size=10)) + 
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
-ggsave("dn_prov_ols_coef.jpeg", width = 7, height = 7)
+ggsave("dn_indfe_prov_coef_cas.jpeg", width = 7, height = 7)
 
-# By south 
-
-ggplot(dn_prov_ols_coef_ns, aes(x = factor(year), y = estimate, ymin = estimate - std.error, ymax = estimate + std.error, color = group)) +
+ggplot(dn_indfe_formal_prov_coef_cas, aes(x = factor(year), y = estimate, ymin = estimate - std.error, ymax = estimate + std.error)) +
   geom_pointrange() +
   geom_hline(yintercept = 0, linetype = "dotted", color = "black") +
-  labs(x = "Year", y = "Estimate and 95% Interval", color = "Region") +
-  ggtitle("") +
-  theme_minimal() +
-  guides(fill = "none") +  
-  theme(axis.line = element_line(color='black'),
-        plot.background = element_blank(),
-        panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank(),
-        panel.border = element_blank(),
-        legend.title=element_blank(),
-        text = element_text(size=10)) + 
-  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
-ggsave("dn_prov_ols_coef_ns.jpeg", width = 7, height = 7)
-
-ggplot(femaledir_yob_fe_prov_s_ns, aes(x = factor(year), y = estimate, ymin = estimate - std.error, ymax = estimate + std.error, color = group)) +
-  geom_pointrange() +
-  geom_hline(yintercept = 0, linetype = "dotted", color = "black") +
-  labs(x = "Year", y = "Estimate and 95% Interval", color = "Region") +
+  labs(x = "Year", y = "Estimate and 95% Interval") +
   ggtitle("") +
   theme_minimal() +
   guides(fill = "none") +  
