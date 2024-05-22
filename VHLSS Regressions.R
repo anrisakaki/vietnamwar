46730691214b7d46cb59a0e28e1938b28ef40b5f
Anri Sakakibara <anrisakaki@gmail.com>
2024-05-22 15:11:42 +0100

    VHLSS Regressions

--- VHLSS Regressions.R
+++ VHLSS Regressions.R
@@ -76,6 +76,134 @@ legend("topleft", col = 1:6, pch = 16, bty = "n", cex = 0.9,
        legend = c("2001", "2003", "2005", "2007", "2009", "2011"))
 dev.off()
 
+# Pre-War and Post-War
+
+png("work_prewar_vhlss_s.png")
+iplot(list(
+  feols(work ~ as.factor(female) + i(as.factor(female), log(tot_bmr_prov_ppn)) + age + age^2 + educ + dist_nearest_base_prov + dist_nearest_hochi_prov + log(popdensity_02) | minority + urban + marital,
+        subset(vhlss02, south == 1 & age > (2002-1950)),
+        weights = ~wt75,
+        vcov = ~tinh),
+  feols(work ~ as.factor(female) + i(as.factor(female), log(tot_bmr_prov_ppn)) + age + age^2 + educ + dist_nearest_base_prov + dist_nearest_hochi_prov + log(popdensity_04) | minority + urban + marital,
+        subset(vhlss04, south == 1 & age > (2004-1950)),
+        weights = ~wt45,
+        vcov = ~tinh),
+  feols(work ~ as.factor(female) + i(as.factor(female), log(tot_bmr_prov_ppn)) + age + age^2 + educ + dist_nearest_base_prov + dist_nearest_hochi_prov + log(popdensity_06) | minority + urban + marital,
+        subset(vhlss06, south == 1 & age > (2006-1950)),
+        weights = ~wt45,
+        vcov = ~tinh),
+  feols(work ~ as.factor(female) + i(as.factor(female), log(tot_bmr_prov_ppn)) + age + age^2 + educ + dist_nearest_base_prov + dist_nearest_hochi_prov + log(popdensity_08) | minority + urban + marital,
+        subset(vhlss08, south == 1 & age > (2008-1950)),
+        weights = ~wt9,
+        vcov = ~tinh),
+  feols(work ~ as.factor(female) + i(as.factor(female), log(tot_bmr_prov_ppn)) + age + age^2 + educ + dist_nearest_base_prov + dist_nearest_hochi_prov + log(popdensity_10) | minority + urban + marital,
+        subset(vhlss10, south == 1 & age > (2010-1950)),
+        weights = ~wt9,
+        vcov = ~tinh),
+  feols(work ~ as.factor(female) + i(as.factor(female), log(tot_bmr_prov_ppn)) + age + age^2 + educ + dist_nearest_base_prov + dist_nearest_hochi_prov + log(popdensity_12)| minority + urban + marital,
+        subset(vhlss12, south == 1 & age > (2012-1950)),
+        weights = ~wt9,
+        vcov = ~tinh)
+))
+legend("topleft", col = 1:6, pch = 16, bty = "n", cex = 0.9, 
+       legend = c("2001", "2003", "2005", "2007", "2009", "2011"))
+dev.off()
+
+png("work_postwar_vhlss_s.png")
+iplot(list(
+  feols(work ~ as.factor(female) + i(as.factor(female), log(tot_bmr_prov_ppn)) + age + age^2 + educ + dist_nearest_base_prov + dist_nearest_hochi_prov + log(popdensity_02) | minority + urban + marital,
+        subset(vhlss02, south == 1 & age <= (2002-1950)),
+        weights = ~wt75,
+        vcov = ~tinh),
+  feols(work ~ as.factor(female) + i(as.factor(female), log(tot_bmr_prov_ppn)) + age + age^2 + educ + dist_nearest_base_prov + dist_nearest_hochi_prov + log(popdensity_04) | minority + urban + marital,
+        subset(vhlss04, south == 1 & age <= (2004-1950)),
+        weights = ~wt45,
+        vcov = ~tinh),
+  feols(work ~ as.factor(female) + i(as.factor(female), log(tot_bmr_prov_ppn)) + age + age^2 + educ + dist_nearest_base_prov + dist_nearest_hochi_prov + log(popdensity_06) | minority + urban + marital,
+        subset(vhlss06, south == 1 & age <= (2006-1950)),
+        weights = ~wt45,
+        vcov = ~tinh),
+  feols(work ~ as.factor(female) + i(as.factor(female), log(tot_bmr_prov_ppn)) + age + age^2 + educ + dist_nearest_base_prov + dist_nearest_hochi_prov + log(popdensity_08) | minority + urban + marital,
+        subset(vhlss08, south == 1 & age <= (2008-1950)),
+        weights = ~wt9,
+        vcov = ~tinh),
+  feols(work ~ as.factor(female) + i(as.factor(female), log(tot_bmr_prov_ppn)) + age + age^2 + educ + dist_nearest_base_prov + dist_nearest_hochi_prov + log(popdensity_10) | minority + urban + marital,
+        subset(vhlss10, south == 1 & age <= (2010-1950)),
+        weights = ~wt9,
+        vcov = ~tinh),
+  feols(work ~ as.factor(female) + i(as.factor(female), log(tot_bmr_prov_ppn)) + age + age^2 + educ + dist_nearest_base_prov + dist_nearest_hochi_prov + log(popdensity_12)| minority + urban + marital,
+        subset(vhlss12, south == 1 & age <= (2012-1950)),
+        weights = ~wt9,
+        vcov = ~tinh)
+))
+legend("topleft", col = 1:6, pch = 16, bty = "n", cex = 0.9, 
+       legend = c("2001", "2003", "2005", "2007", "2009", "2011"))
+dev.off()
+
+png("work_prewar_vhlss_n.png")
+iplot(list(
+  feols(work ~ as.factor(female) + i(as.factor(female), log(tot_bmr_prov_ppn)) + age + age^2 + educ + dist_nearest_base_prov + dist_nearest_hochi_prov + log(popdensity_02) | minority + urban + marital,
+        subset(vhlss02, south == 0 & age > (2002-1975)),
+        weights = ~wt75,
+        vcov = ~tinh),
+  feols(work ~ as.factor(female) + i(as.factor(female), log(tot_bmr_prov_ppn)) + age + age^2 + educ + dist_nearest_base_prov + dist_nearest_hochi_prov + log(popdensity_04) | minority + urban + marital,
+        subset(vhlss04, south == 0 & age > (2004-1975)),
+        weights = ~wt45,
+        vcov = ~tinh),
+  feols(work ~ as.factor(female) + i(as.factor(female), log(tot_bmr_prov_ppn)) + age + age^2 + educ + dist_nearest_base_prov + dist_nearest_hochi_prov + log(popdensity_06) | minority + urban + marital,
+        subset(vhlss06, south == 0 & age > (2006-1975)),
+        weights = ~wt45,
+        vcov = ~tinh),
+  feols(work ~ as.factor(female) + i(as.factor(female), log(tot_bmr_prov_ppn)) + age + age^2 + educ + dist_nearest_base_prov + dist_nearest_hochi_prov + log(popdensity_08) | minority + urban + marital,
+        subset(vhlss08, south == 0 & age > (2008-1975)),
+        weights = ~wt9,
+        vcov = ~tinh),
+  feols(work ~ as.factor(female) + i(as.factor(female), log(tot_bmr_prov_ppn)) + age + age^2 + educ + dist_nearest_base_prov + dist_nearest_hochi_prov + log(popdensity_10) | minority + urban + marital,
+        subset(vhlss10, south == 0 & age > (2010-1975)),
+        weights = ~wt9,
+        vcov = ~tinh),
+  feols(work ~ as.factor(female) + i(as.factor(female), log(tot_bmr_prov_ppn)) + age + age^2 + educ + dist_nearest_base_prov + dist_nearest_hochi_prov + log(popdensity_12)| minority + urban + marital,
+        subset(vhlss12, south == 0 & age > (2012-1975)),
+        weights = ~wt9,
+        vcov = ~tinh)
+))
+legend("topleft", col = 1:6, pch = 16, bty = "n", cex = 0.9, 
+       legend = c("2001", "2003", "2005", "2007", "2009", "2011"))
+dev.off()
+
+png("work_postwar_vhlss_n.png")
+iplot(list(
+  feols(work ~ as.factor(female) + i(as.factor(female), log(tot_bmr_prov_ppn)) + age + age^2 + educ + dist_nearest_base_prov + dist_nearest_hochi_prov + log(popdensity_02) | minority + urban + marital,
+        subset(vhlss02, south == 0 & age <= (2002-1975)),
+        weights = ~wt75,
+        vcov = ~tinh),
+  feols(work ~ as.factor(female) + i(as.factor(female), log(tot_bmr_prov_ppn)) + age + age^2 + educ + dist_nearest_base_prov + dist_nearest_hochi_prov + log(popdensity_04) | minority + urban + marital,
+        subset(vhlss04, south == 0 & age <= (2004-1975)),
+        weights = ~wt45,
+        vcov = ~tinh),
+  feols(work ~ as.factor(female) + i(as.factor(female), log(tot_bmr_prov_ppn)) + age + age^2 + educ + dist_nearest_base_prov + dist_nearest_hochi_prov + log(popdensity_06) | minority + urban + marital,
+        subset(vhlss06, south == 0 & age <= (2006-1975)),
+        weights = ~wt45,
+        vcov = ~tinh),
+  feols(work ~ as.factor(female) + i(as.factor(female), log(tot_bmr_prov_ppn)) + age + age^2 + educ + dist_nearest_base_prov + dist_nearest_hochi_prov + log(popdensity_08) | minority + urban + marital,
+        subset(vhlss08, south == 0 & age <= (2008-1975)),
+        weights = ~wt9,
+        vcov = ~tinh),
+  feols(work ~ as.factor(female) + i(as.factor(female), log(tot_bmr_prov_ppn)) + age + age^2 + educ + dist_nearest_base_prov + dist_nearest_hochi_prov + log(popdensity_10) | minority + urban + marital,
+        subset(vhlss10, south == 0 & age <= (2010-1975)),
+        weights = ~wt9,
+        vcov = ~tinh),
+  feols(work ~ as.factor(female) + i(as.factor(female), log(tot_bmr_prov_ppn)) + age + age^2 + educ + dist_nearest_base_prov + dist_nearest_hochi_prov + log(popdensity_12)| minority + urban + marital,
+        subset(vhlss12, south == 0 & age <= (2012-1975)),
+        weights = ~wt9,
+        vcov = ~tinh)
+))
+legend("topleft", col = 1:6, pch = 16, bty = "n", cex = 0.9, 
+       legend = c("2001", "2003", "2005", "2007", "2009", "2011"))
+dev.off()
+
+# Casualties 
+
 png("work_vhlss_cas.png")
 iplot(list(
   feols(work ~ as.factor(female) + i(as.factor(female), log(killed_tot_prov_ppn)) + age + age^2 + educ + dist_nearest_base_prov + dist_nearest_hochi_prov + log(popdensity_02) | minority + urban + marital,
