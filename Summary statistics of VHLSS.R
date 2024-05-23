a039f341f8397f0f55540f652ee57834081291cb
Anri Sakakibara <anrisakaki@gmail.com>
2024-05-23 16:09:13 +0100

    Summary statistics of VHLSS

--- Summary statistics of VHLSS.R
+++ Summary statistics of VHLSS.R
@@ -126,56 +126,49 @@ prov12_vhlss <- vhlss12 %>%
   prov_sum_fn() %>% 
   mutate(south = ifelse(tinh > 44, 1, 0)) 
 
-# INDUSTRY ANALYSIS 
+#####################
+# INDUSTRY ANALYSIS #
+#####################
 
-industry_02_s <- vhlss02 %>% 
+# 2002 
+
+female02_s <- vhlss02 %>% 
   filter(!is.na(female) & !is.na(industry),
-         age > 15 & age < 65,
-         work == 1,
-         south == 1) %>% 
-  group_by(female, industry) %>% 
-  count(
-    industry, female, wt = wt75
+         age > 15 & age < 65 & work == 1) %>% 
+  group_by(south, female) %>% 
+  summarise(
+    N = sum(wt75)
   ) %>% 
-  pivot_wider(names_from = female, values_from = n) %>% 
-  rename(Male_s = 2,
-         Female_s = 3)
+  pivot_wider(names_from = female, values_from = N) %>% 
+  rename(total_m_workers = 2,
+         total_f_workers = 3)
 
-industry_02_n <- vhlss02 %>% 
+industry_f_02 <- vhlss02 %>% 
   filter(!is.na(female) & !is.na(industry),
-         age > 15 & age < 65,
-         work == 1,
-         south == 0) %>% 
-  group_by(female, industry) %>% 
-  count(
-    industry, female, wt = wt75
-  ) %>% 
-  pivot_wider(names_from = female, values_from = n) %>% 
-  rename(Male_n = 2,
-         Female_n = 3)
+         age > 15 & age < 65 & work == 1 & female == 1) %>% 
+  group_by(south, industry) %>% 
+  summarise(N_f = sum(wt75))
 
-industry_02 <- merge(industry_02_s, industry_02_n, by = "industry")
+industry_m_02 <- vhlss02 %>% 
+  filter(!is.na(female) & !is.na(industry),
+         age > 15 & age < 65 & work == 1 & female == 0) %>% 
+  group_by(south, industry) %>% 
+  summarise(N_m = sum(wt75))
 
-total_men_s <- sum(industry_02_s$Male_s, na.rm = TRUE)
-total_women_s <- sum(industry_02_s$Female_s, na.rm = TRUE)
-total_men_n <- sum(industry_02_n$Male_n, na.rm = TRUE)
-total_women_n <- sum(industry_02_n$Female_n, na.rm = TRUE)
-
-industry_02 <- industry_02 %>%
-  mutate(mshare_s = (Male_s/total_men_s)*100,
-         fshare_s = (Female_s/total_women_s)*100,
-         mshare_n = (Male_n/total_men_n)*100,
-         fshare_n = (Female_n/total_women_n)*100) %>% 
+industry_02 <- merge(industry_f_02, industry_m_02, by = c("south", "industry")) %>% 
+  left_join(female02_s, "south") %>% 
+  mutate(f_comp = round((N_f/total_f_workers)*100, 2),
+         m_comp = round((N_m/total_m_workers)*100, 2)) %>% 
   mutate(Industry = case_when(industry == 1 ~ 'Agriculture',
                               industry == 52 ~ 'Retail trade',
                               industry == 55 ~ 'Hotels and restaurants',
-                              industry == 18 ~ 'Manufacture of wearing apparel',
-                              industry == 15 ~ 'Manufacture of food products',
+                              industry == 18 ~ 'Wearing apparel',
+                              industry == 15 ~ 'Food and beverages',
                               industry == 45 ~ 'Construction',
                               industry == 5 ~ "Fishing",
                               industry == 60 ~ 'Transport',
                               industry == 80 ~ 'Education',
-                              industry == 75 ~ 'Public administration and defence',
+                              industry == 75 ~ 'Public administration',
                               TRUE ~ NA_character_))
 
 save(prov02_vhlss, file = "prov02_vhlss.Rda")
