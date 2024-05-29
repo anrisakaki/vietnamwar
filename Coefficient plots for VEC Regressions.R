d7732ff83c4fbc9137d3498bed8f2fd09b53d479
Anri Sakakibara <anrisakaki@gmail.com>
2024-05-29 15:15:35 +0100

    Coefficient plots for VEC Regressions

--- Coefficient plots for VEC Regressions.R
+++ Coefficient plots for VEC Regressions.R
@@ -21,23 +21,6 @@ ggplot(dn_indfe_prov_coef_ns, aes(x = factor(year), y = estimate, ymin = estimat
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
 ggsave("dn_indfe_prov_coef_ns.jpeg", width = 7, height = 7)
 
-ggplot(dn_formal_fe_coef_prov_ns, aes(x = factor(year), y = estimate, ymin = estimate - std.error, ymax = estimate + std.error, color = group)) +
-  geom_pointrange() +
-  geom_hline(yintercept = 0, linetype = "dotted", color = "black") +
-  labs(x = "Year", y = "Estimate and 95% Interval", color = "Region") +
-  ggtitle("") +
-  theme_minimal() +
-  guides(fill = "none") +  
-  theme(axis.line = element_line(color='black'),
-        plot.background = element_blank(),
-        panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank(),
-        panel.border = element_blank(),
-        legend.title=element_blank(),
-        text = element_text(size=10)) + 
-  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
-ggsave("dn_formal_fe_coef_prov_ns.jpeg", width = 7, height = 7)
-
 ## With casualties 
 
 ggplot(dn_indfe_prov_coef_cas, aes(x = factor(year), y = estimate, ymin = estimate - std.error, ymax = estimate + std.error)) +
@@ -57,22 +40,6 @@ ggplot(dn_indfe_prov_coef_cas, aes(x = factor(year), y = estimate, ymin = estima
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
 ggsave("dn_indfe_prov_coef_cas.jpeg", width = 7, height = 7)
 
-ggplot(dn_indfe_formal_prov_coef_cas, aes(x = factor(year), y = estimate, ymin = estimate - std.error, ymax = estimate + std.error)) +
-  geom_pointrange() +
-  geom_hline(yintercept = 0, linetype = "dotted", color = "black") +
-  labs(x = "Year", y = "Estimate and 95% Interval") +
-  ggtitle("") +
-  theme_minimal() +
-  guides(fill = "none") +  
-  theme(axis.line = element_line(color='black'),
-        plot.background = element_blank(),
-        panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank(),
-        panel.border = element_blank(),
-        legend.title=element_blank(),
-        text = element_text(size=10)) + 
-  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
-
 # By manufacturing 
 ggplot(dn_manu_prov_coef_ns, aes(x = factor(year), y = estimate, ymin = estimate - std.error, ymax = estimate + std.error, color = group)) +
   geom_pointrange() +
@@ -89,23 +56,6 @@ ggplot(dn_manu_prov_coef_ns, aes(x = factor(year), y = estimate, ymin = estimate
         legend.title=element_blank(),
         text = element_text(size=10)) + 
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
-
-ggplot(dn_manu_formal_prov_coef_ns, aes(x = factor(year), y = estimate, ymin = estimate - std.error, ymax = estimate + std.error, color = group)) +
-  geom_pointrange() +
-  geom_hline(yintercept = 0, linetype = "dotted", color = "black") +
-  labs(x = "Year", y = "Estimate and 95% Interval", color = "Region") +
-  ggtitle("") +
-  theme_minimal() +
-  guides(fill = "none") +  
-  theme(axis.line = element_line(color='black'),
-        plot.background = element_blank(),
-        panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank(),
-        panel.border = element_blank(),
-        legend.title=element_blank(),
-        text = element_text(size=10)) + 
-  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
-ggsave("dn_manu_formal_prov_coef_ns.jpeg", width = 7, height = 7)
 
 #################
 # BY FIRRM TYPE #
