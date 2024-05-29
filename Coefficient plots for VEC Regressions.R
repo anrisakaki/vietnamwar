239694dc0aceeda84830307a458949a105480a88
Anri Sakakibara <anrisakaki@gmail.com>
2024-05-29 09:17:39 +0100

    Coefficient plots for VEC Regressions

--- Coefficient plots for VEC Regressions.R
+++ Coefficient plots for VEC Regressions.R
@@ -174,3 +174,19 @@ ggplot(dn_priv_w_state_ns, aes(x = factor(year), y = estimate, ymin = estimate -
         legend.title=element_blank(),
         text = element_text(size=10)) + 
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+
+ggplot(dn_collective_ns, aes(x = factor(year), y = estimate, ymin = estimate - std.error, ymax = estimate + std.error, color = group)) +
+  geom_pointrange() +
+  geom_hline(yintercept = 0, linetype = "dotted", color = "black") +
+  labs(x = "Year", y = "Estimate and 95% Interval", color = "Region") +
+  ggtitle("") +
+  theme_minimal() +
+  guides(fill = "none") +  
+  theme(axis.line = element_line(color='black'),
+        plot.background = element_blank(),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank(),
+        legend.title=element_blank(),
+        text = element_text(size=10)) + 
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
