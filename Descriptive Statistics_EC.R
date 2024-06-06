6d347a6dc97285a349489b91a061d64ed6d88d50
Anri Sakakibara <anrisakaki@gmail.com>
2024-06-06 13:56:37 +0100

    VEC Descriptive statistics

--- Descriptive Statistics_EC.R
+++ Descriptive Statistics_EC.R
@@ -1,45 +1,8 @@
-# Types of firms 
+########################################
+# BMR VS SHARE OF FEMALE FOUNDED FIRMS #
+########################################
 
-ggplot(firmtype_long, aes(x = year, y = Share, color = FirmType)) +
-  geom_line(linewidth = 1) +
-  labs(
-    title = "",
-    x = "Year",
-    y = "Share",
-    color = "Firm Type"
-  ) +
-  theme_minimal() +
-  guides(fill = "none") + 
-  scale_x_continuous(breaks=seq(2002,2018,1)) +
-  theme(axis.line = element_line(color='black'),
-        plot.background = element_blank(),
-        panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank(),
-        panel.border = element_blank(),
-        legend.title=element_blank(),
-        text = element_text(size=10)) + 
-  scale_y_continuous(labels = scales::percent) 
-
-# Map of number of workers across districts 
-
-dn_dist_sf <- left_join(dn_dist, vnmap2, by = c("provname2018" = "NAME_1", "distname2018")) %>% st_as_sf()
-
-ggplot(dn_dist_sf) + 
-  geom_sf(aes(fill = nworkers)) +
-  scale_fill_gradient(name = "Total Formal Workers", low = "green", high = "red", na.value = "white") + 
-  theme(axis.text.x = element_blank(),
-        axis.text.y = element_blank(),
-        axis.ticks = element_blank(),
-        axis.title.y=element_blank(),
-        axis.title.x=element_blank(),
-        panel.background = element_blank()) +
-  ggtitle("")
-
-# Sex ratio of workers
-
-## All country 
-
-ggplot(dplyr::filter(dn_prov, year == 2018), aes(x = log(tot_bmr), y = log(workerratio*100))) +
+ggplot(dplyr::filter(bmr_fdir_prov, south == 0), aes(x = log(tot_bmr), y = share_fdir)) +
   geom_point() +
   geom_smooth(method = "lm",
               se = F) +
@@ -52,13 +15,10 @@ ggplot(dplyr::filter(dn_prov, year == 2018), aes(x = log(tot_bmr), y = log(worke
         panel.border = element_blank(),
         legend.title=element_blank(),
         text = element_text(size=10)) + 
-  labs(x = "log(Bombs, Missiles and Rockets)",
-       y = "log(Ratio of Male to Female Workers) in 2018")
-ggsave("dn_prov_sexratio_18.jpeg", width = 7, height = 7)
+  labs(x = "log(BMR)",
+       y = "Sex Ratio")
 
-# District 
-
-ggplot(dplyr::filter(dn_dist, year == 2018 & south == 1), aes(x = log(tot_bmr), y = log(workerratio*100))) +
+ggplot(dplyr::filter(bmr_fdir_prov, south == 1), aes(x = log(tot_bmr), y = share_fdir)) +
   geom_point() +
   geom_smooth(method = "lm",
               se = F) +
@@ -71,43 +31,6 @@ ggplot(dplyr::filter(dn_dist, year == 2018 & south == 1), aes(x = log(tot_bmr), 
         panel.border = element_blank(),
         legend.title=element_blank(),
         text = element_text(size=10)) + 
-  labs(x = "log(Bombs, Missiles and Rockets)",
-       y = "Ratio of Male to Female Workers in 2018")
-ggsave("dn_dist_sexratio_09.jpeg", width = 7, height = 7)
-
-## South 
-
-ggplot(dplyr::filter(dn_dist, year == 2009 & south == 1), aes(x = log(tot_bmr), y = log(workerratio*100))) +
-  geom_point() +
-  geom_smooth(method = "lm",
-              se = F) +
-  theme_minimal() +
-  guides(fill = "none") +  
-  theme(axis.line = element_line(color='black'),
-        plot.background = element_blank(),
-        panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank(),
-        panel.border = element_blank(),
-        legend.title=element_blank(),
-        text = element_text(size=10)) + 
-  labs(x = "log(Bombs, Missiles and Rockets)",
-       y = "log(Ratio of Male to Female Workers) in 2009 \n(South)")
-ggsave("dn_dist_sexratio_09_s.jpeg", width = 7, height = 7)
-
-ggplot(dplyr::filter(dn_prov, year == 2009 & south == 0), aes(x = log(tot_bmr), y = log(workerratio*100))) +
-  geom_point() +
-  geom_smooth(method = "lm",
-              se = F) +
-  theme_minimal() +
-  guides(fill = "none") +  
-  theme(axis.line = element_line(color='black'),
-        plot.background = element_blank(),
-        panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank(),
-        panel.border = element_blank(),
-        legend.title=element_blank(),
-        text = element_text(size=10)) + 
-  labs(x = "log(Bombs, Missiles and Rockets)",
-       y = "log(Ratio of Male to Female Workers) in 2000 \n(North)")
-ggsave("dn_prov_sexratio_09_n.jpeg", width = 7, height = 7)
+  labs(x = "log(BMR)",
+       y = "Sex Ratio")
 
