e94ddc0b9be816c30bfe750424318e72ac69a3fc
Anri Sakakibara <anrisakaki@gmail.com>
2024-06-13 11:28:55 +0100

    Coefficient plots for VEC Regressions

--- Coefficient plots for VEC Regressions.R
+++ Coefficient plots for VEC Regressions.R
@@ -78,13 +78,32 @@ ggsave("dn_indfe_prov_coef_cas.jpeg", width = 7, height = 7)
 # BY SECTOR #
 #############
 ind_workerratio_01_ns$sector <- factor(ind_workerratio_01_ns$sector, levels = rev(sort(unique(ind_workerratio_01_ns$sector))))
+ind_workerratio_prov_01_ns$sector <- factor(ind_workerratio_prov_01_ns$sector, levels = rev(sort(unique(ind_workerratio_prov_01_ns$sector))))
 
 ggplot(ind_workerratio_01_ns, aes(y = sector, x = estimate, color = group)) +  
   geom_point(position = position_dodge(width = 0.9), size = 3) +
   geom_errorbar(aes(xmin = estimate - std.error, xmax = estimate + std.error),  
                 position = position_dodge(width = 0.9), width = 0.25) +
   geom_vline(xintercept = 0, linetype = "dotted", color = "black") +  
-  labs(y = "", x = "Estimate and 95% Interval") +  s
+  labs(y = "", x = "Estimate and 95% Interval") +  
+  ggtitle("") +
+  theme_minimal() +
+  guides(fill = "none") +  
+  theme(axis.line = element_line(color = 'black'),
+        plot.background = element_blank(),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank(),
+        legend.title = element_blank(),
+        text = element_text(size = 10)) + 
+  theme(axis.text.y = element_text(angle = 0, hjust = 1))  
+
+ggplot(ind_workerratio_prov_01_ns, aes(y = sector, x = estimate, color = group)) +  
+  geom_point(position = position_dodge(width = 0.9), size = 3) +
+  geom_errorbar(aes(xmin = estimate - std.error, xmax = estimate + std.error),  
+                position = position_dodge(width = 0.9), width = 0.25) +
+  geom_vline(xintercept = 0, linetype = "dotted", color = "black") +  
+  labs(y = "", x = "Estimate and 95% Interval") +  
   ggtitle("") +
   theme_minimal() +
   guides(fill = "none") +  
