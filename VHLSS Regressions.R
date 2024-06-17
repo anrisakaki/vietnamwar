ce3596dcf6e96e56a628a82f1ca024fb1ad92be0
Anri Sakakibara <anrisakaki@gmail.com>
2024-06-17 14:20:27 +0100

    VHLSS Regressions

--- VHLSS Regressions.R
+++ VHLSS Regressions.R
@@ -101,6 +101,52 @@ legend("topleft", col = 1:6, pch = 16, bty = "n", cex = 0.9,
        legend = c("2001", "2003", "2005", "2007", "2009", "2011"))
 dev.off()
 
+###############################################
+# PROBABILITY OF BEING MANAGER OF HH BUSINESS #
+###############################################
+
+etable(list(
+  feols(female  ~ log(tot_bmr) | tinh,
+        subset(hhbus02, south == 1),
+        weights = ~wt75,
+        vcov = ~tinh+huyen),
+  feols(female  ~ log(tot_bmr) | tinh,
+        subset(hhbus04, south == 1),
+        weights = ~wt45,
+        vcov = ~tinh+huyen),
+  feols(female  ~ log(tot_bmr) | tinh,
+        subset(hhbus06, south == 1),
+        weights = ~wt45,
+        vcov = ~tinh+huyen),
+  feols(female  ~ log(tot_bmr) | tinh,
+        subset(hhbus02, south == 0),
+        weights = ~wt75,
+        vcov = ~tinh+huyen),
+  feols(female  ~ log(tot_bmr) | tinh,
+        subset(hhbus04, south == 0),
+        weights = ~wt45,
+        vcov = ~tinh+huyen),
+  feols(female  ~ log(tot_bmr) | tinh,
+        subset(hhbus06, south == 0),
+        weights = ~wt45,
+        vcov = ~tinh+huyen)
+))
+
+etable(list(
+  feols(female  ~ log(tot_bmr_prov),
+        subset(hhbus02, south == 1),
+        vcov = ~tinh+huyen),
+  feols(female  ~ log(tot_bmr_prov),
+        subset(hhbus06, south == 1),
+        vcov = ~tinh+huyen),
+  feols(female  ~ log(tot_bmr_prov),
+        subset(hhbus02, south == 0),
+        vcov = ~tinh+huyen),
+  feols(female  ~ log(tot_bmr_prov),
+        subset(hhbus06, south == 0),
+        vcov = ~tinh+huyen)
+))
+
 ######################################
 # PROBABILITY OF WORKING - BY SECTOR #
 ######################################
@@ -288,6 +334,68 @@ iplot(list(
         vcov = ~tinh+huyen)
 ))
 legend("bottomleft", col = 1:6, pch = 16, bty = "n", cex = 0.9, 
+       legend = c("2001", "2003", "2005", "2007", "2009", "2011"))
+dev.off()
+
+png("selfagri_vhlss_dist_s.png")
+iplot(list(
+  feols(selfagri ~ as.factor(female) + i(as.factor(female), log(tot_bmr)) + age + age^2 + educ | minority + urban + marital + tinh,
+        subset(vhlss02, south == 1 & work == 1),
+        weights = ~wt75,
+        vcov = ~tinh+huyen),
+  feols(selfagri ~ as.factor(female) + i(as.factor(female), log(tot_bmr)) + age + age^2 + educ | minority + urban + marital + tinh,
+        subset(vhlss04, south == 1 & work == 1),
+        weights = ~wt45,
+        vcov = ~tinh+huyen),
+  feols(selfagri ~ as.factor(female) + i(as.factor(female), log(tot_bmr)) + age + age^2 + educ | minority + urban + marital + tinh,
+        subset(vhlss06, south == 1 & work == 1),
+        weights = ~wt45,
+        vcov = ~tinh+huyen),
+  feols(selfagri ~ as.factor(female) + i(as.factor(female), log(tot_bmr)) + age + age^2 + educ | minority + urban + marital + tinh,
+        subset(vhlss08, south == 1 & work == 1),
+        weights = ~wt9,
+        vcov = ~tinh+huyen),
+  feols(selfagri ~ as.factor(female) + i(as.factor(female), log(tot_bmr)) + age + age^2 + educ | minority + urban + marital + tinh,
+        subset(vhlss10, south == 1 & work == 1),
+        weights = ~wt9,
+        vcov = ~tinh+huyen),
+  feols(selfagri ~ as.factor(female) + i(as.factor(female), log(tot_bmr)) + age + age^2 + educ | minority + urban + marital + tinh,
+        subset(vhlss12, south == 1 & work == 1),
+        weights = ~wt9,
+        vcov = ~tinh+huyen)
+))
+legend("topleft", col = 1:6, pch = 16, bty = "n", cex = 0.9, 
+       legend = c("2001", "2003", "2005", "2007", "2009", "2011"))
+dev.off()
+
+png("selfagri_vhlss_dist_n.png")
+iplot(list(
+  feols(selfagri ~ as.factor(female) + i(as.factor(female), log(tot_bmr)) + age + age^2 + educ | minority + urban + marital + tinh,
+        subset(vhlss02, south == 0 & work == 1),
+        weights = ~wt75,
+        vcov = ~tinh+huyen),
+  feols(selfagri ~ as.factor(female) + i(as.factor(female), log(tot_bmr)) + age + age^2 + educ | minority + urban + marital + tinh,
+        subset(vhlss04, south == 0 & work == 1),
+        weights = ~wt45,
+        vcov = ~tinh+huyen),
+  feols(selfagri ~ as.factor(female) + i(as.factor(female), log(tot_bmr)) + age + age^2 + educ | minority + urban + marital + tinh,
+        subset(vhlss06, south == 0 & work == 1),
+        weights = ~wt45,
+        vcov = ~tinh+huyen),
+  feols(selfagri ~ as.factor(female) + i(as.factor(female), log(tot_bmr)) + age + age^2 + educ | minority + urban + marital + tinh,
+        subset(vhlss08, south == 0 & work == 1),
+        weights = ~wt9,
+        vcov = ~tinh+huyen),
+  feols(selfagri ~ as.factor(female) + i(as.factor(female), log(tot_bmr)) + age + age^2 + educ | minority + urban + marital + tinh,
+        subset(vhlss10, south == 0 & work == 1),
+        weights = ~wt9,
+        vcov = ~tinh+huyen),
+  feols(selfagri ~ as.factor(female) + i(as.factor(female), log(tot_bmr)) + age + age^2 + educ | minority + urban + marital + tinh,
+        subset(vhlss12, south == 0 & work == 1),
+        weights = ~wt9,
+        vcov = ~tinh+huyen)
+))
+legend("topleft", col = 1:6, pch = 16, bty = "n", cex = 0.9, 
        legend = c("2001", "2003", "2005", "2007", "2009", "2011"))
 dev.off()
 
