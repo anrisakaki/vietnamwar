6cd1b45ee112db02d6d768687443c412711b612d
Anri Sakakibara <anrisakaki@gmail.com>
2024-06-17 14:20:35 +0100

    VHLSS Robustness Checks

--- VHLSS Robustness Checks.R
+++ VHLSS Robustness Checks.R
@@ -198,7 +198,7 @@ dev.off()
 
 # Self employment 
 
-png("self_vhlss_s.png")
+png("selfemp_vhlss_s.png")
 iplot(list(
   feols(selfemp ~ as.factor(female) + i(as.factor(female), log(tot_bmr_prov)) + age + age^2 + educ + log(popdensity_02) | minority + urban + marital,
         subset(vhlss02, work == 1 & south == 1),
@@ -229,7 +229,7 @@ legend("bottomleft", col = 1:6, pch = 16, bty = "n", cex = 0.9,
        legend = c("2001", "2003", "2005", "2007", "2009", "2011"))
 dev.off()
 
-png("self_vhlss_n.png")
+png("selfemp_vhlss_n.png")
 iplot(list(
   feols(selfemp ~ as.factor(female) + i(as.factor(female), log(tot_bmr_prov)) + age + age^2 + educ + log(popdensity_02) | minority + urban + marital,
         subset(vhlss02, work == 1 & south == 0),
@@ -259,28 +259,3 @@ iplot(list(
 legend("bottomleft", col = 1:6, pch = 16, bty = "n", cex = 0.9, 
        legend = c("2001", "2003", "2005", "2007", "2009", "2011"))
 dev.off()
-
-
-##############################
-# PROBABILITY OF WORK BY AGE #
-##############################
-
-# 2002 
-
-## Female 
-
-work_agexbmr_02_s <- tidy(feols(work ~ i(as.factor(age), log(tot_bmr_prov_ppn)) + educ + dist_nearest_base_prov + dist_nearest_hochi_prov | urban + age + minority,
-                           subset(vhlss02, south == 1 & female == 1 & age < 65 & age > 15),
-                           weights = ~wt75,
-                           vcov = ~tinh + age)) %>% filter(grepl("log\\(tot_bmr_prov_ppn\\)", term))
-work_agexbmr_02_n <- tidy(feols(work ~ i(as.factor(age), log(tot_bmr_prov_ppn)) + educ + dist_nearest_base_prov + dist_nearest_hochi_prov | urban + age + minority,
-                           subset(vhlss02, south == 0 & female == 1 & age < 65 & age > 15),
-                           weights = ~wt75,
-                           vcov = ~tinh + age)) %>% filter(grepl("log\\(tot_bmr_prov_ppn\\)", term))
-
-selfagri_agexbmr_coef_02_s$age <- rep(seq(16, 64), each = nrow(selfagri_agexbmr_coef_02_s) / length(seq(16, 64)))
-selfagri_agexbmr_coef_02_n$age <- rep(seq(16, 64), each = nrow(selfagri_agexbmr_coef_02_n) / length(seq(16, 64)))
-
-selfagri_agexbmr_coef_02_n$group <- "North"
-selfagri_agexbmr_coef_02_s$group <- "South"
-selfagri_agexbmr_coef_02_ns <- bind_rows(selfagri_agexbmr_coef_02_n, selfagri_agexbmr_coef_02_s)
