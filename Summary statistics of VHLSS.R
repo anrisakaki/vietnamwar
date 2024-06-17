5f6910a597b107806b15b4b177abddcd9f47367b
Anri Sakakibara <anrisakaki@gmail.com>
2024-06-17 14:20:21 +0100

    Summary statistics of VHLSS

--- Summary statistics of VHLSS.R
+++ Summary statistics of VHLSS.R
@@ -14,7 +14,8 @@ prov_sum_fn <- function (i) {
       fshare_wagework = fwagework / f_total,
       fshare_selfemp = fself_emp / f_total,
       fshare_agri = fagri_work / f_total,
-      wagework_workerratio = mwagework/fwagework
+      wagework_workerratio = mwagework/fwagework,
+      fshare_hhbus = fhhbus / hhbus,
     )
 }
 
@@ -30,7 +31,9 @@ prov02_vhlss <- vhlss02 %>%
     mself_emp = sum(wt75 * (female == 0 & selfemp == 1), na.rm = T),
     mwagework = sum(wt75 * (female == 0 & wagework == 1), na.rm = T),
     fwagework = sum(wt75 * (female == 1 & wagework == 1), na.rm = T),
-    fagri_work = sum(wt75 * (female == 1 & selfagri == 1), na.rm = T)
+    fagri_work = sum(wt75 * (female == 1 & selfagri == 1), na.rm = T),
+    fhhbus = sum(wt75 * (female == 1 & hhbus == 1), na.rm = T),
+    hhbus = sum(wt75 * hhbus, na.rm = T)
   ) %>% 
   left_join(province_bmr_sum02, by = "tinh") %>% 
   prov_sum_fn () %>% 
@@ -49,6 +52,8 @@ dist02_vhlss <- vhlss02 %>%
     mwagework = sum(wt75 * (female == 0 & wagework == 1), na.rm = T),
     fwagework = sum(wt75 * (female == 1 & wagework == 1), na.rm = T),
     fagri_work = sum(wt75 * (female == 1 & selfagri == 1), na.rm = T),
+    fhhbus = sum(wt75 * (female == 1 & hhbus == 1), na.rm = T),
+    hhbus = sum(wt75 * hhbus, na.rm = T),
     tot_bmr = mean(tot_bmr)
   ) %>% 
   prov_sum_fn () %>% 
@@ -84,9 +89,31 @@ prov06_vhlss <- vhlss06 %>%
     mself_emp = sum(wt45 * (female == 0 & selfemp == 1), na.rm = T),
     mwagework = sum(wt45 * (female == 0 & wagework == 1), na.rm = T),
     fwagework = sum(wt45 * (female == 1 & wagework == 1), na.rm = T),
-    fagri_work = sum(wt45 * (female == 1 & selfagri == 1), na.rm = T)
+    fagri_work = sum(wt45 * (female == 1 & selfagri == 1), na.rm = T),
+    fhhbus = sum(wt45 * (female == 1 & hhbus == 1), na.rm = T),
+    hhbus = sum(wt45 * hhbus, na.rm = T)
   ) %>% 
   left_join(province_bmr_sum, by = "tinh") %>% 
+  prov_sum_fn () %>% 
+  mutate(south = ifelse(tinh > 407, 1, 0))
+
+dist06_vhlss <- vhlss06 %>% 
+  filter(!is.na(female), age > 15 & age < 65) %>% 
+  group_by(tinh, huyen) %>% 
+  summarise(
+    m_total = sum(wt45 * (female == 0), na.rm = T),
+    f_total = sum(wt45 * (female == 1), na.rm = T),
+    m_workers = sum(wt45 * (female == 0 & work == 1), na.rm = T),
+    f_workers = sum(wt45 * (female == 1 & work == 1), na.rm = T),
+    fself_emp = sum(wt45 * (female == 1 & selfemp == 1), na.rm = T),
+    mself_emp = sum(wt45 * (female == 0 & selfemp == 1), na.rm = T),
+    mwagework = sum(wt45 * (female == 0 & wagework == 1), na.rm = T),
+    fwagework = sum(wt45 * (female == 1 & wagework == 1), na.rm = T),
+    fagri_work = sum(wt45 * (female == 1 & selfagri == 1), na.rm = T),
+    fhhbus = sum(wt45 * (female == 1 & hhbus == 1), na.rm = T),
+    hhbus = sum(wt45 * hhbus, na.rm = T),
+    tot_bmr = mean(tot_bmr)
+  ) %>% 
   prov_sum_fn () %>% 
   mutate(south = ifelse(tinh > 407, 1, 0))
 
@@ -102,11 +129,34 @@ prov08_vhlss <- vhlss08 %>%
     mself_emp = sum(wt9 * (female == 0 & selfemp == 1), na.rm = T),
     mwagework = sum(wt9 * (female == 0 & wagework == 1), na.rm = T),
     fwagework = sum(wt9 * (female == 1 & wagework == 1), na.rm = T),
-    fagri_work = sum(wt9 * (female == 1 & selfagri == 1), na.rm = T)
+    fagri_work = sum(wt9 * (female == 1 & selfagri == 1), na.rm = T),
+    fhhbus = sum(wt9 * (female == 1 & hhbus == 1), na.rm = T),
+    hhbus = sum(wt9 * hhbus, na.rm = T)
   ) %>% 
   left_join(province_bmr_sum, by = "tinh") %>% 
   prov_sum_fn() %>% 
   mutate(south = ifelse(tinh > 407, 1, 0))
+
+dist08_vhlss <- vhlss08 %>% 
+  filter(!is.na(female), age > 15 & age < 65) %>% 
+  group_by(tinh, huyen) %>% 
+  summarise(
+    m_total = sum(wt9 * (female == 0), na.rm = T),
+    f_total = sum(wt9 * (female == 1), na.rm = T),
+    m_workers = sum(wt9 * (female == 0 & work == 1), na.rm = T),
+    f_workers = sum(wt9 * (female == 1 & work == 1), na.rm = T),
+    fself_emp = sum(wt9 * (female == 1 & selfemp == 1), na.rm = T),
+    mself_emp = sum(wt9 * (female == 0 & selfemp == 1), na.rm = T),
+    mwagework = sum(wt9 * (female == 0 & wagework == 1), na.rm = T),
+    fwagework = sum(wt9 * (female == 1 & wagework == 1), na.rm = T),
+    fagri_work = sum(wt9 * (female == 1 & selfagri == 1), na.rm = T),
+    fhhbus = sum(wt9 * (female == 1 & hhbus == 1), na.rm = T),
+    hhbus = sum(wt9 * hhbus, na.rm = T),
+    tot_bmr = mean(tot_bmr)
+  ) %>% 
+  prov_sum_fn() %>% 
+  mutate(south = ifelse(tinh > 407, 1, 0))
+
 
 prov10_vhlss <- vhlss10 %>% 
   filter(!is.na(female), age > 15 & age < 65) %>% 
