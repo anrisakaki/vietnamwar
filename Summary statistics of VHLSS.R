298c444e7e687e6c9f2d8fb8d680db20c1f26045
Anri Sakakibara <anrisakaki@gmail.com>
2024-12-04 17:03:48 +0000

    Summary statistics of VHLSS

--- Summary statistics of VHLSS.R
+++ Summary statistics of VHLSS.R
@@ -19,26 +19,6 @@ prov_sum_fn <- function (i) {
     )
 }
 
-prov02_vhlss <- vhlss02 %>% 
-  filter(!is.na(female), age > 15 & age < 65) %>% 
-  group_by(tinh) %>% 
-  summarise(
-    m_total = sum(wt75 * (female == 0), na.rm = T),
-    f_total = sum(wt75 * (female == 1), na.rm = T),
-    m_workers = sum(wt75 * (female == 0 & work == 1), na.rm = T),
-    f_workers = sum(wt75 * (female == 1 & work == 1), na.rm = T),
-    fself_emp = sum(wt75 * (female == 1 & selfemp == 1), na.rm = T),
-    mself_emp = sum(wt75 * (female == 0 & selfemp == 1), na.rm = T),
-    mwagework = sum(wt75 * (female == 0 & wagework == 1), na.rm = T),
-    fwagework = sum(wt75 * (female == 1 & wagework == 1), na.rm = T),
-    fagri_work = sum(wt75 * (female == 1 & selfagri == 1), na.rm = T),
-    fhhbus = sum(wt75 * (female == 1 & hhbus == 1), na.rm = T),
-    hhbus = sum(wt75 * hhbus, na.rm = T)
-  ) %>% 
-  left_join(province_bmr_sum02, by = "tinh") %>% 
-  prov_sum_fn () %>% 
-  mutate(south = ifelse(tinh > 407, 1, 0))
-
 dist02_vhlss <- vhlss02 %>% 
   filter(!is.na(female), age > 15 & age < 65) %>% 
   group_by(tinh, huyen) %>% 
@@ -56,44 +36,6 @@ dist02_vhlss <- vhlss02 %>%
     hhbus = sum(wt75 * hhbus, na.rm = T),
     tot_bmr = mean(tot_bmr)
   ) %>% 
-  prov_sum_fn () %>% 
-  mutate(south = ifelse(tinh > 407, 1, 0))
-
-prov04_vhlss <- vhlss04 %>% 
-  filter(!is.na(female), age > 15 & age < 65) %>% 
-  group_by(tinh) %>% 
-  summarise(
-    m_total = sum(wt45 * (female == 0), na.rm = T),
-    f_total = sum(wt45 * (female == 1), na.rm = T),
-    m_workers = sum(wt45 * (female == 0 & work == 1), na.rm = T),
-    f_workers = sum(wt45 * (female == 1 & work == 1), na.rm = T),
-    fself_emp = sum(wt45 * (female == 1 & selfemp == 1), na.rm = T),
-    mself_emp = sum(wt45 * (female == 0 & selfemp == 1), na.rm = T),
-    mwagework = sum(wt45 * (female == 0 & wagework == 1), na.rm = T),
-    fwagework = sum(wt45 * (female == 1 & wagework == 1), na.rm = T),
-    fagri_work = sum(wt45 * (female == 1 & selfagri == 1), na.rm = T)
-  ) %>% 
-  prov_sum_fn () %>% 
-  left_join(province_bmr_sum, by = "tinh") %>% 
-  mutate(south = ifelse(tinh > 407, 1, 0))
-
-prov06_vhlss <- vhlss06 %>% 
-  filter(!is.na(female), age > 15 & age < 65) %>% 
-  group_by(tinh) %>% 
-  summarise(
-    m_total = sum(wt45 * (female == 0), na.rm = T),
-    f_total = sum(wt45 * (female == 1), na.rm = T),
-    m_workers = sum(wt45 * (female == 0 & work == 1), na.rm = T),
-    f_workers = sum(wt45 * (female == 1 & work == 1), na.rm = T),
-    fself_emp = sum(wt45 * (female == 1 & selfemp == 1), na.rm = T),
-    mself_emp = sum(wt45 * (female == 0 & selfemp == 1), na.rm = T),
-    mwagework = sum(wt45 * (female == 0 & wagework == 1), na.rm = T),
-    fwagework = sum(wt45 * (female == 1 & wagework == 1), na.rm = T),
-    fagri_work = sum(wt45 * (female == 1 & selfagri == 1), na.rm = T),
-    fhhbus = sum(wt45 * (female == 1 & hhbus == 1), na.rm = T),
-    hhbus = sum(wt45 * hhbus, na.rm = T)
-  ) %>% 
-  left_join(province_bmr_sum, by = "tinh") %>% 
   prov_sum_fn () %>% 
   mutate(south = ifelse(tinh > 407, 1, 0))
 
@@ -117,26 +59,6 @@ dist06_vhlss <- vhlss06 %>%
   prov_sum_fn () %>% 
   mutate(south = ifelse(tinh > 407, 1, 0))
 
-prov08_vhlss <- vhlss08 %>% 
-  filter(!is.na(female), age > 15 & age < 65) %>% 
-  group_by(tinh) %>% 
-  summarise(
-    m_total = sum(wt9 * (female == 0), na.rm = T),
-    f_total = sum(wt9 * (female == 1), na.rm = T),
-    m_workers = sum(wt9 * (female == 0 & work == 1), na.rm = T),
-    f_workers = sum(wt9 * (female == 1 & work == 1), na.rm = T),
-    fself_emp = sum(wt9 * (female == 1 & selfemp == 1), na.rm = T),
-    mself_emp = sum(wt9 * (female == 0 & selfemp == 1), na.rm = T),
-    mwagework = sum(wt9 * (female == 0 & wagework == 1), na.rm = T),
-    fwagework = sum(wt9 * (female == 1 & wagework == 1), na.rm = T),
-    fagri_work = sum(wt9 * (female == 1 & selfagri == 1), na.rm = T),
-    fhhbus = sum(wt9 * (female == 1 & hhbus == 1), na.rm = T),
-    hhbus = sum(wt9 * hhbus, na.rm = T)
-  ) %>% 
-  left_join(province_bmr_sum, by = "tinh") %>% 
-  prov_sum_fn() %>% 
-  mutate(south = ifelse(tinh > 407, 1, 0))
-
 dist08_vhlss <- vhlss08 %>% 
   filter(!is.na(female), age > 15 & age < 65) %>% 
   group_by(tinh, huyen) %>% 
@@ -156,43 +78,6 @@ dist08_vhlss <- vhlss08 %>%
   ) %>% 
   prov_sum_fn() %>% 
   mutate(south = ifelse(tinh > 407, 1, 0))
-
-
-prov10_vhlss <- vhlss10 %>% 
-  filter(!is.na(female), age > 15 & age < 65) %>% 
-  group_by(tinh) %>% 
-  summarise(
-    m_total = sum(wt9 * (female == 0), na.rm = T),
-    f_total = sum(wt9 * (female == 1), na.rm = T),
-    m_workers = sum(wt9 * (female == 0 & work == 1), na.rm = T),
-    fself_emp = sum(wt9 * (female == 1 & selfemp == 1), na.rm = T),
-    f_workers = sum(wt9 * (female == 1 & work == 1), na.rm = T),
-    mself_emp = sum(wt9 * (female == 0 & selfemp == 1), na.rm = T),
-    mwagework = sum(wt9 * (female == 0 & wagework == 1), na.rm = T),
-    fwagework = sum(wt9 * (female == 1 & wagework == 1), na.rm = T),
-    fagri_work = sum(wt9 * (female == 1 & selfagri == 1), na.rm = T)
-  ) %>% 
-  left_join(province_bmr_sum2, by = "tinh") %>% 
-  prov_sum_fn() %>% 
-  mutate(south = ifelse(tinh > 44, 1, 0))  
-
-prov12_vhlss <- vhlss12 %>% 
-  filter(!is.na(female), age > 15 & age < 65) %>% 
-  group_by(tinh) %>% 
-  summarise(
-    m_total = sum(wt9 * (female == 0), na.rm = T),
-    f_total = sum(wt9 * (female == 1), na.rm = T),
-    m_workers = sum(wt9 * (female == 0 & work == 1), na.rm = T),
-    fself_emp = sum(wt9 * (female == 1 & selfemp == 1), na.rm = T),
-    f_workers = sum(wt9 * (female == 1 & work == 1), na.rm = T),
-    mself_emp = sum(wt9 * (female == 0 & selfemp == 1), na.rm = T),
-    mwagework = sum(wt9 * (female == 0 & wagework == 1), na.rm = T),
-    fwagework = sum(wt9 * (female == 1 & wagework == 1), na.rm = T),
-    fagri_work = sum(wt9 * (female == 1 & selfagri == 1), na.rm = T)
-  ) %>% 
-  left_join(province_bmr_sum2, by = "tinh") %>% 
-  prov_sum_fn() %>% 
-  mutate(south = ifelse(tinh > 44, 1, 0)) 
 
 #####################
 # INDUSTRY ANALYSIS #
