d9c63ecc4769b05e9f7602f39e2bf42c5bb9d0d8
Anri Sakakibara <anrisakaki@gmail.com>
2024-12-04 17:03:07 +0000

    Coefficient plots for PHC

--- Coefficient plots for PHC _ Robustness Check.R
+++ Coefficient plots for PHC _ Robustness Check.R
@@ -2,11 +2,11 @@
 # PROBABILITY OF WORKING, BY AGE COHORT - DISTRICT LEVEL #
 ##########################################################
 
-ggplot(work_agexbmr_09_ns, aes(x = factor(age), y = (estimate), ymin = ((estimate - std.error)), ymax = ((estimate + std.error)), colour = group)) +
+ggplot(work_agexbmr_09_ns, aes(x = factor(age_75), y = (estimate), ymin = ((estimate - std.error)), ymax = ((estimate + std.error)), colour = group)) +
   geom_pointrange() +
   geom_hline(yintercept = 0, linetype = "dotted", color = "black") +
-  labs(x = "Age in 2009", y = "Estimate and 95% Interval", color = "Region") +
-  geom_vline(xintercept = 19, linetype = "dashed", color = "blue") +  
+  labs(x = "Age in 1975", y = "Estimate and 95% Interval", color = "Region") +
+  geom_vline(xintercept = 18, linetype = "dashed", color = "blue") +  
   ggtitle("") +
   theme_minimal() +
   guides(fill = "none") +  
@@ -20,11 +20,11 @@ ggplot(work_agexbmr_09_ns, aes(x = factor(age), y = (estimate), ymin = ((estimat
   theme(axis.text.x = element_text(angle = 0, vjust = 0.5, hjust=1))
 ggsave("work_agexbmr_09_ns.jpeg", width = 14, height = 14)
 
-ggplot(work_agexbmr_cs_09_ns, aes(x = factor(age), y = (estimate), ymin = ((estimate - std.error)), ymax = ((estimate + std.error)), colour = group)) +
+ggplot(work_agexbmr_cs_09_ns, aes(x = factor(age_75), y = (estimate), ymin = ((estimate - std.error)), ymax = ((estimate + std.error)), colour = group)) +
   geom_pointrange() +
   geom_hline(yintercept = 0, linetype = "dotted", color = "black") +
-  labs(x = "Age in 2009", y = "Estimate and 95% Interval", color = "Region") +
-  geom_vline(xintercept = 19, linetype = "dashed", color = "blue") +  
+  labs(x = "Age in 1975", y = "Estimate and 95% Interval", color = "Region") +
+  geom_vline(xintercept = 18, linetype = "dashed", color = "blue") +  
   ggtitle("") +
   theme_minimal() +
   guides(fill = "none") +  
@@ -41,7 +41,7 @@ ggsave("work_agexbmr_cs_09_ns.jpeg", width = 14, height = 14)
 ggplot(work_agexbmr_09_cas, aes(x = factor(age), y = (estimate), ymin = ((estimate - std.error)), ymax = ((estimate + std.error)))) +
   geom_pointrange() +
   geom_hline(yintercept = 0, linetype = "dotted", color = "black") +
-  labs(x = "Age in 2009", y = "Estimate and 95% Interval", color = "Region") +
+  labs(x = "Age in 1975", y = "Estimate and 95% Interval", color = "Region") +
   geom_vline(xintercept = 19, linetype = "dashed", color = "blue") +  
   ggtitle("") +
   theme_minimal() +
