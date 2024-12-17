8edc8c5cdeeb593ebd7102fbc757d0140a2c5365
Anri Sakakibara <anrisakaki@gmail.com>
2024-12-17 09:35:35 +0000

    Cleaning THOR data

--- Cleaning THOR data.R
+++ Cleaning THOR data.R
@@ -62,89 +62,169 @@ gdf <- st_as_sf(thor, coords = c("TGTLONDDD_DDD_WGS84", "TGTLATDD_DDD_WGS84"), c
 gdf$tgtlonddd_ddd_wgs84 <- st_coordinates(gdf)[, "X"]
 gdf$tgtlatdd_ddd_wgs84 <- st_coordinates(gdf)[, "Y"]
 
-south <- bombs_province_miguel %>% 
-  select(provincename, south_corrected) %>% 
-  mutate(varname_1 = recode(provincename,
-                            'Ba Ria' = 'Ba Ria - Vung Tau',
-                            'Da Nang (City)' = 'Da Nang',
-                            'Ha Noi (City)' = 'Ha Noi',
-                            'Hai Phong (City)' = 'Hai Phong',
-                            'Ho Chi Minh (City)' = 'Ho Chi Minh',
-                            'Thuathien-Hue' = 'Thua Thien Hue'))
+gdf <- st_make_valid(gdf) 
 
-# Bombing intensity by district 
-vnmap2 <- vnmap2 %>% 
-  mutate(distname2018 = paste(TYPE_2, NAME_2, sep = " ")) %>% 
-  mutate(distname2018 = case_when(
-    distname2018 == 'Thành phố Thành Phố Bắc Kạn' ~ 'Thành Phố Bắc Kạn',
-    distname2018 == 'Quận Quận 1' ~ 'Quận 1',
-    distname2018 == 'Quận Quận 10' ~ 'Quận 10',
-    distname2018 == 'Quận Quận 12' ~ 'Quận 12',
-    distname2018 == 'Quận Quận 11' ~ 'Quận 11',
-    distname2018 == 'Quận Quận 2' ~ 'Quận 2',
-    distname2018 == 'Quận Quận 3' ~ 'Quận 3',
-    distname2018 == 'Quận Quận 4' ~ 'Quận 4',
-    distname2018 == 'Quận Quận 5' ~ 'Quận 5',
-    distname2018 == 'Quận Quận 6' ~ 'Quận 6',
-    distname2018 == 'Quận Quận 7' ~ 'Quận 7',
-    distname2018 == 'Quận Quận 8' ~ 'Quận 8',
-    distname2018 == 'Quận Quận 9' ~ 'Quận 9',
-    distname2018 == "Huyện Tịnh Biên" & NAME_1 == "An Giang" ~ "Thị xã Tịnh Biên",
-    distname2018 == "Thị xã Dĩ An" & NAME_1 == "Bình Dương" ~ "Thành phố Dĩ An",
-    distname2018 == "Thị xã Thuận An" & NAME_1 == "Bình Dương" ~ "Thành phố Dĩ An",
-    distname2018 == "Thị xã Tân Uyên" & NAME_1 == "Bình Dương" ~ "Huyện Bắc Tân Uyên",
-    distname2018 == "Huyện Chơn Thành" & NAME_1 == "Bình Phước" ~ "Thị xã Chơn Thành",
-    distname2018 == "Thị xã Đồng Xoài" & NAME_1 == "Bình Phước" ~ "Thành phố Đồng Xoài",
-    distname2018 == "Huyện Hoài Nhơn" & NAME_1 == "Bình Định" ~ "Thị xã Hoài Nhơn",
-    distname2018 == "Thành phố Qui Nhơn" & NAME_1 == "Bình Định" ~ "Thành phố Quy Nhơn",
-    distname2018 == "Huyện Việt Yên" & NAME_1 == "Bắc Giang" ~ "Thị Xã Việt Yên",
-    distname2018 == "Huyện Quế Võ" & NAME_1 == "Bắc Ninh" ~ "Thị xã Quế Võ",
-    distname2018 == "Huyện Thuận Thành" & NAME_1 == "Bắc Ninh" ~ "Thị xã Thuận Thành",
-    distname2018 == "Thị xã Từ Sơn" & NAME_1 == "Bắc Ninh" ~ "Thành phố Từ Sơn",
-    distname2018 == "Huyện Duy Tiên" & NAME_1 == "Hà Nam" ~ "Thị xã Duy Tiên",
-    distname2018 == "Thị xã Kỳ Anh (Thị xã)" & NAME_1 == "Hà Tĩnh" ~ "Thị xã Kỳ Anh",
-    distname2018 == "Huyện Mỹ Hào" & NAME_1 == "Hưng Yên" ~ "Thị xã Mỹ Hào",
-    distname2018 == "Thị xã Chí Linh" & NAME_1 == "Hải Dương" ~ "Thành phố Chí Linh",
-    distname2018 == "Huyện Kinh Môn" & NAME_1 == "Hải Dương" ~ "Thị xã Kinh Môn",
-    distname2018 == "Thị xã Long Mỹ (Thị xã)" & NAME_1 == "Hậu Giang" ~ "Thị xã Long Mỹ",
-    distname2018 == "Thị xã Ngã Bảy" & NAME_1 == "Hậu Giang" ~ "Thành phố Ngã Bảy",
-    distname2018 == "Thị xã Hà Tiên" & NAME_1 == "Kiên Giang" ~ "Thành phố Hà Tiên",
-    distname2018 == "Huyện Phú Quốc" & NAME_1 == "Kiên Giang" ~ "Thành phố Phú Quốc",
-    distname2018 == "Huyện Đông Hòa" & NAME_1 == "Phú Yên" ~ "Thị xã Đông Hòa",
-    distname2018 == "Thành phố Thành Phố Đồng Hới" & NAME_1 == "Quảng Bình" ~ "Thành Phố Đồng Hới",
-    distname2018 == "Huyện Đức Phổ" & NAME_1 == "Quảng Ngãi" ~ "Thị xã Đức Phổ",
-    distname2018 == "Thị xã Phổ Yên" & NAME_1 == "Thái Nguyên" ~ "Thành phố Phổ Yên",
-    distname2018 == "Thị xã Cai Lậy (Thị xã)" & NAME_1 == "Tiền Giang" ~ "Thị xã Cai Lậy",
-    distname2018 == "Thị xã Duyên Hải (Thị xã)" & NAME_1 == "Trà Vinh" ~ "Thị xã Duyên Hải",
-    distname2018 == "Huyện Hòa Thành" & NAME_1 == "Tây Ninh" ~ "Thị xã Hòa Thành",
-    distname2018 == "Huyện Trảng Bàng" & NAME_1 == "Tây Ninh" ~ "Thị xã Trảng Bàng",
-    distname2018 == "Huyện Vũng Liêm" & NAME_1 == "Vĩnh Long" ~ "Huyện  Vũng Liêm",
-    distname2018 == "Thị xã Phúc Yên" & NAME_1 == "Vĩnh Phúc" ~ "Thành phố Phúc Yên",
-    distname2018 == "Thị xã Thị Xã Buôn Hồ" & NAME_1 == "Đắk Lắk" ~ "Thị Xã Buôn Hồ",
-    distname2018 == "Thị xã Gia Nghĩa" & NAME_1 == "Đắk Lắk" ~ "Thành phố Gia Nghĩa",
-    distname2018 == "Thị xã Long Khánh" & NAME_1 == "Đồng Nai" ~ "Thành phố Long Khánh",
-    distname2018 == "Thành phố Cao Lãnh (Thành phố)" & NAME_1 == "Đồng Tháp" ~ "Thành phố Cao Lãnh",
-    distname2018 == "Thị xã Hồng Ngự (Thị xã)" & NAME_1 == "Đồng Tháp" ~ "Thị xã Hồng Ngự",
-    TRUE ~ distname2018
-  ),
-  NAME_1 = ifelse(distname2018 == "Thành phố Gia Nghĩa" & NAME_1 == "Đắk Lắk", "Đắk Nông", NAME_1))
+####################################
+# PROVINCE-LEVEL BOMBING INTENSITY #
+####################################
 
-district_bmr <- st_join(gdf, vnmap2)
+province_bmr <- st_join(gdf, vnmap1) %>% rename_all(tolower)
 
-district_bmr_sf <- district_bmr %>% 
-  group_by(NAME_1, VARNAME_2, distname2018) %>% 
+province_bmr_sum_sf <- province_bmr %>% 
+  ungroup() %>% 
+  filter(!is.na(varname_1)) %>% 
+  group_by(varname_1, name_1) %>% 
+  summarise(tot_bmr = sum(numweaponsdelivered),
+            tot_bmr_lb = sum(weightdelivered),
+            tot_agri = sum(numweaponsdelivered[agriculture == 1]),
+            tot_agri_lb = sum(weightdelivered[agriculture == 1]),
+            tot_industry = sum(numweaponsdelivered[industry == 1]),
+            tot_industry_lb = sum(weightdelivered[industry == 1])) %>% 
+  ungroup() %>% 
+  left_join(prov_casualties, by = "varname_1")
+
+bases <- st_transform(bases, crs = 4326)
+hcmtrail <- st_transform(hcmtrail, crs = 4326)
+
+nearest_base <- st_nearest_feature(province_bmr_sum_sf, bases)
+province_bmr_sum_sf$dist_nearest_base <-
+  as.numeric(st_distance(province_bmr_sum_sf, bases[nearest_base, ], by_element = TRUE)) / 1000
+
+nearest_trail <- st_nearest_feature(province_bmr_sum_sf, hcmtrail)
+province_bmr_sum_sf$dist_nearest_hochi <-
+  as.numeric(st_distance(province_bmr_sum_sf, hcmtrail[nearest_trail, ], by_element = TRUE)) / 1000
+
+distance_info <- province_bmr_sum_sf %>% 
+  select(varname_1, dist_nearest_base, dist_nearest_hochi)
+distance_info <- sf::st_drop_geometry(distance_info)
+
+province_bmr_sum <- province_bmr_sum_sf %>% 
+  ungroup() %>% 
+  select(-varname_1) %>%
+  sf::st_drop_geometry() %>% 
+  mutate(name_1 = recode(name_1,
+                         'Điện Biên' = 'Lai Châu',
+                         'Sơn La' = 'Lai Châu',
+                         .default = name_1),
+         tinh = recode(name_1,
+                       'An Giang' = 805,
+                       'Bà Rịa - Vũng Tàu' = 717,
+                       'Bắc Giang' = 221,
+                       'Bắc Kạn' = 207,
+                       'Bạc Liêu' = 821,
+                       'Bắc Ninh' = 106,
+                       'Bến Tre' = 811,
+                       'Bình Định' = 507,
+                       'Bình Dương' = 711,
+                       'Bình Phước' = 707,
+                       'Bình Thuận' = 715,
+                       'Cà Mau' = 823,
+                       'Cần Thơ' = 815,
+                       'Cao Bằng' = 203,
+                       'Đà Nẵng' = 501,
+                       'Đắk Lắk' = 605,
+                       'Đắk Nông' = 606,
+                       'Lai Châu' = 301,
+                       'Đồng Nai' = 713,
+                       'Đồng Tháp' = 803,
+                       'Gia Lai' = 603,
+                       'Hà Giang' = 201,
+                       'Hà Nam' = 111,
+                       'Hà Nội' = 101,
+                       'Hà Tĩnh' = 405,
+                       'Hải Dương' = 107,
+                       'Hải Phòng' = 103,
+                       'Hậu Giang' = 816,
+                       'Hồ Chí Minh' = 701,
+                       'Hoà Bình' = 305,
+                       'Hưng Yên' = 109,
+                       'Khánh Hòa' = 511,
+                       'Kiên Giang' = 813,
+                       'Kon Tum' = 601,
+                       'Lâm Đồng' = 607,
+                       'Lạng Sơn' = 209,
+                       'Lào Cai' = 205,
+                       'Long An' = 801,
+                       'Nam Định' = 113,
+                       'Nghệ An' = 403,
+                       'Ninh Bình' = 117,
+                       'Ninh Thuận' = 705,
+                       'Phú Thọ' = 217,
+                       'Phú Yên' = 509,
+                       'Quảng Bình' = 407,
+                       'Quảng Nam' = 503,
+                       'Quảng Ngãi' = 505,
+                       'Quảng Ninh' = 225,
+                       'Quảng Trị' = 409,
+                       'Sóc Trăng' = 819,
+                       'Tây Ninh' = 709,
+                       'Thái Bình' = 115,
+                       'Thái Nguyên' = 215,
+                       'Thanh Hóa' = 401,
+                       'Thừa Thiên Huế' = 411,
+                       'Tiền Giang' = 807,
+                       'Trà Vinh' = 817,
+                       'Tuyên Quang' = 211,
+                       'Vĩnh Long' = 809,
+                       'Vĩnh Phúc' = 104,
+                       'Yên Bái' = 213,
+                       .default = NA_real_)) %>% 
+  group_by(tinh, name_1) %>% 
+  summarise(tot_bmr_prov = sum(tot_bmr),
+            tot_bmr_lb_prov = sum(tot_bmr_lb),
+            killed_tot_prov = sum(killed_tot),
+            dist_nearest_base_prov = min(dist_nearest_base),
+            dist_nearest_hochi_prov = min(dist_nearest_hochi)) %>% 
+  select(tinh, everything())
+
+save(province_bmr_sum, file = "province_bmr_sum.Rda")
+
+#################################
+# Bombing intensity by district # 
+#################################
+
+geo2_vn <- st_make_valid(geo2_vn)
+district_bmr_phc_sf <- st_join(geo2_vn, gdf)
+
+district_bmr_phc <- district_bmr_phc_sf %>% 
+  sf::st_drop_geometry() %>% 
+  group_by(GEOLEVEL2) %>% 
   summarise(
     tot_bmr = sum(NUMWEAPONSDELIVERED, na.rm = T),
     tot_bmr_lb = sum(WEIGHTDELIVERED, na.rm = T)) %>% 
   sf::st_drop_geometry() %>% 
-  left_join(vnmap2, by = c("NAME_1", "VARNAME_2", "distname2018")) %>% 
-  left_join(dist_casualties, by = "distname2018") %>% 
-  st_as_sf()
+  mutate(mean_tot_bmr = mean(tot_bmr, na.rm = T),
+         sd_tot_bmr = sd(tot_bmr, na.rm = T),
+         tot_bmr_std = (tot_bmr - mean_tot_bmr)/sd_tot_bmr,
+         GEOLEVEL2 = as.double(GEOLEVEL2)) %>% 
+  filter(!is.na(GEOLEVEL2)) %>% 
+  rename_all(tolower)
 
-district_bmr_sum <- district_bmr_sf %>% 
-  rename_all(tolower) %>%
-  sf::st_drop_geometry() %>% 
-  rename(provname = name_1,
-         distname = distname2018)
+district_bmr_sum_phc_sf <- district_bmr_phc %>% 
+  mutate(geolevel2 = as.character(geolevel2)) %>% 
+  left_join(geo2_vn, by = c("geolevel2" = "GEOLEVEL2")) %>% 
+  sf::st_as_sf()
+
+######################
+# SUMMARY OF TARGETS #
+######################
+
+targets_sum <- district_bmr %>% 
+  sf::st_drop_geometry() %>% rename_all(tolower) %>%
+  summarise(agri_s = sum(numweaponsdelivered[tgtcountry == "SOUTH VIETNAM" & agriculture == 1], na.rm = T),
+            agri_n = sum(numweaponsdelivered[tgtcountry == "NORTH VIETNAM" & agriculture == 1], na.rm = T),
+            infra_s = sum(numweaponsdelivered[tgtcountry == "SOUTH VIETNAM" & infrastructure == 1], na.rm = T),
+            infra_n = sum(numweaponsdelivered[tgtcountry == "NORTH VIETNAM" & infrastructure == 1], na.rm = T),
+            civilian_s = sum(numweaponsdelivered[tgtcountry == "SOUTH VIETNAM" & civilian == 1], na.rm = T),
+            civilian_n = sum(numweaponsdelivered[tgtcountry == "NORTH VIETNAM" & civilian == 1], na.rm = T),
+            industry_s = sum(numweaponsdelivered[tgtcountry == "SOUTH VIETNAM" & industry == 1], na.rm = T),
+            industry_n = sum(numweaponsdelivered[tgtcountry == "NORTH VIETNAM" & industry == 1], na.rm = T))
+
+south_missiontype <- as.data.frame(table(district_bmr$MFUNC_DESC[district_bmr$TGTCOUNTRY == "SOUTH VIETNAM"]))
+south_missiontype$Share <- prop.table(south_missiontype$Freq)
+
+north_missiontype <- as.data.frame(table(district_bmr$MFUNC_DESC[district_bmr$TGTCOUNTRY == "NORTH VIETNAM"]))
+north_missiontype$Share <- prop.table(north_missiontype$Freq)
 
 save(district_bmr_sum, file = "district_bmr_sum.Rda")
+save(district_bmr_phc, file = "district_bmr_phc.Rda")
