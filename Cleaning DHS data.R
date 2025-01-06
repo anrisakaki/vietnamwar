0669de7c4c7dd49a2f8af76ec37e25364e72986f
Anri Sakakibara <anrisakaki@gmail.com>
2025-01-06 21:46:16 +0000

    Cleaning DHS data

--- Cleaning DHS data.R
+++ Cleaning DHS data.R
@@ -1,3 +1,9 @@
 dhs97 <- dhs97 %>% 
   mutate(sprovin = ifelse(sprovin == 22, 1, sprovin)) %>% 
   left_join(bombs_province97, by = "sprovin")
+
+dhs_summary <- dhs97 %>% 
+  filter(v012 > (1997-(1975-15))) %>% 
+  group_by(sprovin) %>% 
+  summarise(firstmarriage = weighted.mean(v511, v005),
+            tot_bmr_prov = mean(tot_bmr_prov))
