6d7eed1d45174637313e976b1e9c51febf1bba4a
Anri Sakakibara <anrisakaki@gmail.com>
2025-01-11 12:31:04 +0000

    DHS Regressions

--- DHS Regressions.R
+++ DHS Regressions.R
@@ -1,8 +1,14 @@
-etable(feols(v511 ~ tot_bmr_prov_std + as.factor(v131) + v133 + as.factor(v025) + as.factor(v130) | v012,
-             subset(dhs97, v012 > (1997-(1975-15))),
-             weights = ~v005))
+iplot(feols(v511 ~ i(age_75, tot_bmr_prov_std) + as.factor(v131) + v133 + as.factor(v025) | age_75,
+             dhs97,
+             weights = ~v005,
+             vcov = ~sprovin))
 
-etable(feols(spouse_age_diff ~ tot_bmr_prov_std + as.factor(v131) + v133 + as.factor(v025) + as.factor(v130),
+iplot(feols(v511 ~ i(age_75, tot_bmr_prov_std) + as.factor(v025) + v107 +as.factor(v131) | age_75,
+            dhs02,
+            weights = ~v005,
+            vcov = ~sprovin))
+
+etable(feols(spouse_age_diff ~ tot_bmr_prov_std + as.factor(v131) + v133 + as.factor(v025) + as.factor(v130) | v012,
              subset(dhs97, v012 > (1997-(1975-15))),
              weights = ~v005,
              vcov = ~sprovin))
