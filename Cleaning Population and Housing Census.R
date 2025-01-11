dbe0be408d8a8d33fbe36626efeee06a20e4fe8c
Anri Sakakibara <anrisakaki@gmail.com>
2025-01-11 12:30:45 +0000

    Cleaning PHC

--- Cleaning Population and Housing Census.R
+++ Cleaning Population and Housing Census.R
@@ -9,7 +9,8 @@ for (i in bombs) {
 phc <- phc %>%
   mutate(yrschool = ifelse(yrschool > 19, NA, yrschool),
          married = ifelse(marst == 2, 1, 0),
-         widowed = ifelse(marst == 4, 1, 0),
+         evermarried = ifelse(marst == 4 | marst == 2, 1, 0),
+         widowed = ifelse(marst == 4 & sex == 2, 1, 0),
          single = ifelse(marst == 1, 1, 0),
          minority = ifelse(ethnicvn > 1, 1, 0),
          literate = ifelse(lit == 2, 1, 0),
@@ -27,6 +28,7 @@ phc <- phc %>%
          manu = ifelse(age < 17 | age > 64, NA, manu),
          self = ifelse(age < 17 | age > 64, NA, self),
          housework = ifelse(age < 17 | age > 64, NA, housework),
+         age_75 = 1975 - birthyr,
          urban = ifelse(urban == 1, 1, 0),
          south = ifelse(geo1_vn1989 > 26 & year == 1989 | geo1_vn1989 == 2 & year == 1989 , 1, 0),
          south = ifelse(geo1_vn1999 > 405 & year == 1999, 1, 0),
@@ -38,40 +40,55 @@ phc <- phc %>%
 
 # Separating by year 
 
+phc_fn <- function(i){
+  
+  i %>% 
+  group_by(serial) %>%
+  mutate(
+    widowed_mother = ifelse(
+      birthyr[match(momloc, pernum)] <= (1975-16) &
+        widowed[match(momloc, pernum)] == 1,
+      1,
+      ifelse(
+        !is.na(momloc) & birthyr[match(momloc, pernum)] > (1975-16),
+        NA,
+        0
+      )
+    ),
+    spouse_age = age[match(sploc, pernum)],
+    spouse_yrschool = yrschool[match(sploc, pernum)]
+  ) %>%
+  ungroup()
+}
+
 phc89 <- phc %>% 
   filter(year == 1989) %>% 
   left_join(bombs_province89, by = "geo1_vn1989") %>% 
   mutate(south = ifelse(geo1_vn1989 > 26 | geo1_vn1989 == 2, 1, 0))  %>% 
-  group_by(serial) %>% 
-  mutate(widow_hh = ifelse(any(widowed == 1 & female == 1 & age > 38 & age < 69), 1, 0),
-         widow_hh = ifelse(is.na(widow_hh), 0, widow_hh))
+  phc_fn()
 
 phc99 <- phc %>% 
   filter(year == 1999) %>% 
   mutate(geo1_vn1999 = ifelse(geo1_vn1999 == 105, 101, geo1_vn1999)) %>% 
   left_join(bombs_province99, by = "geo1_vn1999") %>% 
-  mutate(south = ifelse(geo1_vn1999 > 405, 1, 0))  %>% 
-  group_by(serial) %>% 
-  mutate(widow_hh = ifelse(any(widowed == 1 & female == 1 & age > 48 & age < 79), 1, 0),
-         widow_hh = ifelse(is.na(widow_hh), 0, widow_hh))
+  mutate(south = ifelse(geo1_vn1999 > 405, 1, 0)) %>% 
+  phc_fn()
 
 phc09 <- phc %>% 
   filter(year == 2009) %>% 
   left_join(bombs_province09, by = "geo1_vn2009") %>% 
   left_join(district_bmr_phc, by = "geo2_vn") %>% 
-  mutate(south = ifelse(geo1_vn2009 > 44, 1, 0))%>% 
-  group_by(serial) %>% 
-  mutate(widow_hh = ifelse(any(widowed == 1 & female == 1 & age > 58 & age < 89), 1, 0),
-         widow_hh = ifelse(is.na(widow_hh), 0, widow_hh))
+  mutate(south = ifelse(geo1_vn2009 > 44, 1, 0)) %>% 
+  phc_fn()
 
 phc19 <- phc %>% 
   filter(year == 2019) %>% 
   left_join(bombs_province09, by = c("geo1_vn2019" = "geo1_vn2009")) %>% 
   left_join(district_bmr_phc, by = "geo2_vn") %>% 
-  mutate(south = ifelse(geo1_vn2019 > 44, 1, 0))%>% 
-  group_by(serial) %>% 
-  mutate(widow_hh = ifelse(any(widowed == 1 & female == 1 & age > 68), 1, 0),
-         widow_hh = ifelse(is.na(widow_hh), 0, widow_hh))
+  mutate(south = ifelse(geo1_vn2019 > 44, 1, 0),
+         spagegap = spouse_age - age,
+         speducgap = spouse_yrschool - yrschool) %>% 
+  phc_fn()
 
 phc_all <- bind_rows(phc89, phc99, phc09, phc19)
 
@@ -82,6 +99,8 @@ save(phc19, file = "phc19.Rda")
 save(phc_all, file = "phc_all.Rda")
 
 # Widows in 1989
+
+phc89 %>% summarise(total_widows = sum(perwt[widowed == 1 & female == 1], na.rm = TRUE))
 
 widowed_89 <- phc89 %>%
   filter(female == 1 & age >15) %>%
@@ -96,21 +115,21 @@ widowed_89 <- phc89 %>%
 
 etable(list(
   # age 
-  feols(age ~ as.factor(widowed), subset(phc89, female == 1 & age > (1989 - (1975-15)) & age < 65 & south == 1), weights = ~perwt),
+  feols(age ~ as.factor(widowed), subset(phc89, female == 1 & birthyr < (1975-16) & age < 65 & south == 1 & evermarried == 1), weights = ~perwt),
   
   # work 
-  feols(work ~ as.factor(widowed), subset(phc89, female == 1 & age > (1989 - (1975-15))  & age < 65 & south == 1), weights = ~perwt),
+  feols(work ~ as.factor(widowed), subset(phc89, female == 1 & birthyr < (1975-16)  & age < 65 & south == 1 & evermarried == 1), weights = ~perwt),
   
   # educ 
-  feols(yrschool ~ as.factor(widowed), subset(phc89, female == 1 & age > (1989 - (1975-15))  & age < 65 & south == 1), weights = ~perwt),
+  feols(yrschool ~ as.factor(widowed), subset(phc89, female == 1 & birthyr < (1975-16)  & age < 65 & south == 1 & evermarried == 1), weights = ~perwt),
   
   # no. of children 
-  feols(nchild ~ as.factor(widowed), subset(phc89, female == 1 & age > (1989 - (1975-15))  & age < 65 & south == 1), weights = ~perwt),
+  feols(chborn ~ as.factor(widowed), subset(phc89, female == 1 & birthyr < (1975-16)  & age < 65 & south == 1 & evermarried == 1 & chborn < 99), weights = ~perwt),
   
   # urban 
-  feols(urban ~ as.factor(widowed), subset(phc89, female == 1 & age > (1989 - (1975-15))  & age < 65 & south == 1), weights = ~perwt),
+  feols(urban ~ as.factor(widowed), subset(phc89, female == 1 & birthyr < (1975-16)  & age < 65 & south == 1 & evermarried == 1), weights = ~perwt),
   
   # famsize 
-  feols(famsize ~ as.factor(widowed), subset(phc89, female == 1 & age > (1989 - (1975-15))  & age < 65 & south == 1), weights = ~perwt)
+  feols(famsize ~ as.factor(widowed), subset(phc89, female == 1 & birthyr < (1975-16)  & age < 65 & south == 1 & evermarried == 1), weights = ~perwt)
 ))
   
